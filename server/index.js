c8dd3a86cf0b5b3ee03a145cdbe59c3e590b3a58
Adam Chmara <achmara@frinx.io>
2019-04-08 12:02:46 +0200

    Add .env for deployment specific configurations
    Signed-off-by: Adam Chmara <achmara@frinx.io>

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const pino = require('express-pino-logger')();
 const odlAPI = require('../src/api/odl');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
