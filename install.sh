d498e73f341bafaef23ac560ed827fea1d0ae82c
Adam Chmara <achmara@frinx.io>
2019-05-15 15:52:01 +0200

    Install script prepends <http://> to --odl host address automatically
    Signed-off-by: Adam Chmara <achmara@frinx.io>

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 # install.sh - installation script of UniConfig-UI container
 
 ODL=
-ODL_HOST_DEFAULT="http://localhost:8080"
+ODL_HOST_DEFAULT="localhost:8080"
 
 while [ "$1" != "" ]; do
     case $1 in
@@ -24,7 +24,7 @@ echo "Setting up UniConfig UI ..."
 if [ -z "$ODL" ]; then
     echo "\e[93mUsing ${ODL_HOST_DEFAULT} as default, please set your ODL host address.\e[0m"
   else
-    sed -i 's,.*ODL_HOST=.*,ODL_HOST='"$ODL"',' ./.env
+    sed -i 's,.*ODL_HOST=.*,ODL_HOST='"http://""$ODL"',' ./.env
     echo "\e[35mUsing ODL at host: '$ODL' \e[0m"
 fi
 
