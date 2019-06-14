099792a757ca9bd8cce0e5cefecfedcf40f0045b
Adam Chmara <achmara@frinx.io>
2019-06-14 13:49:24 +0200

    Categorize workflows into tabs & outsource into separate components
    Signed-off-by: Adam Chmara <achmara@frinx.io>

---
+++ src/components/workflows/workflowList/WorkflowDefs/WorkflowDefs.js
@@ -0,0 +1,220 @@
+import React, {Component} from 'react';
+import {Accordion, Button, Card, Col, Form, Row, Table} from 'react-bootstrap'
+import {Typeahead} from 'react-bootstrap-typeahead';
+import 'react-bootstrap-typeahead/css/Typeahead.css';
+import './WorkflowDefs.css'
+import DefinitionModal from "../../definitonModal/DefinitionModal";
+import InputModal from "../../inputModal/InputModal";
+
+const http = require('../../../../server/HttpServerSide').HttpClient;
+
+class WorkflowDefs extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            keywords: "",
+            labels: [],
+            data: [],
+            table: [],
+            highlight: [],
+            activeRow: null,
+            activeWf: null,
+            defModal: false
+        };
+        this.table = React.createRef();
+        this.onEditSearch = this.onEditSearch.bind(this);
+    }
+
+    componentWillMount() {
+        this.search();
+    }
+
+    componentDidMount() {
+        http.get('/api/conductor/metadata/workflow').then(res => {
+            this.setState({
+                data: res.result || []
+            })
+        })
+    }
+
+    onEditSearch(event) {
+        this.setState({keywords: event.target.value}, () =>{
+            this.search()
+        })
+    }
+
+    onLabelSearch(event) {
+        this.setState({labels: event}, () =>{
+            this.searchLabel()
+        })
+    }
+
+    searchLabel() {
+        let toBeRendered = [];
+        const rows = this.state.keywords !== "" ? this.state.table : this.state.data;
+        for (let i = 0; i < rows.length; i++) {
+            if (rows[i]["description"]) {
+                let tags = rows[i]["description"].split("-").pop().replace(/\s/g, "").split(",");
+                if (this.state.labels.every(elem => tags.indexOf(elem) > -1)) {
+                    toBeRendered.push(rows[i]);
+                }
+            }
+        }
+        this.setState({
+            table: toBeRendered,
+        });
+        return null;
+    }
+
+    search() {
+        let toBeRendered = [];
+        let toBeHighlited = [];
+        let query = this.state.keywords.toUpperCase();
+        if (query !== "") {
+            const rows = this.state.table.length > 0 ? this.state.table : this.state.data;
+            for (let i = 0; i < rows.length; i++) {
+                if (rows[i]["name"] && rows[i]["name"].toString().toUpperCase().indexOf(query) !== -1) {
+                    toBeRendered.push(rows[i]);
+                    toBeHighlited.push(i);
+                }
+            }
+        } else {
+            this.searchLabel();
+            return;
+        }
+        this.setState({
+            table: toBeRendered,
+            highlight: toBeHighlited,
+        })
+    }
+
+    calculateHighlight(i) {
+        if (this.state.highlight[i] !== null) {
+            return 'hilit'
+        }
+        return ''
+    }
+
+    changeActiveRow(i) {
+        this.getLabels();
+        this.setState({
+            activeRow: this.state.activeRow === i ? null : i,
+            activeWf: document.querySelector(`#wf${i}`).innerText
+        });
+    }
+
+    repeat() {
+        let output = [];
+        let highlight;
+        let dataset;
+        if (this.state.keywords === "" && this.state.labels.length < 1) {
+            dataset = this.state.data;
+            highlight = false
+        } else {
+            dataset = this.state.table;
+            highlight = true
+        }
+        for (let i = 0; i < dataset.length; i++) {
+            output.push(
+                <div className="wfRow" key={i}>
+                    <Accordion.Toggle onClick={this.changeActiveRow.bind(this,i)} className="clickable" as={Card.Header} variant="link" eventKey={i}>
+                        <p id={`wf${i}`} className={highlight ? this.calculateHighlight(i)  : ''}>{dataset[i]["name"]+" / "+dataset[i]["version"]}</p>
+                    </Accordion.Toggle>
+                    <Accordion.Collapse eventKey={i}>
+                        <Card.Body style={{padding: "0px"}}>
+                            <div style={{background: "linear-gradient(-120deg, rgb(0, 147, 255) 0%, rgb(0, 118, 203) 100%)", padding: "15px", marginBottom: "10px"}}>
+                                <Button variant="outline-light noshadow" onClick={this.showInputModal.bind(this,i)}>Input</Button>
+                                <Button variant="outline-light noshadow" onClick={this.showDefinitionModal.bind(this,i)}>Definition</Button>
+                                <Button variant="outline-light noshadow">Diagram</Button>
+                            </div>
+                            <div className="accordBody">
+                                <b>Tasks</b><br/>
+                                <p>{JSON.stringify(dataset[i]["tasks"].map(task => {return task.name}))}</p>
+                                <b>{dataset[i]["inputParameters"] ? "Input Parameters" : null}</b><br/>
+                                <p>{JSON.stringify(dataset[i]["inputParameters"])}</p>
+                            </div>
+                        </Card.Body>
+                    </Accordion.Collapse>
+                </div>
+            )
+        }
+        return output
+    }
+
+    getLabels() {
+        let labelsArr = [];
+        this.state.data.map(wf => {
+            let str = wf["description"].substring(wf["description"].indexOf("-") + 1);
+            if (str === wf["description"]) {
+                str = null;
+            }
+            if (str) {
+                str = str.replace(/\s/g, "");
+                labelsArr = labelsArr.concat(str.split(","));
+            }
+            return null;
+        });
+        return [...new Set([].concat(...labelsArr))];
+    }
+
+    showDefinitionModal() {
+        this.setState({
+            defModal: !this.state.defModal
+        })
+    }
+
+    showInputModal() {
+        this.setState({
+            inputModal: !this.state.inputModal
+        })
+    }
+
+    render(){
+
+        let definitionModal = this.state.defModal ?
+            <DefinitionModal wf={this.state.activeWf} modalHandler={this.showDefinitionModal.bind(this)}
+                             show={this.state.defModal}/> : null;
+
+        let inputModal = this.state.inputModal ?
+            <InputModal wf={this.state.activeWf} modalHandler={this.showInputModal.bind(this)}
+                        show={this.state.inputModal}/> : null;
+
+        return (
+         <div>
+             {definitionModal}
+             {inputModal}
+             <Row>
+                 <Col>
+                     <Typeahead
+                         selected={this.state.labels}
+                         onChange={this.onLabelSearch.bind(this)} clearButton
+                         labelKey="name" multiple options={this.getLabels()}
+                         placeholder="Search by label."/>
+                 </Col>
+                 <Col>
+                     <Form.Group>
+                         <Form.Control value={this.state.keywords} onChange={this.onEditSearch}
+                                       placeholder="Search by keyword."/>
+                     </Form.Group>
+                 </Col>
+             </Row>
+             <div className="scrollWrapper">
+                 <Table ref={this.table}>
+                     <thead>
+                     <tr>
+                         <th>Name/Version</th>
+                     </tr>
+                     </thead>
+                     <tbody>
+                     <Accordion activeKey={this.state.activeRow}>
+                         {this.repeat()}
+                     </Accordion>
+                     </tbody>
+                 </Table>
+             </div>
+         </div>
+        )
+    }
+}
+
+export default WorkflowDefs
