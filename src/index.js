2cb5ae4ec534bbd784c054cc964c4ff7bc1616d9
Adam Chmara <achmara@frinx.io>
2019-06-29 01:25:37 +0200

    Provide store, middleware and redux enhancers to application
    Signed-off-by: Adam Chmara <achmara@frinx.io>

--- src/index.js
+++ src/index.js
@@ -4,7 +4,22 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 import App from "./App";
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+import {combineReducers, createStore, applyMiddleware, compose} from "redux";
+import thunk from 'redux-thunk';
+import bulkReducer from './store/reducers/bulk';
+import searchReducer from './store/reducers/searchExecs'
+import {Provider} from "react-redux";
+
+
+const rootReducer = combineReducers({
+    bulkReducer, searchReducer
+});
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+
+ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
