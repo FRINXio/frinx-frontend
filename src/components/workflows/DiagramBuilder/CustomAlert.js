e535738eca332936e12f9e9e7239273919b3f386
Adam Chmara <adam.chmara1@gmail.com>
2019-10-01 15:59:41 +0200

    [Workflow builder] Fix alerts animations
    Signed-off-by: Adam Chmara <adam.chmara1@gmail.com>

--- src/components/workflows/DiagramBuilder/CustomAlert.js
+++ src/components/workflows/DiagramBuilder/CustomAlert.js
@@ -1,26 +1,18 @@
 import React from 'react';
 import {Alert} from "react-bootstrap";
 
-const customAlertCss = {
-    position: "absolute",
-    width: "100%",
-    zIndex: "9"
-};
-
 const CustomAlert = (props) => {
     return (
-        <div style={customAlertCss}>
-            {props.show ?
-                <Alert variant={props.alertVariant} dismissible
-                       onClose={() => props.showCustomAlert(false, "")}
-                       style={{textAlign: "right"}}>
-                    {props.alertVariant === "danger" ?
-                        <i className="fas fa-exclamation-triangle"/>
-                        :
-                        <i className="fas fa-info-circle"/>}
-                    &nbsp;&nbsp;{props.msg}
-                </Alert>
-                : null}
+        <div className={props.show ? 'custom-alert-in' : 'custom-alert-out'}>
+            <Alert show={props.show} variant={props.alertVariant} dismissible
+                   onClose={() => props.showCustomAlert(false)}
+                   style={{textAlign: "right"}}>
+                {props.alertVariant === "danger" ?
+                    <i className="fas fa-exclamation-triangle"/>
+                    :
+                    <i className="fas fa-info-circle"/>}
+                &nbsp;&nbsp;{props.msg}
+            </Alert>
         </div>
     )
 };
