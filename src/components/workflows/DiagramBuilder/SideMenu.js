d603f68b4d77de2f620686dd75c11142bd9032de
Adam Chmara <adam.chmara1@gmail.com>
2019-10-03 15:08:19 +0200

    [Workflow builder] Refactor SideMenu code
    Signed-off-by: Adam Chmara <adam.chmara1@gmail.com>

--- src/components/workflows/DiagramBuilder/SideMenu.js
+++ src/components/workflows/DiagramBuilder/SideMenu.js
@@ -2,101 +2,92 @@ import React from "react";
 import SideMenuItem from "./SideMenuItem";
 import {getWfInputsRegex} from "./builder-utils";
 
-const functionalTaks = (props) => {
+const SideMenu = (props) => {
 
-    let functionalTasks = [];
+    const functionalTaks = () => {
+        let functionalTasks = [];
 
-    props.functional.forEach((func, i) => {
-        if (func === "fork") {
+        props.functional.forEach((func, i) => {
+            if (func === "fork") {
+                let wfObject = {
+                    name: "forkTask",
+                    taskReferenceName: "forkTaskRef",
+                    type: "FORK_JOIN",
+                    forkTasks: [],
+                    optional: false,
+                    startDelay: 0
+                };
+                functionalTasks.push(<SideMenuItem id={`functionalNode${i}`} model={{type: func, wfObject}}
+                                                   name={func.toUpperCase()} color="#0095FF"/>)
+            } else if (func === "join") {
+                let wfObject = {
+                    name: "joinTask",
+                    taskReferenceName: "joinTaskRef",
+                    type: "JOIN",
+                    joinOn: [],
+                    optional: false,
+                    startDelay: 0
+                };
+                functionalTasks.push(<SideMenuItem id={`functionalNode${i}`} model={{type: func, wfObject}}
+                                                   name={func.toUpperCase()} color="#0095FF"/>)
+            } else if (func === "decision") {
+                let wfObject = {
+                    name: "decisionTask",
+                    taskReferenceName: "decisionTaskRef",
+                    inputParameters: {
+                        case_value_param: ""
+                    },
+                    type: "DECISION",
+                    caseValueParam: "case_value_param",
+                    decisionCases: {
+                        false: [],
+                        true: []
+                    },
+                    optional: false,
+                    startDelay: 0
+                };
+                functionalTasks.push(<SideMenuItem id={`functionalNode${i}`} model={{type: func, wfObject}}
+                                                   name={func.toUpperCase()} color="#0095FF"/>)
+            } else {
+                functionalTasks.push(<SideMenuItem id={`functionalNode${i}`} model={{type: func}}
+                                                   name={func.toUpperCase()} color="#0095FF"/>)
+            }
+        });
+        return functionalTasks;
+    };
+
+    const tasks = () => {
+        let workflows = props.workflows || [];
+        let tasks = [];
+
+        workflows.map((wf, i) => {
             let wfObject = {
-                name: "forkTask",
-                taskReferenceName: "forkTaskRef",
-                type: "FORK_JOIN",
-                forkTasks: [],
-                optional: false,
-                startDelay: 0
-            };
-
-            functionalTasks.push(<SideMenuItem id={`functionalNode${i}`} model={{type: func, wfObject}}
-                                      name={func.toUpperCase()} color="#0095FF"/>)
-        }
-        else if (func === "join") {
-            let wfObject = {
-                name: "joinTask",
-                taskReferenceName: "joinTaskRef",
-                type: "JOIN",
-                joinOn: [],
-                optional: false,
-                startDelay: 0
-            };
-
-            functionalTasks.push(<SideMenuItem id={`functionalNode${i}`} model={{type: func, wfObject}}
-                                      name={func.toUpperCase()} color="#0095FF"/>)
-        }
-        else if (func === "decision") {
-            let wfObject = {
-                name: "decisionTask",
-                taskReferenceName: "decisionTaskRef",
-                inputParameters: {
-                    case_value_param: ""
-                },
-                type: "DECISION",
-                caseValueParam: "case_value_param",
-                decisionCases: {
-                    false: [],
-                    true: []
-                },
-                optional: false,
-                startDelay: 0
-            };
-
-            functionalTasks.push(<SideMenuItem id={`functionalNode${i}`} model={{type: func, wfObject}}
-                                      name={func.toUpperCase()} color="#0095FF"/>)
-
-        } else {
-            functionalTasks.push(<SideMenuItem id={`functionalNode${i}`} model={{type: func}}
-                                      name={func.toUpperCase()} color="#0095FF"/>)
-        }
-    });
-    return functionalTasks;
-};
-
-const tasks = (props) => {
-
-    let workflows = props.workflows || [];
-    let tasks = [];
-
-    workflows.map((wf, i) => {
-        let wfObject = {
-            name: wf.name,
-            taskReferenceName: wf.name.toLowerCase().trim()
-                + "_ref_" + Math.random().toString(36).toUpperCase().substr(2, 4),
-            inputParameters: getWfInputsRegex(wf),
-            type: "SUB_WORKFLOW",
-            subWorkflowParam: {
                 name: wf.name,
-                version: wf.version
-            },
-            optional: false,
-            startDelay: 0
-        };
-        return tasks.push(<SideMenuItem key={`wf${i}`} model={{type: "in/out", wfObject, name: wf.name}}
-                                 name={wf.name} color="#0095FF"/>)
-    });
+                taskReferenceName: wf.name.toLowerCase().trim()
+                    + "_ref_" + Math.random().toString(36).toUpperCase().substr(2, 4),
+                inputParameters: getWfInputsRegex(wf),
+                type: "SUB_WORKFLOW",
+                subWorkflowParam: {
+                    name: wf.name,
+                    version: wf.version
+                },
+                optional: false,
+                startDelay: 0
+            };
+            return tasks.push(<SideMenuItem key={`wf${i}`} model={{type: "in/out", wfObject, name: wf.name}}
+                                            name={wf.name} color="#0095FF"/>)
+        });
+        return tasks;
+    };
 
-    return tasks;
+    return (
+        <div>
+            {props.show ?
+                <div
+                    className="tray">{props.category === "Functional" ? functionalTaks(props) : tasks(props)}</div> : null}
+        </div>
+    )
 };
-
-const SideMenu = (props) => (
-    <div>
-        {props.show ?
-            props.category === "Functional" ?
-                <div className="tray">{functionalTaks(props)}</div>
-                :
-                <div className="tray">{tasks(props)}</div>
-            : null}
-    </div>
-);
 
 export default SideMenu;
 
