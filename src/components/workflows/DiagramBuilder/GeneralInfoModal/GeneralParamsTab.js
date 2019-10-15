b35f83fb13e0a9decc5a94515f90ed23fedfc032
Adam Chmara <adam.chmara1@gmail.com>
2019-10-15 13:26:42 +0200

    [Workflow builder] Refactor GeneralParamsTab to functional component & install prettier
    Signed-off-by: Adam Chmara <adam.chmara1@gmail.com>

--- src/components/workflows/DiagramBuilder/GeneralInfoModal/GeneralParamsTab.js
+++ src/components/workflows/DiagramBuilder/GeneralInfoModal/GeneralParamsTab.js
@@ -1,183 +1,205 @@
-import React, { Component } from 'react';
-import {Button, ButtonGroup, Col, Form, InputGroup, Row} from "react-bootstrap";
-import {Typeahead} from 'react-bootstrap-typeahead';
-import {workflowDescriptions} from "../../../constants";
-import {getLabelsFromString} from "../builder-utils";
+import React from "react";
+import {
+  Button,
+  ButtonGroup,
+  Col,
+  Form,
+  InputGroup,
+  Row
+} from "react-bootstrap";
+import { Typeahead } from "react-bootstrap-typeahead";
+import { workflowDescriptions } from "../../../constants";
+import { getLabelsFromString } from "../builder-utils";
 
-class GeneralParamsTab extends Component {
-    constructor(props, context) {
-        super(props, context);
+const GeneralParamsTab = props => {
+  const { isWfNameLocked, isWfNameValid } = props;
+  const hiddenParams = [
+    "name",
+    "description",
+    "schemaVersion",
+    "workflowStatusListenerEnabled",
+    "tasks",
+    "outputParameters",
+    "inputParameters",
+    "updateTime"
+  ];
 
-        this.state = {
-            finalWf: this.props.finalWf,
-            existingLabels: Array.from(this.props.getExistingLabels())
-        };
-
-        this.handleSubmit = this.handleSubmit.bind(this)
+  const handleSubmit = e => {
+    if (e.key === "Enter" || e === "Enter") {
+      props.handleSubmit(e);
     }
+  };
 
-    static getDerivedStateFromProps(props, state) {
-        if (props.finalWf !== state.finalWf) {
-            return {
-                finalWf: props.finalWf
+  const lockedNameField = () => (
+    <Form.Group>
+      <InputGroup size="lg">
+        <InputGroup.Prepend>
+          <InputGroup.Text>
+            <i className="fas fa-lock" />
+            &nbsp;&nbsp;name
+          </InputGroup.Text>
+        </InputGroup.Prepend>
+        <Form.Control
+          disabled
+          type="input"
+          onChange={e => props.handleInput(e.target.value, "name")}
+          value={props.finalWf["name"]}
+        />
+      </InputGroup>
+    </Form.Group>
+  );
+
+  const unlockedNameField = () => (
+    <Form.Group>
+      <InputGroup size="lg">
+        <InputGroup.Prepend>
+          <InputGroup.Text>name:</InputGroup.Text>
+        </InputGroup.Prepend>
+        <Form.Control
+          isValid={isWfNameValid}
+          isInvalid={!isWfNameValid}
+          type="input"
+          onChange={e => props.handleInput(e.target.value, "name")}
+          value={props.finalWf["name"]}
+        />
+        <Form.Control.Feedback type={isWfNameValid ? "valid" : "invalid"}>
+          {isWfNameValid
+            ? "unique name of workflow"
+            : props.finalWf["description"].length < 1
+            ? "unique name of workflow"
+            : "workflow with this name already exits"}
+        </Form.Control.Feedback>
+      </InputGroup>
+    </Form.Group>
+  );
+
+  const description = () => {
+    const desc = props.finalWf["description"].split(" - ")[0];
+    const labels = getLabelsFromString(props.finalWf["description"]);
+    const existingLabels = Array.from(props.getExistingLabels());
+
+    return (
+      <Form.Group>
+        <InputGroup style={{ marginBottom: "8px" }}>
+          <InputGroup.Prepend>
+            <InputGroup.Text>description:</InputGroup.Text>
+          </InputGroup.Prepend>
+          <Form.Control
+            type="input"
+            onChange={e =>
+              props.handleInput(e.target.value + " - " + labels, "description")
             }
-        }
-        return null
-    }
-
-    handleSubmit(e) {
-        if (e.key === "Enter" || e === "Enter") {
-            this.props.handleSubmit(e)
-        }
-    }
-
-    render() {
-        let hiddenParams = ["name", "description", "schemaVersion", "workflowStatusListenerEnabled", "tasks", "outputParameters", "inputParameters", "updateTime"];
-        let nameFieldValue = this.state.finalWf["name"];
-        let descFieldValue = this.state.finalWf["description"];
-        let {isWfNameLocked, isWfNameValid} = this.props;
-
-        const lockedNameField = (nameFieldValue) => (
-            <Form.Group>
-                <InputGroup size="lg">
-                    <InputGroup.Prepend>
-                        <InputGroup.Text><i className="fas fa-lock"/>&nbsp;&nbsp;name</InputGroup.Text>
-                    </InputGroup.Prepend>
-                    <Form.Control
-                        disabled
-                        type="input"
-                        onChange={(e) => this.props.handleInput(e.target.value, "name")}
-                        value={nameFieldValue}/>
-                </InputGroup>
-            </Form.Group>
-        );
-
-        const unlockedNameField = (nameFieldValue) => (
-            <Form.Group>
-                <InputGroup size="lg">
-                    <InputGroup.Prepend>
-                        <InputGroup.Text>name:</InputGroup.Text>
-                    </InputGroup.Prepend>
-                    <Form.Control
-                        isValid={isWfNameValid}
-                        isInvalid={!isWfNameValid}
-                        type="input"
-                        onChange={(e) => this.props.handleInput(e.target.value, "name")}
-                        value={nameFieldValue}/>
-                    <Form.Control.Feedback type={isWfNameValid ? "valid" : "invalid"}>
-                        {isWfNameValid ?
-                            "unique name of workflow" :
-                            (nameFieldValue.length < 1 ? "unique name of workflow" : "workflow with this name already exits")}
-                    </Form.Control.Feedback>
-                </InputGroup>s\
-            </Form.Group>
-        );
-
-        const description = (descFieldValue) => {
-            let desc = descFieldValue.split('-')[0];
-            let labels = getLabelsFromString(descFieldValue);
-
-            return (
-            <Form.Group>
-                <InputGroup style={{marginBottom: "8px"}}>
-                    <InputGroup.Prepend>
-                        <InputGroup.Text>description:</InputGroup.Text>
-                    </InputGroup.Prepend>
-                    <Form.Control
-                        type="input"
-                        onChange={(e) => this.props.handleInput(e.target.value + "-" + labels, "description")}
-                        value={desc}/>
-                </InputGroup>
-                <Typeahead
-                    allowNew
-                    multiple
-                    clearButton
-                    newSelectionPrefix="Add a new label: "
-                    defaultSelected={labels}
-                    value={labels}
-                    onChange={(e) => this.props.handleInput(
-                        desc + "-" + e.map(item => item.label ? item.label.toUpperCase() : item), "description")}
-                    options={this.state.existingLabels}
-                    placeholder="Add labels..."
-                />
-                <Form.Text className="text-muted">
-                    {workflowDescriptions["description"]}
-                </Form.Text>
-            </Form.Group>
+            value={desc}
+          />
+        </InputGroup>
+        <Typeahead
+          allowNew
+          multiple
+          clearButton
+          newSelectionPrefix="Add a new label: "
+          defaultSelected={labels}
+          value={labels}
+          onChange={e =>
+            props.handleInput(
+              desc +
+                " - " +
+                e.map(item => (item.label ? item.label.toUpperCase() : item)),
+              "description"
             )
-        };
+          }
+          options={existingLabels}
+          placeholder="Add labels..."
+        />
+        <Form.Text className="text-muted">
+          {workflowDescriptions["description"]}
+        </Form.Text>
+      </Form.Group>
+    );
+  };
 
-        const buttonWrappedField = (item, left, right) => (
-            <Form.Group>
-                <InputGroup>
-                    <InputGroup.Prepend>
+  const buttonWrappedField = (item, left, right) => (
+    <Form.Group>
+      <InputGroup>
+        <InputGroup.Prepend>
+          <InputGroup.Text>{item[0]}:</InputGroup.Text>
+        </InputGroup.Prepend>
+        <Form.Control value={item[1]} onChange={() => {}} />
+        <InputGroup.Append>
+          <ButtonGroup>
+            <Button
+              variant="outline-primary"
+              onClick={() => props.handleInput(left[1], item[0])}
+            >
+              {left[0]}
+            </Button>
+            <Button
+              variant="outline-primary"
+              onClick={() => props.handleInput(right[1], item[0])}
+            >
+              {right[0]}
+            </Button>
+          </ButtonGroup>
+        </InputGroup.Append>
+      </InputGroup>
+      <Form.Text className="text-muted">
+        {workflowDescriptions[item[0]]}
+      </Form.Text>
+    </Form.Group>
+  );
+
+  return (
+    <Form onKeyPress={handleSubmit}>
+      {isWfNameLocked ? lockedNameField() : unlockedNameField()}
+      {description()}
+      <Row>
+        {Object.entries(props.finalWf).map((item, i) => {
+          if (!hiddenParams.includes(item[0])) {
+            if (item[0] === "version") {
+              return (
+                <Col sm={6} key={`col2-${i}`}>
+                  {buttonWrappedField(
+                    item,
+                    ["-", item[1] - 1],
+                    ["+", item[1] + 1]
+                  )}
+                </Col>
+              );
+            }
+            if (item[0] === "restartable") {
+              return (
+                <Col sm={6} key={`col2-${i}`}>
+                  {buttonWrappedField(item, ["<", !item[1]], [">", !item[1]])}
+                </Col>
+              );
+            } else {
+              return (
+                <Col sm={6} key={`col3-${i}`}>
+                  <Form.Group>
+                    <InputGroup>
+                      <InputGroup.Prepend>
                         <InputGroup.Text>{item[0]}:</InputGroup.Text>
-                    </InputGroup.Prepend>
-                    <Form.Control
-                        value={item[1]} onChange={()=>{}}/>
-                    <InputGroup.Append>
-                        <ButtonGroup>
-                        <Button variant="outline-primary"
-                                onClick={() => this.props.handleInput(left[1], item[0])}>{left[0]}</Button>
-                        <Button variant="outline-primary"
-                                onClick={() => this.props.handleInput(right[1], item[0])}>{right[0]}</Button>
-                        </ButtonGroup>
-                    </InputGroup.Append>
-                </InputGroup>
-                <Form.Text className="text-muted">
-                    {workflowDescriptions[item[0]]}
-                </Form.Text>
-            </Form.Group>
-
-        );
-
-        return (
-            <Form onKeyPress={this.handleSubmit}>
-                {isWfNameLocked ? lockedNameField(nameFieldValue) : unlockedNameField(nameFieldValue)}
-                {description(descFieldValue)}
-                <Row>
-                    {Object.entries(this.state.finalWf).map((item, i) => {
-                        if (!hiddenParams.includes(item[0])) {
-                            if (item[0] === "version") {
-                                return (
-                                    <Col sm={6} key={`col2-${i}`}>
-                                        {buttonWrappedField(item, ["-", item[1] - 1], ["+", item[1] + 1])}
-                                    </Col>
-                                )
-                            }
-                            if (item[0] === "restartable") {
-                                return (
-                                    <Col sm={6} key={`col2-${i}`}>
-                                        {buttonWrappedField(item, ["<", !item[1]], [">", !item[1]])}
-                                    </Col>
-                                )
-                            } else {
-                                return (
-                                    <Col sm={6} key={`col3-${i}`}>
-                                        <Form.Group>
-                                            <InputGroup>
-                                                <InputGroup.Prepend>
-                                                    <InputGroup.Text>{item[0]}:</InputGroup.Text>
-                                                </InputGroup.Prepend>
-                                                <Form.Control
-                                                    type="input"
-                                                    onChange={(e) => this.props.handleInput(e.target.value, item[0])}
-                                                    value={item[1]}/>
-                                                <Form.Text className="text-muted">
-                                                    {workflowDescriptions[item[0]]}
-                                                </Form.Text>
-                                            </InputGroup>
-                                        </Form.Group>
-                                    </Col>
-                                )
-                            }
+                      </InputGroup.Prepend>
+                      <Form.Control
+                        type="input"
+                        onChange={e =>
+                          props.handleInput(e.target.value, item[0])
                         }
-                        return null;
-                    })}
-                </Row>
-            </Form>
-        );
-    }
-}
+                        value={item[1]}
+                      />
+                      <Form.Text className="text-muted">
+                        {workflowDescriptions[item[0]]}
+                      </Form.Text>
+                    </InputGroup>
+                  </Form.Group>
+                </Col>
+              );
+            }
+          }
+          return null;
+        })}
+      </Row>
+    </Form>
+  );
+};
 
 export default GeneralParamsTab;
