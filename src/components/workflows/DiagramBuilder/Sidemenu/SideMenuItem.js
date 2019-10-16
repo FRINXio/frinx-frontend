1eebf8e9a02f241ab1a5421e7a56971c3e4a303f
Tomas <thodor@frinx.io>
2019-10-16 12:24:41 +0200

    Create WfLabel component
    Signed-off-by: Tomas <thodor@frinx.io>

--- src/components/workflows/DiagramBuilder/Sidemenu/SideMenuItem.js
+++ src/components/workflows/DiagramBuilder/Sidemenu/SideMenuItem.js
@@ -1,38 +1,47 @@
 import React from "react";
-import {wfLabelsColor} from "../../../constants";
-import {getLabelsFromString} from "../builder-utils";
+import { getLabelsFromString } from "../builder-utils";
+import WfLabels from "../../../common/WfLabels";
 
-const SideMenuItem = (props) => {
+const SideMenuItem = props => {
+  let description = null;
+  let labels = [];
+  let version = null;
 
-    let description = null;
-    let labels = [];
-    let version = null;
+  if (props.model.description) {
+    description = props.model.description.split("-")[0];
+    labels = getLabelsFromString(props.model.description);
+    version = props.model.wfObject.subWorkflowParam.version;
+  }
 
-    if (props.model.description) {
-        description = props.model.description.split('-')[0];
-        labels = getLabelsFromString(props.model.description);
-        version = props.model.wfObject.subWorkflowParam.version;
-    }
-
-    return (
-    <div draggable={true} onDragStart={e => {e.dataTransfer.setData("storm-diagram-node", JSON.stringify(props.model));}}
-         className="tray-item">
-        <div title={props.name} className='tray-item-name'>
-            <b>{props.name}</b>
+  return (
+    <div
+      draggable={true}
+      onDragStart={e => {
+        e.dataTransfer.setData(
+          "storm-diagram-node",
+          JSON.stringify(props.model)
+        );
+      }}
+      className="tray-item"
+    >
+      <div title={props.name} className="tray-item-name">
+        <b>{props.name}</b>
+      </div>
+      <div className="tray-item-content">
+        <div className="tray-item-description">
+          <b>version {version}</b>
+          {description ? (
+            `- ${description}`
+          ) : (
+            <i> - no description available</i>
+          )}
         </div>
-        <div className='tray-item-content'>
-            <div className='tray-item-description'>
-                <b>version {version}</b>{description ? `- ${description}` : <i> - no description available</i>}
-            </div>
-            {labels.map((label,i) => {
-                let color = i >= wfLabelsColor.length ? wfLabelsColor[0] : wfLabelsColor[i];
-                return (
-                    <div className='wfLabel' style={{backgroundColor: color}}>{label}</div>
-                )
-            })}
-        </div>
+        {labels.map((label, i) => {
+          return <WfLabels key={label} label={label} index={i} />;
+        })}
+      </div>
     </div>
-    )
+  );
 };
 
-export default SideMenuItem
+export default SideMenuItem;
