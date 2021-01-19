057b1509e58354123727c2b3af5d4972e68c9c11
Pavol Porubsky <paulooze@gmail.com>
2021-01-19 14:06:56 +0100

    handle peerDeps; update import in dashboard

--- packages/frinx-workflow-ui/webpack.config.js
+++ packages/frinx-workflow-ui/webpack.config.js
@@ -86,7 +86,26 @@ module.exports = {
   externals: isDev
     ? undefined
     : {
-        react: 'react',
-        reactDOM: 'react-dom',
+        react: {
+          root: 'React',
+          commonjs2: 'react',
+          commonjs: 'react',
+          amd: 'react',
+          umd: 'react',
+        },
+        'react-dom': {
+          root: 'ReactDOM',
+          commonjs2: 'react-dom',
+          commonjs: 'react-dom',
+          amd: 'react-dom',
+          umd: 'react-dom',
+        },
+        'react-router-dom': {
+          root: 'ReactRouterDOM',
+          commonjs2: 'react-router-dom',
+          commonjs: 'react-router-dom',
+          amd: 'react-router-dom',
+          umd: 'react-router-dom',
+        },
       },
 };
