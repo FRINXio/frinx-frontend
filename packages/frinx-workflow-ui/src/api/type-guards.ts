c833b1a6d5d4405362e015db7c417a075ca1cf71
Adam Chmara <adam.chmara1@gmail.com>
2021-02-02 13:36:52 +0100

    Change type-guards, minor fixes
    Signed-off-by: Adam Chmara <adam.chmara1@gmail.com>

--- packages/frinx-workflow-ui/src/api/type-guards.ts
+++ packages/frinx-workflow-ui/src/api/type-guards.ts
@@ -1,16 +1,31 @@
-import { Workflow, TaskDefinition, Queue, WorkflowPayload, EventListener } from 'helpers/types';
+import { Workflow, TaskDefinition, Queue, WorkflowPayload, EventListener } from '../helpers/types';
 
 // TODO more strict in the future
 
 export function isWorkflow(workflow: unknown): workflow is Workflow {
-  return typeof (workflow as Workflow).name === 'string' && typeof (workflow as Workflow).version === 'number';
+  if (workflow !== null && typeof workflow === 'object') {
+    return (
+      'name' in workflow! &&
+      typeof workflow['name'] === 'string' &&
+      'version' in workflow! &&
+      typeof workflow['version'] === 'number'
+    );
+  }
+
+  return false;
 }
 
 export function isTaskDefinition(taskDefinition: unknown): taskDefinition is TaskDefinition {
-  return (
-    typeof (taskDefinition as TaskDefinition).name === 'string' &&
-    typeof (taskDefinition as TaskDefinition).timeoutPolicy === 'string'
-  );
+  if (taskDefinition !== null && typeof taskDefinition === 'object') {
+    return (
+      'name' in taskDefinition! &&
+      typeof taskDefinition['name'] === 'string' &&
+      'timeoutPolicy' in taskDefinition! &&
+      typeof taskDefinition['timeoutPolicy'] === 'number'
+    );
+  }
+
+  return false;
 }
 
 export function isArrayTypeOf<T>(array: unknown, testFunc: (value: unknown) => value is T): array is T[] {
@@ -18,12 +33,27 @@ export function isArrayTypeOf<T>(array: unknown, testFunc: (value: unknown) => v
 }
 
 export function isEventListener(eventListener: unknown): eventListener is EventListener {
-  return (
-    typeof (eventListener as EventListener).name === 'string' &&
-    typeof (eventListener as EventListener).event === 'string'
-  );
+  if (eventListener !== null && typeof eventListener === 'object') {
+    return (
+      'name' in eventListener! &&
+      typeof eventListener['name'] === 'string' &&
+      'event' in eventListener! &&
+      typeof eventListener['event'] === 'string'
+    );
+  }
+
+  return false;
 }
 
 export function isQueue(queue: unknown): queue is Queue {
-  return typeof (queue as Queue).queueName === 'string' && typeof (queue as Queue).qsize === 'number';
+  if (queue !== null && typeof queue === 'object') {
+    return (
+      'queueName' in queue! &&
+      typeof queue['queueName'] === 'string' &&
+      'qsize' in queue! &&
+      typeof queue['qsize'] === 'number'
+    );
+  }
+
+  return false;
 }
