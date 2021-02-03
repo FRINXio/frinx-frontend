60f659c9a9640576a1d2527877c701346adee327
Adam Chmara <achmara@frinx.io>
2021-02-03 15:41:25 +0100

    Uniflow API export, typechecking (#15)
    * Exporting API calls
    
    Signed-off-by: Adam Chmara <adam.chmara1@gmail.com>
    
    * Added more types and requests
    
    Signed-off-by: Adam Chmara <adam.chmara1@gmail.com>
    
    * Add type guards
    
    Signed-off-by: Adam Chmara <adam.chmara1@gmail.com>
    
    * Minor fixes in API
    
    Signed-off-by: Adam Chmara <adam.chmara1@gmail.com>
    
    * Move api from dashboard to frinx-workflow-ui
    
    Signed-off-by: Adam Chmara <adam.chmara1@gmail.com>
    
    * Move helpers from dashboard to frinx-workflow-ui
    
    Signed-off-by: Adam Chmara <adam.chmara1@gmail.com>
    
    * Change type-guards, minor fixes
    
    Signed-off-by: Adam Chmara <adam.chmara1@gmail.com>
    
    * Move API and types to dashboard
    
    Signed-off-by: Adam Chmara <adam.chmara1@gmail.com>

---
+++ packages/frinx-dashboard/src/api/uniflow/api-helpers.ts
@@ -0,0 +1,44 @@
+// TODO dynamic source of url
+const CONDUCTOR_API_URL: string = 'localhost:8080/api';
+
+export async function apiFetch(path: string, options: RequestInit): Promise<unknown> {
+  const url = `${CONDUCTOR_API_URL}${path}`;
+  const response = await fetch(url, options);
+
+  if (!response.ok) {
+    throw new Error(`apiFetch failed with http-code ${response.status}`);
+  }
+
+  return response.json();
+}
+
+export async function sendGetRequest(path: string): Promise<unknown> {
+  const options = {
+    method: 'GET',
+  };
+  return apiFetch(path, options);
+}
+
+export async function sendPostRequest(path: string, body: unknown): Promise<unknown> {
+  const options = {
+    method: 'POST',
+    body: JSON.stringify(body),
+  };
+  return apiFetch(path, options);
+}
+
+export async function sendPutRequest(path: string, body: unknown): Promise<unknown> {
+  const options = {
+    method: 'PUT',
+    body: JSON.stringify(body),
+  };
+  return apiFetch(path, options);
+}
+
+export async function sendDeleteRequest(path: string, body?: unknown): Promise<unknown> {
+  const options = {
+    method: 'DELETE',
+    body: JSON.stringify(body),
+  };
+  return apiFetch(path, options);
+}
