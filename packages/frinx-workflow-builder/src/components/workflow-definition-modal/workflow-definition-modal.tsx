693e387ba91ecca8f20e20b50dc2647f6f5269c9
Adam Chmara <adam.chmara1@gmail.com>
2021-03-30 16:17:27 +0200

    Import reorder
    Signed-off-by: Adam Chmara <adam.chmara1@gmail.com>

--- packages/frinx-workflow-builder/src/components/workflow-definition-modal/workflow-definition-modal.tsx
+++ packages/frinx-workflow-builder/src/components/workflow-definition-modal/workflow-definition-modal.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react';
+import AceEditor from 'react-ace';
 import { Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay } from '@chakra-ui/react';
 import { Workflow } from '../../helpers/types';
-import AceEditor from 'react-ace';
 import 'ace-builds/src-noconflict/mode-json';
 import 'ace-builds/src-noconflict/theme-textmate';
 import 'ace-builds/src-noconflict/ext-language_tools';
@@ -20,16 +20,16 @@ const WorkflowDefinitionModal: FC<Props> = ({ isOpen, onClose, workflow }) => {
         <ModalHeader>Workflow definition</ModalHeader>
         <ModalCloseButton />
         <ModalBody>
-        <AceEditor
-          readOnly
-          mode="json"
-          theme="textmate"
-          wrapEnabled
-          value={JSON.stringify(workflow, null, 2)}
-          fontSize={16}
-          tabSize={2}
-          width="100%"
-        />
+          <AceEditor
+            readOnly
+            mode="json"
+            theme="textmate"
+            wrapEnabled
+            value={JSON.stringify(workflow, null, 2)}
+            fontSize={16}
+            tabSize={2}
+            width="100%"
+          />
         </ModalBody>
       </ModalContent>
     </Modal>
