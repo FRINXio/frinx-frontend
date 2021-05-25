04f12b2908c798e87b0d1e033ac9e2f3f783035d
Pavol Porubsky <paulooze@gmail.com>
2021-05-25 13:24:54 +0200

    add description to workflow/task list in wf-builder; add search to left-menu in wf-builder

---
+++ packages/frinx-workflow-builder/src/components/left-menu/left-menu.tsx
@@ -0,0 +1,65 @@
+import React, { FC, memo } from 'react';
+import { Box, Flex, Heading, IconButton, Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react';
+import { AddIcon } from '@chakra-ui/icons';
+import { ExtendedTask, TaskDefinition, Workflow } from '../../helpers/types';
+import { createSystemTasks, createTask } from '../../helpers/task.helpers';
+import TaskList from './task-list';
+import WorkflowList from './workflow-list';
+
+type Props = {
+  onTaskAdd: (task: ExtendedTask) => void;
+  workflows: Workflow[];
+  taskDefinitions: TaskDefinition[];
+};
+
+const LeftMenu: FC<Props> = memo(({ onTaskAdd, workflows, taskDefinitions }) => {
+  return (
+    <Box background="white" boxShadow="base" px={4} py={10} height="100%">
+      <Tabs display="flex" flexDirection="column" height="100%" isLazy>
+        <TabList>
+          <Tab>System tasks</Tab>
+          <Tab>Tasks</Tab>
+          <Tab>Workflows</Tab>
+        </TabList>
+        <TabPanels flex={1} overflowY="auto">
+          <TabPanel py={6}>
+            {createSystemTasks().map((label) => (
+              <Flex
+                key={label}
+                alignItems="center"
+                height={16}
+                border="1px"
+                borderColor="gray.200"
+                px={4}
+                my={4}
+                borderRadius="md"
+                userSelect="none"
+              >
+                <Heading as="h4" size="xs">
+                  {label}
+                </Heading>
+                <Box marginLeft="auto">
+                  <IconButton
+                    aria-label="Add task"
+                    icon={<AddIcon />}
+                    onClick={() => {
+                      onTaskAdd(createTask(label));
+                    }}
+                  />
+                </Box>
+              </Flex>
+            ))}
+          </TabPanel>
+          <TabPanel>
+            <TaskList onTaskAdd={onTaskAdd} taskDefinitions={taskDefinitions} />
+          </TabPanel>
+          <TabPanel>
+            <WorkflowList onTaskAdd={onTaskAdd} workflows={workflows} />
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </Box>
+  );
+});
+
+export default LeftMenu;
