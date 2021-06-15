dcaaee2d1540ade05847f70cbcd33028b62fe10f
Pavol Porubsky <paulooze@gmail.com>
2021-06-15 10:10:11 +0200

    improve table design

--- packages/frinx-inventory-client/src/index-dev.ts
+++ packages/frinx-inventory-client/src/index-dev.ts
@@ -1,6 +1,6 @@
 import { createElement } from 'react';
 import { render } from 'react-dom';
-import DeviceList from './pages/device-list/device-list';
+import Root from './root';
 
 const mountElement = document.querySelector('#root');
 
@@ -8,4 +8,4 @@ if (mountElement == null) {
   throw new Error('#root element not found');
 }
 
-render(createElement(DeviceList), mountElement);
+render(createElement(Root), mountElement);
