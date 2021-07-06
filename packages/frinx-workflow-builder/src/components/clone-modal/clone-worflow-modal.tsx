185dc3f846c36bd1b6d49168f541df0f460cebd2
marcomruz <marcomruz1@gmail.com>
2021-07-06 14:25:10 +0200

    add clone workflow modal

---
+++ packages/frinx-workflow-builder/src/components/clone-modal/clone-worflow-modal.tsx
@@ -0,0 +1,93 @@
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+  Button,
+  FormControl,
+  FormErrorMessage,
+  Input,
+  useToast,
+} from '@chakra-ui/react';
+import { isEmpty } from 'lodash';
+import React, { FC, FormEvent, useRef, useState } from 'react';
+
+type Props = {
+  onWorkflowClone: (name: string) => void;
+  isOpen: boolean;
+  onClose: () => void;
+};
+
+const CloneWorkflowModal: FC<Props> = ({ onWorkflowClone, isOpen, onClose }) => {
+  const [wfNameError, setWfNameError] = useState<string | null>(null);
+  const toast = useToast();
+
+  const cloneInputRef = useRef<HTMLInputElement | null>(null);
+  const cancelCloneRef = useRef();
+
+  const handleCloneModalClose = () => {
+    setWfNameError(null);
+    onClose();
+  };
+
+  const handleCloneWorkflow = (e: FormEvent = {} as FormEvent) => {
+    if (!isEmpty(e)) {
+      e.preventDefault();
+    }
+
+    const workflowName = cloneInputRef.current?.value;
+
+    if (!workflowName) {
+      setWfNameError('Please enter name of workflow');
+      return;
+    }
+
+    try {
+      onWorkflowClone(workflowName);
+      handleCloneModalClose();
+
+      toast({
+        duration: 2000,
+        title: `Succesfully saved workflow as ${workflowName}`,
+        isClosable: true,
+        status: 'success',
+      });
+    } catch (err) {
+      toast({ duration: 2000, title: 'There was a problem with saving workflow', isClosable: true, status: 'error' });
+    }
+  };
+
+  return (
+    <AlertDialog
+      isOpen={isOpen}
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      leastDestructiveRef={cancelCloneRef}
+      onClose={handleCloneModalClose}
+    >
+      <AlertDialogOverlay>
+        <AlertDialogContent>
+          <AlertDialogHeader>Clone Workflow</AlertDialogHeader>
+          <AlertDialogBody>
+            <form onSubmit={handleCloneWorkflow}>
+              <FormControl isInvalid={wfNameError !== null}>
+                <Input placeholder="Please enter name of workflow" ref={cloneInputRef} />
+                <FormErrorMessage>{wfNameError}</FormErrorMessage>
+              </FormControl>
+            </form>
+          </AlertDialogBody>
+          <AlertDialogFooter>
+            <Button onClick={handleCloneModalClose}>Cancel</Button>
+            <Button colorScheme="blue" ml={4} onClick={handleCloneWorkflow}>
+              Clone
+            </Button>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialogOverlay>
+    </AlertDialog>
+  );
+};
+
+export default CloneWorkflowModal;
