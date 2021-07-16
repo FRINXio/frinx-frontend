b9ddad37a209387829acb4961ff34a109d404f8c
marcomruz <marcomruz1@gmail.com>
2021-07-16 16:26:23 +0200

    add vlan form template to uniresource & create path to this form

--- packages/frinx-uniresource-ui/src/index.ts
+++ packages/frinx-uniresource-ui/src/index.ts
@@ -8,6 +8,7 @@ import CreateNestedPool from './components/create-nested-pool';
 // import CreateNewStrategy from './components/create-new-strategy';
 import CreateStrategyPage from './pages/create-strategy-page/create-strategy-page';
 import CreateAllocatingIpv4PrefixPoolPage from './pages/create-pool-page/create-allocating-ipv4_prefix-pool-page';
+import CreateAllocatingVlanPoolPage from './pages/create-pool-page/create-allocating-vlan-pool-page';
 
 export { Root as UniresourceAppProvider };
 export { PoolsPage };
@@ -17,3 +18,4 @@ export { ResourceTypesList };
 export { CreateNestedPool };
 export { CreateStrategyPage };
 export { CreateAllocatingIpv4PrefixPoolPage };
+export { CreateAllocatingVlanPoolPage };
