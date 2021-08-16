e56bf73cc4825609558c3573e4856a55551dcbe9
marcomruz <marcomruz1@gmail.com>
2021-08-16 12:02:28 +0200

    add device data store page in device inventory

--- packages/frinx-inventory-client/src/index.ts
+++ packages/frinx-inventory-client/src/index.ts
@@ -1,6 +1,7 @@
 import DeviceList from './pages/device-list/device-list';
 import CreateDevicePage from './pages/create-device/create-device-page';
 import { InventoryAPIProvider } from './inventory-api-provider';
+import DeviceConfig from './pages/device-config/device-config';
 
-export { DeviceList, CreateDevicePage };
+export { DeviceList, CreateDevicePage, DeviceConfig };
 export { InventoryAPIProvider };
