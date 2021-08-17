77eaff1b2c0369aa1328cdb05032fd4de2cf999a
Marco Mruz <marcomruz1@gmail.com>
2021-08-17 17:02:43 +0200

    add create snapshot & reset config mutations to config screen in device inventory ui

--- packages/frinx-inventory-client/src/pages/device-config/device-config-page.tsx
+++ packages/frinx-inventory-client/src/pages/device-config/device-config-page.tsx
@@ -9,7 +9,19 @@ import {
   Flex,
   Spacer,
   useToast,
+  useDisclosure,
   HStack,
+  FormControl,
+  FormLabel,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  FormErrorMessage,
 } from '@chakra-ui/react';
 import AceEditor from 'react-ace';
 import React, { FC, useEffect, useState } from 'react';
@@ -21,6 +33,10 @@ import {
   QueryDataStoreQueryVariables,
   UpdateDataStoreMutation,
   UpdateDataStoreMutationVariables,
+  ResetConfigMutation,
+  ResetConfigMutationVariables,
+  AddSnapshotMutation,
+  AddSnapshotMutationVariables,
 } from '../../__generated__/graphql';
 
 const DATA_STORE_QUERY = gql`
@@ -51,6 +67,27 @@ const COMMIT_DATA_STORE_MUTATION = gql`
   }
 `;
 
+const RESET_CONFIG_MUTATION = gql`
+  mutation resetConfig($deviceId: String!) {
+    resetConfig(deviceId: $deviceId) {
+      dataStore {
+        config
+        operational
+      }
+    }
+  }
+`;
+
+const ADD_SNAPSHOT_MUTATION = gql`
+  mutation addSnapshot($input: AddSnapshotInput!) {
+    addSnapshot(input: $input) {
+      snapshot {
+        name
+      }
+    }
+  }
+`;
+
 type Props = {
   deviceId: string;
 };
@@ -66,8 +103,14 @@ const DeviceConfig: FC<Props> = ({ deviceId }) => {
   const [, commitConfig] = useMutation<CommitDataStoreConfigMutation, CommitDataStoreConfigMutationVariables>(
     COMMIT_DATA_STORE_MUTATION,
   );
+  const [, resetConfig] = useMutation<ResetConfigMutation, ResetConfigMutationVariables>(RESET_CONFIG_MUTATION);
+  const [, addSnapshot] = useMutation<AddSnapshotMutation, AddSnapshotMutationVariables>(ADD_SNAPSHOT_MUTATION);
+
   const [config, setConfig] = useState<string>();
+  const [snapshotName, setSnapshotName] = useState<string>('');
+  const [isSnapshotNameInvalid, setIsSnapshotNameInvalid] = useState(false);
   const toast = useToast();
+  const { isOpen, onClose, onOpen } = useDisclosure();
 
   useEffect(() => {
     if (data != null && data.dataStore != null) {
@@ -103,9 +146,9 @@ const DeviceConfig: FC<Props> = ({ deviceId }) => {
   };
 
   const handleOnCommitConfig = async () => {
-    const { error: responseError } = await commitConfig({ deviceId });
+    const { data: responseData } = await commitConfig({ deviceId });
 
-    if (responseError != null) {
+    if (responseData?.commitConfig.isOk) {
       toast({
         duration: 2000,
         isClosable: true,
@@ -122,6 +165,55 @@ const DeviceConfig: FC<Props> = ({ deviceId }) => {
     }
   };
 
+  const handleOnResetConfig = async () => {
+    const { data: responseData } = await resetConfig({ deviceId });
+    if (responseData != null) {
+      toast({
+        duration: 2000,
+        isClosable: true,
+        status: 'success',
+        title: 'Successfully reset config data store',
+      });
+
+      executeQuery();
+    } else {
+      toast({
+        duration: 2000,
+        isClosable: true,
+        status: 'error',
+        title: 'Failed to reset config data store',
+      });
+    }
+  };
+
+  const handleOnAddSnapshot = async () => {
+    if (snapshotName.length === 0) {
+      setIsSnapshotNameInvalid(true);
+      return;
+    }
+
+    const { error: responseError } = await addSnapshot({ input: { name: snapshotName, deviceId } });
+    if (responseError != null) {
+      toast({
+        duration: 2000,
+        isClosable: true,
+        status: 'success',
+        title: 'Successfully created snapshot',
+      });
+    } else {
+      toast({
+        duration: 2000,
+        isClosable: true,
+        status: 'error',
+        title: 'Failed to create snapshot',
+      });
+    }
+  };
+
+  const handleChangeSnapshotName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSnapshotName(e.target.value);
+  };
+
   if (fetching) {
     return <Progress size="xs" isIndeterminate mt={-10} />;
   }
@@ -131,49 +223,91 @@ const DeviceConfig: FC<Props> = ({ deviceId }) => {
   }
 
   return (
-    <Container maxWidth={1280}>
-      <Grid templateColumns="repeat(2, 1fr)" gap={4}>
-        <GridItem w="100%">
-          <Flex>
+    <>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Create snapshot</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <FormControl isInvalid={isSnapshotNameInvalid}>
+              <FormLabel>Snapshot name</FormLabel>
+              <Input placeholder="Snapshot name" onChange={handleChangeSnapshotName} />
+              <FormErrorMessage>Please enter snapshot name</FormErrorMessage>
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={handleOnAddSnapshot}>
+              Create
+            </Button>
+            <Button
+              onClick={() => {
+                onClose();
+                setIsSnapshotNameInvalid(false);
+              }}
+            >
+              Cancel
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+
+      <Container maxWidth={1280}>
+        <Box my={2}>
+          <HStack>
+            <Button colorScheme="blue" onClick={onOpen}>
+              Create snapshot
+            </Button>
+            <Button colorScheme="blue" onClick={handleOnResetConfig}>
+              Reset config
+            </Button>
+          </HStack>
+        </Box>
+
+        <Grid templateColumns="repeat(2, 1fr)" gap={4}>
+          <GridItem w="100%">
+            <Flex>
+              <Box my={4}>
+                <Text>Config data store</Text>
+              </Box>
+              <Spacer />
+              <Box my={2}>
+                <HStack>
+                  <Button colorScheme="blue" onClick={handleOnSaveConfig}>
+                    Save
+                  </Button>
+                  <Button colorScheme="blue" onClick={handleOnCommitConfig}>
+                    Commit
+                  </Button>
+                </HStack>
+              </Box>
+            </Flex>
+            <AceEditor
+              width="100%"
+              mode="json"
+              value={config}
+              theme="tomorrow"
+              onChange={(val) => {
+                setConfig(val);
+              }}
+            />
+          </GridItem>
+          <GridItem w="100%">
             <Box my={4}>
-              <Text>Config data store</Text>
+              <Text>Operational data store</Text>
             </Box>
-            <Spacer />
-            <Box my={2}>
-              <HStack>
-                <Button colorScheme="blue" onClick={handleOnSaveConfig}>
-                  Save
-                </Button>
-                <Button colorScheme="blue" onClick={handleOnCommitConfig}>
-                  Commit
-                </Button>
-              </HStack>
-            </Box>
-          </Flex>
-          <AceEditor
-            width="100%"
-            mode="json"
-            value={config}
-            theme="tomorrow"
-            onChange={(val) => {
-              setConfig(val);
-            }}
-          />
-        </GridItem>
-        <GridItem w="100%">
-          <Box my={4}>
-            <Text>Operational data store</Text>
-          </Box>
-          <AceEditor
-            width="100%"
-            mode="json"
-            value={JSON.stringify(JSON.parse(data?.dataStore?.operational ?? ''), null, 2)}
-            theme="tomorrow"
-            readOnly
-          />
-        </GridItem>
-      </Grid>
-    </Container>
+            <AceEditor
+              width="100%"
+              mode="json"
+              value={JSON.stringify(JSON.parse(data?.dataStore?.operational ?? ''), null, 2)}
+              theme="tomorrow"
+              readOnly
+            />
+          </GridItem>
+        </Grid>
+      </Container>
+    </>
   );
 };
 
