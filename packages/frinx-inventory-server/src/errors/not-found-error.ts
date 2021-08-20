f8e79e3a363167826cf599d156be06ee859b3449
Pavol Porubsky <paulooze@gmail.com>
2021-08-20 16:53:43 +0200

    update react version; remove forgotten inventory-server

--- packages/frinx-inventory-server/src/errors/not-found-error.ts
+++
@@ -1,8 +0,0 @@
-import APIError from './api-error';
-import { HttpStatusCode } from './base-error';
-
-export default class NotFoundError extends APIError {
-  constructor(message: string) {
-    super('NOT FOUND', HttpStatusCode.NOT_FOUND, true, message);
-  }
-}
--- packages/frinx-inventory-server/src/is-dev.ts
+++
@@ -1,9 +0,0 @@
-const ENVIRONMENT = process.env.NODE_ENV || 'development';
-const isProd = ENVIRONMENT === 'production';
-const isDev = ENVIRONMENT === 'development';
-
-if (!(isProd || isDev)) {
-  throw new Error('server: isProd or isDev has to be true');
-}
-
-export default isDev;
