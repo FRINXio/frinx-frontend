d911f67861eac6c8e58d5f04e09ae7a2f958eac3
Pavol Porubsky <paulooze@gmail.com>
2021-08-20 13:11:37 +0200

    redesign config screen; add more mutations/queries

--- packages/frinx-inventory-client/src/pages/device-config/device-config-page.tsx
+++ packages/frinx-inventory-client/src/pages/device-config/device-config-page.tsx
@@ -1,31 +1,9 @@
-import {
-  Container,
-  Grid,
-  GridItem,
-  Text,
-  Progress,
-  Button,
-  Box,
-  Flex,
-  Spacer,
-  useToast,
-  useDisclosure,
-  HStack,
-  FormControl,
-  FormLabel,
-  Input,
-  Modal,
-  ModalBody,
-  ModalCloseButton,
-  ModalContent,
-  ModalFooter,
-  ModalHeader,
-  ModalOverlay,
-  FormErrorMessage,
-} from '@chakra-ui/react';
-import AceEditor from 'react-ace';
+import { Container, Progress, useToast, useDisclosure, Box } from '@chakra-ui/react';
 import React, { FC, useEffect, useState } from 'react';
 import { useQuery, gql, useMutation } from 'urql';
+import DeviceConfigEditors from './device-config-editors';
+import DeviceConfigActions from './device-config-actions';
+import CreateSnapshotModal from './create-snapshot-modal';
 import {
   CommitDataStoreConfigMutation,
   CommitDataStoreConfigMutationVariables,
@@ -37,13 +15,20 @@ import {
   ResetConfigMutationVariables,
   AddSnapshotMutation,
   AddSnapshotMutationVariables,
+  ApplySnapshotMutation,
+  ApplySnapshotMutationVariables,
 } from '../../__generated__/graphql';
+import DiffOutputModal from './diff-output-modal';
 
 const DATA_STORE_QUERY = gql`
   query queryDataStore($deviceId: String!) {
     dataStore(deviceId: $deviceId) {
       config
       operational
+      snapshots {
+        name
+        createdAt
+      }
     }
   }
 `;
@@ -60,8 +45,8 @@ const UPDATE_DATA_STORE_MUTATION = gql`
 `;
 
 const COMMIT_DATA_STORE_MUTATION = gql`
-  mutation commitDataStoreConfig($deviceId: String!) {
-    commitConfig(deviceId: $deviceId) {
+  mutation commitDataStoreConfig($input: CommitConfigInput!) {
+    commitConfig(input: $input) {
       isOk
     }
   }
@@ -88,12 +73,21 @@ const ADD_SNAPSHOT_MUTATION = gql`
   }
 `;
 
+const APPLY_SNAPSHOT_MUTATION = gql`
+  mutation applySnapshot($input: ApplySnapshotInput!) {
+    applySnapshot(input: $input) {
+      isOk
+      output
+    }
+  }
+`;
+
 type Props = {
   deviceId: string;
 };
 
 const DeviceConfig: FC<Props> = ({ deviceId }) => {
-  const [{ data, fetching, error }, executeQuery] = useQuery<QueryDataStoreQuery, QueryDataStoreQueryVariables>({
+  const [{ data, fetching, error }, reexecuteQuery] = useQuery<QueryDataStoreQuery, QueryDataStoreQueryVariables>({
     query: DATA_STORE_QUERY,
     variables: { deviceId },
   });
@@ -105,12 +99,12 @@ const DeviceConfig: FC<Props> = ({ deviceId }) => {
   );
   const [, resetConfig] = useMutation<ResetConfigMutation, ResetConfigMutationVariables>(RESET_CONFIG_MUTATION);
   const [, addSnapshot] = useMutation<AddSnapshotMutation, AddSnapshotMutationVariables>(ADD_SNAPSHOT_MUTATION);
+  const [, applySnapshot] = useMutation<ApplySnapshotMutation, ApplySnapshotMutationVariables>(APPLY_SNAPSHOT_MUTATION);
 
   const [config, setConfig] = useState<string>();
-  const [snapshotName, setSnapshotName] = useState<string>('');
-  const [isSnapshotNameInvalid, setIsSnapshotNameInvalid] = useState(false);
   const toast = useToast();
-  const { isOpen, onClose, onOpen } = useDisclosure();
+  const { isOpen: isSnapshotModalOpen, onClose: onSnapshotModalClose, onOpen: onSnapshotModalOpen } = useDisclosure();
+  const { isOpen: isDiffModalOpen, onClose: onDiffModalClose, onOpen: onDiffModalOpen } = useDisclosure();
 
   useEffect(() => {
     if (data != null && data.dataStore != null) {
@@ -134,7 +128,7 @@ const DeviceConfig: FC<Props> = ({ deviceId }) => {
     }
 
     if (responseData != null) {
-      executeQuery();
+      reexecuteQuery({ requestPolicy: 'network-only' });
 
       toast({
         title: 'Successfully updated config data store',
@@ -145,22 +139,22 @@ const DeviceConfig: FC<Props> = ({ deviceId }) => {
     }
   };
 
-  const handleOnCommitConfig = async () => {
-    const { data: responseData } = await commitConfig({ deviceId });
+  const handleOnCommitConfig = async (isDryRun?: boolean) => {
+    const { data: responseData } = await commitConfig({ input: { deviceId, shouldDryRun: isDryRun } });
 
     if (responseData?.commitConfig.isOk) {
       toast({
         duration: 2000,
         isClosable: true,
         status: 'success',
-        title: 'Successfully commited changes',
+        title: 'Successfully commited to network',
       });
     } else {
       toast({
         duration: 2000,
         isClosable: true,
         status: 'error',
-        title: 'Failed to commit changes',
+        title: 'Failed to commit to network',
       });
     }
   };
@@ -172,51 +166,48 @@ const DeviceConfig: FC<Props> = ({ deviceId }) => {
         duration: 2000,
         isClosable: true,
         status: 'success',
-        title: 'Successfully reset config data store',
+        title: 'Successfully replaced config with operational',
       });
 
-      executeQuery();
+      reexecuteQuery({ requestPolicy: 'network-only' });
     } else {
       toast({
         duration: 2000,
         isClosable: true,
         status: 'error',
-        title: 'Failed to reset config data store',
+        title: 'Failed to replaced config with operational',
       });
     }
   };
 
-  const handleOnAddSnapshot = async () => {
-    if (snapshotName.length === 0) {
-      setIsSnapshotNameInvalid(true);
-      return;
-    }
+  const handleOnAddSnapshot = async (name: string) => {
+    const { error: responseError } = await addSnapshot({ input: { name, deviceId } });
+    const hasError = responseError != null;
 
-    const { data: responseData } = await addSnapshot({ input: { name: snapshotName, deviceId } });
-    if (responseData != null) {
-      toast({
-        duration: 2000,
-        isClosable: true,
-        status: 'success',
-        title: 'Successfully created snapshot',
-      });
-
-      onClose();
-    } else {
-      toast({
-        duration: 2000,
-        isClosable: true,
-        status: 'error',
-        title: 'Failed to create snapshot',
-      });
-    }
+    toast({
+      duration: 2000,
+      isClosable: true,
+      status: hasError ? 'error' : 'success',
+      title: hasError ? 'Failed to create snapshot' : 'Successfully created snapshot',
+    });
   };
 
-  const handleChangeSnapshotName = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSnapshotName(e.target.value);
+  const handleOnApplySnapshot = async (name: string) => {
+    const { error: responseError } = await applySnapshot({ input: { name, deviceId } });
+
+    const hasError = responseError != null;
+
+    toast({
+      duration: 2000,
+      isClosable: true,
+      status: hasError ? 'error' : 'success',
+      title: hasError ? 'Failed to apply snapshot' : 'Successfully applied snapshot',
+    });
   };
 
-  if (fetching) {
+  const isInitialLoading = fetching && data == null;
+
+  if (isInitialLoading) {
     return <Progress size="xs" isIndeterminate mt={-10} />;
   }
 
@@ -224,90 +215,43 @@ const DeviceConfig: FC<Props> = ({ deviceId }) => {
     return <div>{error.message}</div>;
   }
 
+  const snapshots = data?.dataStore?.snapshots ?? [];
+
   return (
     <>
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Create snapshot</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody pb={6}>
-            <FormControl isInvalid={isSnapshotNameInvalid}>
-              <FormLabel>Snapshot name</FormLabel>
-              <Input placeholder="Snapshot name" onChange={handleChangeSnapshotName} />
-              <FormErrorMessage>Please enter snapshot name</FormErrorMessage>
-            </FormControl>
-          </ModalBody>
-
-          <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={handleOnAddSnapshot}>
-              Create
-            </Button>
-            <Button
-              onClick={() => {
-                onClose();
-                setIsSnapshotNameInvalid(false);
-              }}
-            >
-              Cancel
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-
+      <CreateSnapshotModal
+        isOpen={isSnapshotModalOpen}
+        onClose={onSnapshotModalClose}
+        onFormSubmit={handleOnAddSnapshot}
+      />
+      {isDiffModalOpen && <DiffOutputModal onClose={onDiffModalClose} deviceId={deviceId} />}
       <Container maxWidth={1280}>
-        <Box my={2}>
-          <HStack>
-            <Button colorScheme="blue" onClick={onOpen}>
-              Create snapshot
-            </Button>
-            <Button colorScheme="blue" onClick={handleOnResetConfig}>
-              Reset config
-            </Button>
-          </HStack>
+        <DeviceConfigActions
+          onCreateSnapshotBtnClick={onSnapshotModalOpen}
+          snapshots={snapshots}
+          onLoadSnapshotClick={handleOnApplySnapshot}
+          onCommitBtnClick={() => {
+            handleOnCommitConfig();
+          }}
+          onDryRunBtnClick={() => {
+            handleOnCommitConfig(true);
+          }}
+          onCalculateDiffBtnClick={onDiffModalOpen}
+        />
+        <Box background="white" padding={4}>
+          <DeviceConfigEditors
+            config={config ?? ''}
+            operational={JSON.stringify(JSON.parse(data?.dataStore?.operational ?? ''), null, 2)}
+            onConfigChange={(cfg) => {
+              setConfig(cfg);
+            }}
+            onReplaceBtnClick={handleOnResetConfig}
+            onConfigSaveBtnClick={handleOnSaveConfig}
+            onRefreshBtnClick={() => {
+              reexecuteQuery({ requestPolicy: 'network-only' });
+            }}
+          />
         </Box>
-
-        <Grid templateColumns="repeat(2, 1fr)" gap={4}>
-          <GridItem w="100%">
-            <Flex>
-              <Box my={4}>
-                <Text>Config data store</Text>
-              </Box>
-              <Spacer />
-              <Box my={2}>
-                <HStack>
-                  <Button colorScheme="blue" onClick={handleOnSaveConfig}>
-                    Save
-                  </Button>
-                  <Button colorScheme="blue" onClick={handleOnCommitConfig}>
-                    Commit
-                  </Button>
-                </HStack>
-              </Box>
-            </Flex>
-            <AceEditor
-              width="100%"
-              mode="json"
-              value={config}
-              theme="tomorrow"
-              onChange={(val) => {
-                setConfig(val);
-              }}
-            />
-          </GridItem>
-          <GridItem w="100%">
-            <Box my={4}>
-              <Text>Operational data store</Text>
-            </Box>
-            <AceEditor
-              width="100%"
-              mode="json"
-              value={JSON.stringify(JSON.parse(data?.dataStore?.operational ?? ''), null, 2)}
-              theme="tomorrow"
-              readOnly
-            />
-          </GridItem>
-        </Grid>
       </Container>
     </>
   );
---
+++ packages/frinx-inventory-client/src/pages/device-config/diff-output-modal.tsx
@@ -0,0 +1,69 @@
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+} from '@chakra-ui/react';
+import React, { VoidFunctionComponent } from 'react';
+import { gql, useQuery } from 'urql';
+import { CalculatedDiffQuery, CalculatedDiffQueryVariables } from '../../__generated__/graphql';
+
+const CALCULATED_DIFF_QUERY = gql`
+  query calculatedDiff($deviceId: String!) {
+    calculatedDiff(deviceId: $deviceId) {
+      output
+    }
+  }
+`;
+
+type Props = {
+  onClose: () => void;
+  deviceId: string;
+};
+
+const DiffOutputModal: VoidFunctionComponent<Props> = ({ onClose, deviceId }) => {
+  const [{ data, fetching, error }] = useQuery<CalculatedDiffQuery, CalculatedDiffQueryVariables>({
+    query: CALCULATED_DIFF_QUERY,
+    variables: {
+      deviceId,
+    },
+    requestPolicy: 'network-only',
+  });
+
+  if (fetching || error != null) {
+    return null;
+  }
+
+  const { output } = data.calculatedDiff;
+
+  return (
+    <Modal isOpen onClose={onClose} size="6xl">
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Calculated diff output</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody pb={6}>
+          <pre>
+            <code>{JSON.stringify(JSON.parse(output), null, 2)}</code>
+          </pre>
+        </ModalBody>
+
+        <ModalFooter>
+          <Button
+            onClick={() => {
+              onClose();
+            }}
+          >
+            Close
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default DiffOutputModal;
