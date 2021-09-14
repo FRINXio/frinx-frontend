47b914b168205d884c9c34ddc539ef8874352f74
Martin Sottnik <msottnik@gmail.com>
2021-09-14 15:10:48 +0200

    vpn service form added

---
+++ packages/frinx-gamma/src/api/unistore/api-helpers.ts
@@ -0,0 +1,56 @@
+const CONDUCTOR_API_URL = window.__CONFIG__.conductor_api_url;
+
+export async function apiFetch(path: string, options: RequestInit): Promise<unknown> {
+  const url = `${CONDUCTOR_API_URL}${path}`;
+  const response = await fetch(url, options);
+
+  if (!response.ok) {
+    throw new Error(`apiFetch failed with http-code ${response.status}`);
+  }
+
+  if (response.status === 201) {
+    return null;
+  }
+
+  return response.json();
+}
+
+export async function sendGetRequest(path: string): Promise<unknown> {
+  const options = {
+    method: 'GET',
+  };
+  return apiFetch(path, options);
+}
+
+export async function sendPostRequest(path: string, body: unknown): Promise<unknown> {
+  const options = {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: {
+      'content-type': 'application/json',
+    },
+  };
+  return apiFetch(path, options);
+}
+
+export async function sendPutRequest(path: string, body: unknown): Promise<unknown> {
+  const options = {
+    method: 'PUT',
+    body: JSON.stringify(body),
+    headers: {
+      'content-type': 'application/json',
+    },
+  };
+  return apiFetch(path, options);
+}
+
+export async function sendDeleteRequest(path: string, body?: unknown): Promise<unknown> {
+  const options = {
+    method: 'DELETE',
+    body: JSON.stringify(body),
+    headers: {
+      'content-type': 'application/json',
+    },
+  };
+  return apiFetch(path, options);
+}
---
+++ packages/frinx-gamma/src/components/forms/vpn-service-form.tsx
@@ -0,0 +1,212 @@
+import React, { FC, FormEvent, useState } from 'react';
+import {
+  Flex,
+  HStack,
+  Tag,
+  Box,
+  Divider,
+  Button,
+  Stack,
+  FormControl,
+  FormLabel,
+  Input,
+  Select,
+  IconButton,
+  TagLabel,
+  TagCloseButton,
+} from '@chakra-ui/react';
+import { AddIcon } from '@chakra-ui/icons';
+import { VpnServiceTopology, DefaultCVlanEnum, VpnService, MaximumRoutes } from './service-types';
+
+type Props = {
+  service: VpnService;
+  extranetVpns: string[];
+  onSubmit: (p: VpnService) => void;
+  onCancel: () => void;
+};
+
+const VpnServiceForm: FC<Props> = ({ extranetVpns, service, onSubmit, onCancel }) => {
+  const [serviceState, setServiceState] = useState(service);
+  const [extranetVpnSelect, setExtranetVpnSelect] = useState<string | null>(null);
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    onSubmit(serviceState);
+  };
+
+  const handleExtranetVpnAdd = () => {
+    if (!extranetVpnSelect) {
+      return;
+    }
+    const newExtranetVpns = [...serviceState.extranetVpns, extranetVpnSelect];
+    setServiceState({
+      ...serviceState,
+      extranetVpns: newExtranetVpns,
+    });
+    setExtranetVpnSelect(null);
+  };
+
+  const handleExtranetVpnRemove = (vpn: string) => {
+    const newExtranetVpns = [...serviceState.extranetVpns].filter((v) => v !== vpn);
+    setServiceState({
+      ...serviceState,
+      extranetVpns: newExtranetVpns,
+    });
+  };
+
+  const filteredExtranetVpns = extranetVpns.filter((ev) => {
+    return !serviceState.extranetVpns.includes(ev);
+  });
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <FormControl id="topic" my={6}>
+        <FormLabel>Customer Name</FormLabel>
+        <Input
+          variant="filled"
+          name="topic"
+          value={serviceState.customerName}
+          onChange={(event) => {
+            setServiceState({
+              ...serviceState,
+              customerName: event.target.value,
+            });
+          }}
+        />
+      </FormControl>
+      <FormControl id="vpnServiceTopology" my={6}>
+        <FormLabel>Vpn Service Topology</FormLabel>
+        <Select
+          variant="filled"
+          name="vpnServiceTopology"
+          value={serviceState.vpnServiceTopology}
+          onChange={(event) => {
+            event.persist();
+            const eventValue = event.target.value as VpnServiceTopology;
+            setServiceState({
+              ...serviceState,
+              vpnServiceTopology: eventValue,
+            });
+          }}
+        >
+          <option value="any-any">any-any</option>
+          <option value="hub-spoke">hub-spoke</option>
+          <option value="hub-spoke-disjointed">hub-spoke-disjointed</option>
+        </Select>
+      </FormControl>
+      <FormControl id="defaultCVlan" my={6}>
+        <FormLabel>Default C Vlan</FormLabel>
+        <Select
+          variant="filled"
+          name="defaultCVlan"
+          value={serviceState.defaultCVlan}
+          onChange={(event) => {
+            event.persist();
+            const defaultCVlan = event.target.value as unknown as DefaultCVlanEnum;
+            setServiceState({
+              ...serviceState,
+              defaultCVlan,
+            });
+          }}
+        >
+          {[...Object.entries(DefaultCVlanEnum)].map((e) => {
+            const [k, v] = e;
+            return (
+              <option key={k} value={v}>
+                {k}
+              </option>
+            );
+          })}
+        </Select>
+      </FormControl>
+      <FormControl id="maxiumRoutes" my={6}>
+        <FormLabel>Maximum Routes</FormLabel>
+        <Select
+          variant="filled"
+          name="maximumRoutes"
+          value={serviceState.maximumRoutes}
+          onChange={(event) => {
+            event.persist();
+            const eventValue = event.target.value as unknown as MaximumRoutes;
+            setServiceState({
+              ...serviceState,
+              maximumRoutes: eventValue,
+            });
+          }}
+        >
+          <option value="1000">1000</option>
+          <option value="ipv6">ipv6</option>
+        </Select>
+      </FormControl>
+
+      <FormControl id="extranet-vpns" my={6}>
+        <FormLabel>Extranet VPNs</FormLabel>
+        <Flex
+          alignItems="stretch"
+          border="1px"
+          borderColor="gray.200"
+          px={4}
+          my={4}
+          borderRadius="md"
+          userSelect="none"
+        >
+          <Box py={4} flex={1}>
+            <Select
+              variant="filled"
+              name="extranetVpns"
+              value={extranetVpnSelect || ''}
+              onChange={(event) => {
+                event.persist();
+                const eventValue = event.target.value as string;
+                setExtranetVpnSelect(eventValue);
+              }}
+            >
+              <option>-- choose option</option>
+              {filteredExtranetVpns &&
+                filteredExtranetVpns.map((vpn) => {
+                  return (
+                    <option key={`extranet-vpn-${vpn}`} value={vpn}>
+                      {vpn}
+                    </option>
+                  );
+                })}
+            </Select>
+          </Box>
+
+          <Box marginLeft={4} alignSelf="center">
+            <IconButton
+              size="sm"
+              aria-label="Add Extranet VPN"
+              icon={<AddIcon />}
+              onClick={() => {
+                handleExtranetVpnAdd();
+              }}
+            />
+          </Box>
+        </Flex>
+        {serviceState.extranetVpns.length > 0 && (
+          <HStack>
+            {serviceState.extranetVpns.map((vpn) => {
+              return (
+                <Tag key={`selected-extranet-vpn-${vpn}`} size="lg">
+                  <TagLabel>{vpn}</TagLabel>
+                  <TagCloseButton onClick={() => handleExtranetVpnRemove(vpn)} />
+                </Tag>
+              );
+            })}
+          </HStack>
+        )}
+      </FormControl>
+
+      <Divider my={4} />
+      <Stack direction="row" spacing={2} align="center">
+        <Button type="submit" colorScheme="blue">
+          Save changes
+        </Button>
+        <Button onClick={onCancel}>Cancel</Button>
+      </Stack>
+    </form>
+  );
+};
+
+export default VpnServiceForm;
