cad51fae1ddacd5167b7d103a76d8880fac48764
Martin Sottnik <msottnik@gmail.com>
2021-09-16 19:08:14 +0200

    simplify json jq form

--- packages/frinx-workflow-builder/src/components/task-form/json-input-form.tsx
+++ packages/frinx-workflow-builder/src/components/task-form/json-input-form.tsx
@@ -1,35 +1,17 @@
-import React, { FC, useEffect, useState } from 'react';
-import { FormControl, FormLabel, Input, useTheme } from '@chakra-ui/react';
-import { pick, omit } from 'lodash';
-import { JsonJQInputParams } from '../../helpers/types';
-import Editor from '../common/editor';
+import React, { FC } from 'react';
+import { FormControl, FormLabel, Input } from '@chakra-ui/react';
+import { ExtendedTask, JsonJQInputParams } from '../../helpers/types';
+import AutocompleteTaskReferenceNameMenu from '../autocomplete-task-reference-name/autocomplete-task-reference-name-menu';
 
 type Props = {
   params: JsonJQInputParams;
+  tasks: ExtendedTask[];
+  task: ExtendedTask;
   onChange: (p: JsonJQInputParams) => void;
   onValidation: (isValid: boolean) => void;
 };
 
-const JsonJQInputsForm: FC<Props> = ({ params, onChange, onValidation }) => {
-  const theme = useTheme();
-  const [inputJson, setInputJson] = useState(JSON.stringify(omit(params, ['queryExpression']), null, 2));
-  const [queryExpression, setQueryExpression] = useState<string>(
-    pick(params, 'queryExpression').queryExpression as string,
-  );
-
-  useEffect(() => {
-    try {
-      const json = JSON.parse(inputJson);
-      onValidation(true);
-      onChange({
-        ...json,
-        queryExpression,
-      });
-    } catch {
-      onValidation(false);
-    }
-  }, [inputJson, queryExpression]); // eslint-disable-line react-hooks/exhaustive-deps
-
+const JsonJQInputsForm: FC<Props> = ({ params, onChange, tasks, task }) => {
   return (
     <>
       <FormControl id="queryExpression" my={6}>
@@ -37,28 +19,43 @@ const JsonJQInputsForm: FC<Props> = ({ params, onChange, onValidation }) => {
         <Input
           type="text"
           name="queryExpression"
-          value={queryExpression}
+          value={params.queryExpression.replace(/^\.key/, '')}
           onChange={(event) => {
             event.persist();
-            setQueryExpression(event.target.value);
+            onChange({
+              ...params,
+              queryExpression: `.key${event.target.value}`,
+            });
           }}
         />
       </FormControl>
 
-      <FormControl id="inputParams" my={6}>
-        <FormLabel>JSON:</FormLabel>
-        <Editor
-          name="inputParams"
-          value={inputJson}
-          onChange={(value) => {
-            setInputJson(value);
+      <FormControl id="key" my={6}>
+        <FormLabel>Task Reference Name:</FormLabel>
+        <AutocompleteTaskReferenceNameMenu
+          tasks={tasks}
+          task={task}
+          onChange={(updatedInputValue) => {
+            onChange({
+              ...params,
+              key: updatedInputValue,
+            });
           }}
-          enableBasicAutocompletion
-          height="200px"
-          style={{
-            borderRadius: theme.radii.md,
-          }}
-        />
+          inputValue={params.key}
+        >
+          <Input
+            type="text"
+            name="key"
+            value={params.key}
+            onChange={(event) => {
+              event.persist();
+              onChange({
+                ...params,
+                key: event.target.value,
+              });
+            }}
+          />
+        </AutocompleteTaskReferenceNameMenu>
       </FormControl>
     </>
   );
