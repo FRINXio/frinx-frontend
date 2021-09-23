364aa625d4a5871e153cb961c2e18c82b39cc215
Marco Mruz <marcomruz1@gmail.com>
2021-09-23 14:38:24 +0200

    update flowtype

--- packages/frinx-workflow-ui/src/common/flowtypes.ts
+++ packages/frinx-workflow-ui/src/common/flowtypes.ts
@@ -1,25 +1,23 @@
 // @flow
 
 export type Task = {
-  taskType: string,
-  status: string,
-  reasonForIncompletion: string,
-  referenceTaskName: string,
-  callbackAfterSeconds: number,
-  pollCount: number,
-  logs: {},
-  inputData: {},
-  outputData: {},
+  taskType: string;
+  status: string;
+  reasonForIncompletion: string;
+  referenceTaskName: string;
+  callbackAfterSeconds: number;
+  pollCount: number;
+  logs: {};
+  inputData: {};
+  outputData: {};
   workflowTask: {
-    description: string,
+    description: string;
     taskDefinition: {
-      description: string,
-    },
-  },
-  // seq: number;
-  // referenceTaskName: string;
-  // subWorkflowId: string;
-  // startTime: number;
-  // endTime: number;
-  // status: string;
+      description: string;
+    };
+  };
+  seq: number;
+  subWorkflowId: string;
+  startTime: number;
+  endTime: number;
 };
