96b0fdcb9c0c645d90ca874fd40ca0609db0f4fb
Marco Mruz <marcomruz1@gmail.com>
2021-09-24 13:09:25 +0200

    update from workflow-exec to workflow-execution

--- packages/frinx-workflow-ui/src/pages/workflow-list/WorkflowDefs/DiagramModal/DiagramModal.js
+++ packages/frinx-workflow-ui/src/pages/workflow-list/WorkflowDefs/DiagramModal/DiagramModal.js
@@ -1,6 +1,6 @@
 // @flow
 import React from 'react';
-import WorkflowDia from '../../workflow-exec/details-modal/WorkflowDia/WorkflowDia';
+import WorkflowDia from '../../workflow-execution/details-modal/WorkflowDia/WorkflowDia';
 import {
   Button,
   Modal,
