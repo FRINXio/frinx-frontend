541f0de20cc6914e6b75e99409a0b4c813143461
Marco Mruz <marcomruz1@gmail.com>
2021-10-12 18:13:04 +0200

    change extension of uniflow api provider

--- packages/frinx-workflow-ui/src/uniflow-api-provider.tsx
+++ packages/frinx-workflow-ui/src/uniflow-api-provider.tsx
@@ -1,19 +1,12 @@
 import React, { createContext, FC } from 'react';
-import callbackUtils from './utils/callbackUtils';
+import callbackUtils, { Callbacks } from './utils/callback-utils';
 
 export const UniflowApiContext = createContext(false);
-
-// TODO types for callbacks
-export type UniflowApiCallbacks = {
-  getWorkflows: unknown;
-  getTaskDefinitions: unknown;
-};
-
 const UniflowApiProvider: FC = ({ children }) => {
   return <UniflowApiContext.Provider value>{children}</UniflowApiContext.Provider>;
 };
 
-export function getUniflowApiProvider(callbacks: UniflowApiCallbacks): FC {
+export function getUniflowApiProvider(callbacks: Callbacks): FC {
   callbackUtils.setCallbacks(callbacks);
   return UniflowApiProvider;
 }
