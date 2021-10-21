c65093100aa4e16345bb7515bafbe54a3173b77c
Martin Sottnik <msottnik@gmail.com>
2021-10-21 11:22:19 +0200

    bearer backup fix (#224)
    Co-authored-by: Pavol Porubský <paulooze@gmail.com>

--- packages/frinx-gamma/src/pages/create-site-network-access/create-site-network-access.tsx
+++ packages/frinx-gamma/src/pages/create-site-network-access/create-site-network-access.tsx
@@ -43,7 +43,7 @@ const getDefaultNetworkAccess = (): SiteNetworkAccess => ({
     bearerReference: '',
     requestedCLan: RequestedCVlan.l3vpn,
     requestedType: {
-      requestedType: 'dotlat',
+      requestedType: 'dot1ad',
       strict: false,
     },
   },
