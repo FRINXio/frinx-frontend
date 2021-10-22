0d3e65ee87aed99891f3952d1f7e535d9d37f2c3
Marco Mruz <marcomruz1@gmail.com>
2021-10-22 12:09:18 +0200

    rename page select

--- packages/frinx-workflow-ui/src/common/page-select.tsx
+++ packages/frinx-workflow-ui/src/common/page-select.tsx
@@ -1,13 +1,12 @@
-// @flow
 import React, { Component } from 'react';
 import { Flex } from '@chakra-ui/react';
 import { Next, PageGroup, Paginator, Previous } from 'chakra-paginator';
 
 type Props = {
-  viewedPage: number,
-  count: number,
-  indent: number,
-  handler: (defaultPageCount: number, pageCount?: number) => void,
+  viewedPage: number;
+  count: number;
+  indent: number;
+  handler: (defaultPageCount: number, pageCount?: number) => void;
 };
 
 type StateType = {};
