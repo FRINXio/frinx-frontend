a3f157e9a10c0b03e755e3cb2428b146d3adbd59
Martin Sottnik <msottnik@gmail.com>
2021-11-08 12:39:41 +0100

    bearer form validation (#263)

--- packages/frinx-gamma/src/components/forms/vpn-bearer-form.tsx
+++ packages/frinx-gamma/src/components/forms/vpn-bearer-form.tsx
@@ -1,10 +1,35 @@
-import React, { FC, FormEvent, useEffect, useState } from 'react';
-import { Divider, Button, Input, Select, Stack, FormControl, FormLabel } from '@chakra-ui/react';
+import React, { VoidFunctionComponent } from 'react';
+import { Divider, Button, Input, Select, Stack, FormControl, FormErrorMessage, FormLabel } from '@chakra-ui/react';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 import { VpnSite } from './site-types';
 import { VpnBearer, VpnCarrier, VpnNode } from './bearer-types';
 import CarrierForm from './carrier-form';
 import ConnectionForm from './connection-form';
 import Autocomplete2 from '../autocomplete-2/autocomplete-2';
+
+const CarrierSchema = yup.object().shape({
+  carrierName: yup.string().nullable(),
+  carrierReference: yup.string().nullable(),
+  serviceType: yup.string().nullable(),
+  serviceStatus: yup.string().nullable(),
+});
+const ConnectionSchema = yup.object().shape({
+  encapsulationType: yup.string().nullable(),
+  svlanAssignmentType: yup.string().nullable(),
+  tpId: yup.string().nullable(),
+  mtu: yup.number().required('Mtu is required'),
+  remoteNeId: yup.string().nullable(),
+  remotePortId: yup.string().nullable(),
+});
+const BearerSchema = yup.object().shape({
+  spBearerReference: yup.string().required('spBearerReference is required'),
+  neId: yup.string().required('Ne Id is required'),
+  portId: yup.string().required('Port Id is required'),
+  description: yup.string().nullable(),
+  carrier: CarrierSchema.nullable(),
+  connection: ConnectionSchema.nullable(),
+});
 
 type Props = {
   mode: 'add' | 'edit';
@@ -16,87 +41,67 @@ type Props = {
   onSiteChange?: (s: VpnSite) => void;
 };
 
-const VpnBearerForm: FC<Props> = ({ mode, nodes, carriers, bearer, onSubmit, onCancel }) => {
-  const [bearerState, setBearerState] = useState(bearer);
-
-  useEffect(() => {
-    setBearerState({
+const VpnBearerForm: VoidFunctionComponent<Props> = ({ mode, nodes, carriers, bearer, onSubmit, onCancel }) => {
+  const { values, errors, setFieldValue, handleChange, handleSubmit } = useFormik({
+    initialValues: {
       ...bearer,
-    });
-  }, [bearer]);
-
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    onSubmit(bearerState);
-  };
+    },
+    validationSchema: BearerSchema,
+    onSubmit: (formValues) => {
+      onSubmit(formValues);
+    },
+  });
 
   const nodeItems = nodes.map((n) => ({
     value: n.neId,
     label: `${n.neId} (${n.routerId})`,
   }));
   const [selectedNode] = nodeItems.filter((n) => {
-    return n.value === bearerState.neId;
+    return n.value === values.neId;
   });
 
   return (
     <form onSubmit={handleSubmit}>
-      <FormControl id="ne-id" my={6}>
+      <FormControl id="ne-id" my={6} isRequired isInvalid={errors.neId != null}>
         <FormLabel>Ne Id</FormLabel>
         <Autocomplete2
           items={nodeItems}
           selectedItem={selectedNode}
           onChange={(item) => {
-            setBearerState({
-              ...bearerState,
-              neId: item ? item.value : '',
-            });
+            setFieldValue('neId', item ? item.value : null);
           }}
         />
+        {errors.neId && <FormErrorMessage>{errors.neId}</FormErrorMessage>}
       </FormControl>
-      <FormControl id="port-id" my={6}>
+      <FormControl id="port-id" my={6} isRequired isInvalid={errors.portId != null}>
         <FormLabel>Port ID</FormLabel>
-        <Select
-          variant="filled"
-          name="port-id"
-          value={bearerState.portId}
-          onChange={(event) => {
-            setBearerState({
-              ...bearerState,
-              portId: event.target.value,
-            });
-          }}
-        >
+        <Select variant="filled" name="portId" value={values.portId} onChange={handleChange}>
+          <option value="">-- choose port id</option>
           {[...Array(8).keys()].map((v) => {
-            return <option value={`xe-0/1/${v}`}>{`xe-0/1/${v}`}</option>;
+            return <option key={`port-id-xe-${v}`} value={`xe-0/1/${v}`}>{`xe-0/1/${v}`}</option>;
           })}
         </Select>
+        {errors.portId && <FormErrorMessage>{errors.portId}</FormErrorMessage>}
       </FormControl>
-      <FormControl id="sp-bearer-reference" my={6}>
+      <FormControl id="sp-bearer-reference" my={6} isRequired isInvalid={errors.spBearerReference != null}>
         <FormLabel>Gamma hub-link ID</FormLabel>
         <Input
           variant="filled"
-          name="sp-bearer-reference"
-          value={bearerState.spBearerReference}
+          name="spBearerReference"
+          value={values.spBearerReference}
           disabled={mode === 'edit'}
-          onChange={(event) => {
-            setBearerState({
-              ...bearerState,
-              spBearerReference: event.target.value,
-            });
-          }}
+          onChange={handleChange}
         />
+        {errors.spBearerReference && <FormErrorMessage>{errors.spBearerReference}</FormErrorMessage>}
       </FormControl>
       <FormControl id="description" my={6}>
         <FormLabel>Description</FormLabel>
         <Input
           variant="filled"
           name="description"
-          value={bearerState.description || ''}
+          value={values.description || ''}
           onChange={(event) => {
-            setBearerState({
-              ...bearerState,
-              description: event.target.value || null,
-            });
+            setFieldValue('description', event.target.value || null);
           }}
         />
       </FormControl>
@@ -104,18 +109,16 @@ const VpnBearerForm: FC<Props> = ({ mode, nodes, carriers, bearer, onSubmit, onC
       <CarrierForm
         carriers={carriers}
         carrier={
-          bearerState.carrier || { carrierName: null, carrierReference: null, serviceType: null, serviceStatus: null }
+          values.carrier || { carrierName: null, carrierReference: null, serviceType: null, serviceStatus: null }
         }
+        errors={errors}
         onChange={(carrier) => {
-          setBearerState({
-            ...bearerState,
-            carrier,
-          });
+          setFieldValue('carrier', carrier);
         }}
       />
       <ConnectionForm
         connection={
-          bearerState.connection || {
+          values.connection || {
             tpId: null,
             svlanAssignmentType: null,
             encapsulationType: null,
@@ -124,11 +127,9 @@ const VpnBearerForm: FC<Props> = ({ mode, nodes, carriers, bearer, onSubmit, onC
             remotePortId: null,
           }
         }
+        errors={errors}
         onChange={(connection) => {
-          setBearerState({
-            ...bearerState,
-            connection,
-          });
+          setFieldValue('connection', connection);
         }}
       />
 
