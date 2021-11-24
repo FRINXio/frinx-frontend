f88a5f7ceea4bd6b7eb5a6c1076b537f9e7941fa
Marco Mruz <marcomruz1@gmail.com>
2021-11-24 17:12:35 +0100

    handle on change event for claim resource modal form

--- packages/frinx-uniresource-ui/src/pages/pool-detail-page/pool-detail-page.tsx
+++ packages/frinx-uniresource-ui/src/pages/pool-detail-page/pool-detail-page.tsx
@@ -4,12 +4,16 @@ import {
   ButtonGroup,
   Flex,
   Heading,
-  HStack,
-  ListItem,
   Progress,
   Spacer,
+  Table,
+  Tbody,
+  Td,
   Text,
-  UnorderedList,
+  Th,
+  Thead,
+  Tr,
+  useDisclosure,
 } from '@chakra-ui/react';
 import React, { FC } from 'react';
 import { gql, useMutation, useQuery } from 'urql';
@@ -19,12 +23,19 @@ import {
   ClaimResourceMutationMutationVariables,
   FreeResourceMutationMutation,
   FreeResourceMutationMutationVariables,
+  PoolCapacityPayload,
   QueryPoolDetailQuery,
   QueryPoolDetailQueryVariables,
 } from '../../__generated__/graphql';
+import ClaimResourceModal from './claim-resource-modal';
 
 type Props = {
   poolId: string;
+};
+
+export type PoolResource = {
+  poolProperties: Record<string, string>;
+  poolPropertyTypes: Record<string, 'int' | 'string'>;
 };
 
 const POOL_DETAIL_QUERY = gql`
@@ -70,7 +81,25 @@ const FREE_RESOURCES_MUTATION = gql`
   }
 `;
 
+function getTotalCapacity(capacity: PoolCapacityPayload | null): number {
+  if (capacity == null) {
+    return 0;
+  }
+  return capacity.freeCapacity + capacity.utilizedCapacity;
+}
+function getCapacityValue(capacity: PoolCapacityPayload | null): number {
+  if (capacity == null) {
+    return 0;
+  }
+  const totalCapacity = getTotalCapacity(capacity);
+  if (totalCapacity === 0) {
+    return 0;
+  }
+  return (capacity.utilizedCapacity / totalCapacity) * 100;
+}
+
 const PoolDetailPage: FC<Props> = ({ poolId }) => {
+  const claimResourceModal = useDisclosure();
   const [{ data, fetching: isLoadingPool }] = useQuery<QueryPoolDetailQuery, QueryPoolDetailQueryVariables>({
     query: POOL_DETAIL_QUERY,
     variables: {
@@ -85,7 +114,7 @@ const PoolDetailPage: FC<Props> = ({ poolId }) => {
     FREE_RESOURCES_MUTATION,
   );
 
-  const claimPoolResource = (userInput: Map<string, string>, description?: string) => {
+  const claimPoolResource = (userInput: PoolResource, description?: string) => {
     claimResource({
       poolId,
       userInput,
@@ -109,12 +138,22 @@ const PoolDetailPage: FC<Props> = ({ poolId }) => {
   }
 
   const { QueryResourcePool: resourcePool } = data;
-  const canClaimResources = resourcePool.Capacity != null && resourcePool.Capacity.freeCapacity > 0;
-  const canFreeResources =
-    resourcePool.Capacity != null && resourcePool.Capacity.freeCapacity !== resourcePool.Capacity.utilizedCapacity;
+  const capacityValue = getCapacityValue(resourcePool.Capacity);
+  const totalCapacity = getTotalCapacity(resourcePool.Capacity);
+  const canClaimResources =
+    resourcePool.Capacity != null &&
+    resourcePool.Capacity.freeCapacity > 0 &&
+    resourcePool.Capacity.freeCapacity <= totalCapacity;
+  const canFreeResources = resourcePool.Capacity != null && resourcePool.Capacity.freeCapacity !== totalCapacity;
 
   return (
     <PageContainer>
+      <ClaimResourceModal
+        poolName={resourcePool.Name}
+        isOpen={claimResourceModal.isOpen}
+        onClose={claimResourceModal.onClose}
+        onClaim={claimPoolResource}
+      />
       <Flex alignItems="center">
         <Heading size="3xl" as="h2" mb={6}>
           {resourcePool.Name}
@@ -122,36 +161,50 @@ const PoolDetailPage: FC<Props> = ({ poolId }) => {
         <Spacer />
         <Box>
           <ButtonGroup spacing={6} variant="outline">
-            <Button onClick={() => console.log('claimol som')} colorScheme="blue" isDisabled={canClaimResources}>
+            <Button onClick={claimResourceModal.onOpen} colorScheme="blue" isDisabled={!canClaimResources}>
               Claim resources
             </Button>
-            <Button onClick={() => console.log('freeol som')} colorScheme="red" isDisabled={canFreeResources}>
+            <Button onClick={() => console.log('freeol som')} colorScheme="red" isDisabled={!canFreeResources}>
               Free resources
             </Button>
           </ButtonGroup>
         </Box>
       </Flex>
 
-      <HStack>
-        <Text fontSize="xl">
-          Capacity: {resourcePool.Capacity?.freeCapacity}/{resourcePool.Capacity?.utilizedCapacity}
+      <Box background="white" padding={5}>
+        <Text fontSize="lg">Utilized capacity</Text>
+        <Progress size="xs" value={capacityValue} />
+        <Text as="span" fontSize="xs" color="gray.600" fontWeight={500}>
+          {resourcePool.Capacity?.freeCapacity ?? 0} / {totalCapacity}
         </Text>
-      </HStack>
+      </Box>
 
       <Box mt={10}>
         <Heading size="lg">Allocated Resources</Heading>
-        <UnorderedList listStyleType="none">
-          {resourcePool.allocatedResources != null &&
-            resourcePool.allocatedResources.edges.map((resource) => (
-              <ListItem key={resource?.node.Description}>
-                <Box>
-                  {resource?.node.Description}
-                  <br />
-                  {resource?.node.Properties.address}/{resource?.node.Properties.prefix}
-                </Box>
-              </ListItem>
-            ))}
-        </UnorderedList>
+        <Table background="white">
+          <Thead>
+            <Tr>
+              <Th>Description</Th>
+              <Th>Address</Th>
+              <Th>Prefix</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {resourcePool.allocatedResources != null && resourcePool.allocatedResources.edges.length > 0 ? (
+              resourcePool.allocatedResources.edges.map((resource) => (
+                <Tr>
+                  <Td>{resource?.node.Description}</Td>
+                  <Td>{resource?.node.Properties.address}</Td>
+                  <Td>{resource?.node.Properties.prefix}</Td>
+                </Tr>
+              ))
+            ) : (
+              <Tr>
+                <Td>There are no allocated resources yet.</Td>
+              </Tr>
+            )}
+          </Tbody>
+        </Table>
       </Box>
     </PageContainer>
   );
