5746dfa4577860d2b5311d3024e595f320982b22
Paulooze <paulooze@gmail.com>
2021-11-30 09:32:35 +0100

    fix feather-icons-react d.ts file error in wf-builder

--- packages/frinx-workflow-builder/custom.d.ts
+++ packages/frinx-workflow-builder/custom.d.ts
@@ -1,12 +1,4 @@
-import { FC } from 'react';
-
 declare module '*.svg' {
   const content: string;
-  return content;
-}
-
-declare module 'feather-icons-react' {
-  const content: FC<{ icon: string; size: string }>;
-
-  return content;
+  export default content;
 }
