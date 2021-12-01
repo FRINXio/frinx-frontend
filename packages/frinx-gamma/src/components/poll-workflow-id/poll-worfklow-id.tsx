6259925461b0fd1bf34e545624718c2bcdd92fe4
Martin Sottnik <msottnik@gmail.com>
2021-12-01 12:55:48 +0100

    get service id from workflow (#307)
    * get service id from workflow
    
    * poll workflow id

---
+++ packages/frinx-gamma/src/components/poll-workflow-id/poll-worfklow-id.tsx
@@ -0,0 +1,36 @@
+import { useEffect, VoidFunctionComponent } from 'react';
+import { useAsyncGenerator } from '../commit-status-modal/commit-status-modal.helpers';
+
+type Props = {
+  workflowId: string;
+  onFinish: (result: string) => void;
+};
+
+type WorkflowResponse = {
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  response_body: {
+    counter: number;
+    text: string;
+  };
+};
+
+const WorkflowId: VoidFunctionComponent<Props> = ({ workflowId, onFinish }) => {
+  const execPayload = useAsyncGenerator({ workflowId });
+
+  useEffect(() => {
+    if (execPayload == null) {
+      return;
+    }
+
+    if (execPayload.status === 'COMPLETED') {
+      const { output } = execPayload;
+      const body = output as unknown as WorkflowResponse;
+      const { text } = body.response_body;
+      onFinish(text);
+    }
+  }, [execPayload, onFinish]);
+
+  return null;
+};
+
+export default WorkflowId;
--- packages/frinx-gamma/src/pages/create-vpn-service/create-vpn-service.tsx
+++ packages/frinx-gamma/src/pages/create-vpn-service/create-vpn-service.tsx
@@ -1,12 +1,14 @@
 import { Box, Container, Heading } from '@chakra-ui/react';
 import React, { useEffect, useState, VoidFunctionComponent } from 'react';
 import callbackUtils from '../../unistore-callback-utils';
+import uniflowCallbackUtils from '../../uniflow-callback-utils';
 import { apiVpnServiceToClientVpnService } from '../../components/forms/converters';
 import { getSelectOptions } from '../../components/forms/options.helper';
 import { DefaultCVlanEnum, VpnService } from '../../components/forms/service-types';
 import VpnServiceForm from '../../components/forms/vpn-service-form';
 import ErrorMessage from '../../components/error-message/error-message';
 import { generateVpnId } from '../../helpers/id-helpers';
+import PollWorkflowId from '../../components/poll-workflow-id/poll-worfklow-id';
 
 const defaultVpnService: VpnService = {
   customerName: '',
@@ -23,11 +25,20 @@ type Props = {
 };
 
 const CreateVpnServicePage: VoidFunctionComponent<Props> = ({ onSuccess, onCancel }) => {
+  const [workflowId, setWorkflowId] = useState<string | null>(null);
+  const [vpnId, setVpnId] = useState<string | null>(null);
   const [vpnServices, setVpnServices] = useState<VpnService[] | null>(null);
   const [submitError, setSubmitError] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchData = async () => {
+      const uniflowCallbacks = uniflowCallbackUtils.getCallbacks;
+      const workflowResult = await uniflowCallbacks.executeWorkflow({
+        name: 'Allocate_VpnServiceId',
+        version: 1,
+        input: {},
+      });
+      setWorkflowId(workflowResult.text);
       const callbacks = callbackUtils.getCallbacks;
       const services = await callbacks.getVpnServices(null, null);
       const clientVpnServices = apiVpnServiceToClientVpnService(services);
@@ -62,6 +73,16 @@ const CreateVpnServicePage: VoidFunctionComponent<Props> = ({ onSuccess, onCance
     onCancel();
   };
 
+  if (!workflowId) {
+    return null;
+  }
+
+  if (!vpnId) {
+    return <PollWorkflowId workflowId={workflowId} onFinish={setVpnId} />;
+  }
+
+  const vpnService = { ...defaultVpnService, vpnId };
+
   return (
     <Container>
       <Box padding={6} margin={6} background="white">
@@ -71,7 +92,7 @@ const CreateVpnServicePage: VoidFunctionComponent<Props> = ({ onSuccess, onCance
           <VpnServiceForm
             mode="add"
             services={vpnServices}
-            service={defaultVpnService}
+            service={vpnService}
             onSubmit={handleSubmit}
             extranetVpns={extranetVpns}
             onCancel={handleCancel}
