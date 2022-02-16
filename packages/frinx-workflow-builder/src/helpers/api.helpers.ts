ca972c288a6c9730ed33ea4dd5a85b3d7733b0b2
Martin Sottnik <msottnik@gmail.com>
2022-02-16 14:39:11 +0100

    flow to api transformation (decission node)

--- packages/frinx-workflow-builder/src/helpers/api.helpers.ts
+++ packages/frinx-workflow-builder/src/helpers/api.helpers.ts
@@ -1,7 +1,6 @@
 import { partition } from 'lodash';
-import { Children } from 'react';
-import { Elements, isNode, isEdge, Edge, Node, getConnectedEdges, getOutgoers, getIncomers } from 'react-flow-renderer';
-import { Task, Workflow } from './types';
+import { Elements, isNode, Node, getConnectedEdges, getOutgoers, getIncomers } from 'react-flow-renderer';
+import { Task } from './types';
 import unwrap from './unwrap';
 
 function convertNodeToTask(node: Node): Task {
@@ -21,6 +20,18 @@ function findJoinNode(elements: Elements, forkNode: Node, depth: number): Node {
   throw Error('no valid join was found');
 }
 
+function findDecisionEndNode(elements: Elements, forkNode: Node, depth: number): Node {
+  const children = getOutgoers(forkNode, elements);
+  for (const ch of children) {
+    if (getIncomers(ch, elements).length > 1) {
+      return ch;
+    }
+    return findDecisionEndNode(elements, ch, depth + 1);
+  }
+
+  throw Error('no valid decision end was found');
+}
+
 function traverseElements(
   tasks: Task[],
   elements: Elements,
@@ -32,11 +43,32 @@ function traverseElements(
     return tasks;
   }
 
-  const [nodes] = partition(elements, isNode);
+  const [nodes, edges] = partition(elements, isNode);
 
   const currentNode = unwrap(nodes.find((n) => n.id === id));
   const currentTask = convertNodeToTask(currentNode);
   const children = getOutgoers(currentNode, elements);
+
+  // DECISION
+  if (currentTask.type === 'DECISION') {
+    const decisionEndNode = findDecisionEndNode(elements, currentNode, depth);
+
+    const decisionTasks: [string, Task[]][] = children.map((decision) => {
+      const connectionEdges = getConnectedEdges([decision], edges);
+      const startBranchEdge = unwrap(connectionEdges.find((e) => e.source === currentTask.taskReferenceName));
+      const decisionStartNode = unwrap(nodes.find((n) => n.id === startBranchEdge?.target));
+      const decisionTasks = traverseElements([], elements, decisionStartNode.id, decisionEndNode.id);
+      return [unwrap(startBranchEdge.sourceHandle), decisionTasks];
+    });
+
+    const [decisionCases, defaultCase] = partition(decisionTasks, (d) => d[0] !== 'default');
+
+    currentTask.decisionCases = decisionCases.reduce((acc, cur) => ({ ...acc, [cur[0]]: cur[1] }), {});
+    currentTask.defaultCase = defaultCase[0][1];
+
+    const nextTasks: Task[] = traverseElements([], elements, decisionEndNode.id, 'end');
+    return [...tasks, currentTask, ...nextTasks];
+  }
 
   // FORK JOIN
   if (currentTask.type === 'FORK_JOIN') {
