d5172e22bea7989ad36da85758823756995a7dc2
Martin Sottnik <msottnik@gmail.com>
2022-02-17 12:58:24 +0100

    small refactoring

--- packages/frinx-workflow-builder/src/helpers/api.helpers.ts
+++ packages/frinx-workflow-builder/src/helpers/api.helpers.ts
@@ -1,6 +1,6 @@
 import { partition } from 'lodash';
 import { Elements, isNode, Node, getConnectedEdges, getOutgoers, getIncomers } from 'react-flow-renderer';
-import { Task } from './types';
+import { DecisionTask, ForkTask, Task } from './types';
 import unwrap from './unwrap';
 
 function convertNodeToTask(node: Node): Task {
@@ -31,7 +31,7 @@ function findDecisionEndNode(elements: Elements, node: Node, depth: number): Nod
   // eslint-disable-next-line no-restricted-syntax
   for (const ch of children) {
     newDepth = isConnectionNode(ch, elements) ? newDepth - 1 : newDepth;
-    newDepth = ch.data.task.type === 'DECISION' ? newDepth + 1 : newDepth;
+    newDepth = ch.data.task.type === 'DECISION' || ch.data.task.type === 'FORK_JOIN' ? newDepth + 1 : newDepth;
 
     if (getIncomers(ch, elements).length > 1 && depth === 0) {
       return ch;
@@ -42,53 +42,63 @@ function findDecisionEndNode(elements: Elements, node: Node, depth: number): Nod
   throw Error('no valid decision end was found');
 }
 
+function getDecisionTask(tasks: Task[], elements: Elements, currentNode: Node): Task[] {
+  const [nodes, edges] = partition(elements, isNode);
+  const currentTask = convertNodeToTask(currentNode) as DecisionTask;
+  const children = getOutgoers(currentNode, elements);
+  const decisionEndNode = findDecisionEndNode(elements, currentNode, 0);
+
+  const decisionTasks: [string, Task[]][] = children.map((decision) => {
+    const connectionEdges = getConnectedEdges([decision], edges);
+    const startBranchEdge = unwrap(connectionEdges.find((e) => e.source === currentTask.taskReferenceName));
+    const decisionStartNode = unwrap(nodes.find((n) => n.id === startBranchEdge?.target));
+    const currentDecisionTasks = traverseElements([], elements, decisionStartNode.id, decisionEndNode.id); // eslint-disable-line @typescript-eslint/no-use-before-define
+    return [unwrap(startBranchEdge.sourceHandle), currentDecisionTasks];
+  });
+
+  const [decisionCases, defaultCase] = partition(decisionTasks, (d) => d[0] !== 'default');
+
+  currentTask.decisionCases = decisionCases.reduce((acc, cur) => ({ ...acc, [cur[0]]: cur[1] }), {});
+  const defaultCaseTasks = defaultCase[0][1];
+  currentTask.defaultCase = defaultCaseTasks;
+
+  const nextTasks: Task[] = traverseElements([], elements, decisionEndNode.id, 'end'); // eslint-disable-line @typescript-eslint/no-use-before-define
+  return [...tasks, currentTask, ...nextTasks];
+}
+
+function getForkTask(tasks: Task[], elements: Elements, currentNode: Node): Task[] {
+  const currentTask = convertNodeToTask(currentNode) as ForkTask;
+  const children = getOutgoers(currentNode, elements);
+  const joinNode = findJoinNode(elements, currentNode, 0);
+
+  currentTask.forkTasks = children.map((fork) => {
+    return traverseElements([], elements, fork.id, joinNode.id); // eslint-disable-line @typescript-eslint/no-use-before-define
+  });
+
+  const nextTasks = traverseElements([], elements, joinNode.id, 'end'); // eslint-disable-line @typescript-eslint/no-use-before-define
+
+  return [...tasks, currentTask, ...nextTasks];
+}
+
 function traverseElements(tasks: Task[], elements: Elements, id: string, endId = 'end', depth = 0): Task[] {
   if (id === endId) {
     return tasks;
   }
 
-  const [nodes, edges] = partition(elements, isNode);
+  const [nodes] = partition(elements, isNode);
 
   const currentNode = unwrap(nodes.find((n) => n.id === id));
   const currentTask = convertNodeToTask(currentNode);
   const children = getOutgoers(currentNode, elements);
 
-  // DECISION
   if (currentTask.type === 'DECISION') {
-    const decisionEndNode = findDecisionEndNode(elements, currentNode, 0);
-
-    const decisionTasks: [string, Task[]][] = children.map((decision) => {
-      const connectionEdges = getConnectedEdges([decision], edges);
-      const startBranchEdge = unwrap(connectionEdges.find((e) => e.source === currentTask.taskReferenceName));
-      const decisionStartNode = unwrap(nodes.find((n) => n.id === startBranchEdge?.target));
-      const currentDecisionTasks = traverseElements([], elements, decisionStartNode.id, decisionEndNode.id);
-      return [unwrap(startBranchEdge.sourceHandle), currentDecisionTasks];
-    });
-
-    const [decisionCases, defaultCase] = partition(decisionTasks, (d) => d[0] !== 'default');
-
-    currentTask.decisionCases = decisionCases.reduce((acc, cur) => ({ ...acc, [cur[0]]: cur[1] }), {});
-    const defaultCaseTasks = defaultCase[0][1];
-    currentTask.defaultCase = defaultCaseTasks;
-
-    const nextTasks: Task[] = traverseElements([], elements, decisionEndNode.id, 'end');
-    return [...tasks, currentTask, ...nextTasks];
+    return getDecisionTask([], elements, currentNode);
   }
 
-  // FORK JOIN
   if (currentTask.type === 'FORK_JOIN') {
-    const joinNode = findJoinNode(elements, currentNode, depth);
-
-    currentTask.forkTasks = children.map((fork) => {
-      return traverseElements([], elements, fork.id, joinNode.id, depth + 1);
-    });
-
-    const nextTasks = traverseElements([], elements, joinNode.id, 'end', depth);
-
-    return [...tasks, currentTask, ...nextTasks];
+    return getForkTask([], elements, currentNode);
   }
 
-  // SIMPLE
   const nextTasks = children.map((n) => {
     return traverseElements([], elements, n.id, endId, depth);
   });
