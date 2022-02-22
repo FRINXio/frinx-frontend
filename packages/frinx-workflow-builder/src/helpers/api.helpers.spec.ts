455f0b2dba9f968cbf7ff62066e77c6c3568cdb3
Martin Sottnik <msottnik@gmail.com>
2022-02-22 09:48:44 +0100

    transformation tests added (#400)

---
+++ packages/frinx-workflow-builder/src/helpers/api.helpers.spec.ts
@@ -0,0 +1,32 @@
+import { assert, describe, test } from 'vitest';
+import { readFileSync } from 'fs';
+import { ExtendedTask, Workflow } from './types';
+import { getElementsFromWorkflow } from './data.helpers';
+import { convertToTasks } from './api.helpers';
+
+function loadWorkflow(fileName: string): Workflow<ExtendedTask> {
+  return JSON.parse(readFileSync(`./src/helpers/workflows/${fileName}`).toString());
+}
+
+describe('api helpers', () => {
+  test('decision workflow to api transformation', () => {
+    const decisionWorkflow = loadWorkflow('decision_workflow.json');
+    const elements = getElementsFromWorkflow(decisionWorkflow);
+    const transformedDecisionWorkflow = convertToTasks(elements);
+    assert.deepEqual(decisionWorkflow.tasks, transformedDecisionWorkflow);
+  });
+
+  test('nested decision workflow to api transformation', () => {
+    const decisionWorkflow = loadWorkflow('nested_decision_workflow.json');
+    const elements = getElementsFromWorkflow(decisionWorkflow);
+    const transformedDecisionWorkflow = convertToTasks(elements);
+    assert.deepEqual(decisionWorkflow.tasks, transformedDecisionWorkflow);
+  });
+
+  test('fork workflow to api transformation', () => {
+    const forkWorkflow = loadWorkflow('fork_workflow.json');
+    const elements = getElementsFromWorkflow(forkWorkflow);
+    const transformedForkWorkflow = convertToTasks(elements);
+    assert.deepEqual(forkWorkflow.tasks, transformedForkWorkflow);
+  });
+});
