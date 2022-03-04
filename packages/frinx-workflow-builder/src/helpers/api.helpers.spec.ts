f1e174cb7c00d9cbdb7895c19fdd65c53a786e09
Martin Sottnik <msottnik@gmail.com>
2022-03-04 12:25:36 +0100

    fix workflow with decision node ending

--- packages/frinx-workflow-builder/src/helpers/api.helpers.spec.ts
+++ packages/frinx-workflow-builder/src/helpers/api.helpers.spec.ts
@@ -11,22 +11,29 @@ function loadWorkflow(fileName: string): Workflow<ExtendedTask> {
 describe('api helpers', () => {
   test('decision workflow to api transformation', () => {
     const decisionWorkflow = loadWorkflow('decision_workflow.json');
-    const elements = getElementsFromWorkflow(decisionWorkflow);
+    const elements = getElementsFromWorkflow(decisionWorkflow.tasks);
     const transformedDecisionWorkflow = convertToTasks(elements);
     assert.deepEqual(decisionWorkflow.tasks, transformedDecisionWorkflow);
   });
 
   test('nested decision workflow to api transformation', () => {
     const decisionWorkflow = loadWorkflow('nested_decision_workflow.json');
-    const elements = getElementsFromWorkflow(decisionWorkflow);
+    const elements = getElementsFromWorkflow(decisionWorkflow.tasks);
     const transformedDecisionWorkflow = convertToTasks(elements);
     assert.deepEqual(decisionWorkflow.tasks, transformedDecisionWorkflow);
   });
 
   test('fork workflow to api transformation', () => {
     const forkWorkflow = loadWorkflow('fork_workflow.json');
-    const elements = getElementsFromWorkflow(forkWorkflow);
+    const elements = getElementsFromWorkflow(forkWorkflow.tasks);
     const transformedForkWorkflow = convertToTasks(elements);
     assert.deepEqual(forkWorkflow.tasks, transformedForkWorkflow);
+  });
+
+  test('uni_tx_start workflow to api transformation', () => {
+    const decisionWorkflow = loadWorkflow('uni_tx_start_workflow.json');
+    const elements = getElementsFromWorkflow(decisionWorkflow.tasks);
+    const transformedDecisionWorkflow = convertToTasks(elements);
+    assert.deepEqual(decisionWorkflow.tasks, transformedDecisionWorkflow);
   });
 });
