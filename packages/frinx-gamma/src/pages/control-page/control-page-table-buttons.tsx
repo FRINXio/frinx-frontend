005c14bbeea182b90a3d42285762064623a39830
Martin Sottnik <msottnik@gmail.com>
2022-04-04 16:49:27 +0200

    unified discard button added (#610)

--- packages/frinx-gamma/src/pages/control-page/control-page-table-buttons.tsx
+++ packages/frinx-gamma/src/pages/control-page/control-page-table-buttons.tsx
@@ -4,27 +4,12 @@ import React, { VoidFunctionComponent } from 'react';
 
 type Props = {
   onCommitBtnClick: () => void;
-  onDiscardBtnClick: () => void;
   isCommitLoading: boolean;
-  isDiscardLoading: boolean;
 };
 
-const ControlPageTableButtons: VoidFunctionComponent<Props> = ({
-  onCommitBtnClick,
-  onDiscardBtnClick,
-  isCommitLoading,
-  isDiscardLoading,
-}) => {
+const ControlPageTableButtons: VoidFunctionComponent<Props> = ({ onCommitBtnClick, isCommitLoading }) => {
   return (
     <HStack spacing={2}>
-      <Tooltip label="Discard changes">
-        <IconButton
-          aria-label="Discard changes"
-          icon={<Icon as={FeatherIcon} icon="trash" />}
-          onClick={onDiscardBtnClick}
-          isLoading={isDiscardLoading}
-        />
-      </Tooltip>
       <Tooltip label="Commit changes">
         <IconButton
           aria-label="Commit changes"
