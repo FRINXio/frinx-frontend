03aec670a46ac31ec975657f5bcaa1b72f24aceb
Martin Sottnik <msottnik@gmail.com>
2022-06-13 11:11:50 +0200

    executed and hierarchical changes 1 (#739)
    * executed and hierarchical changes
    
    * fix types
    
    * uniflow api changes
    
    * fix types
    
    * types fix 2
    
    * fix types 3

--- packages/frinx-workflow-ui/src/pages/workflow-list/executed-workflow-list/executed-workflow-list.tsx
+++ packages/frinx-workflow-ui/src/pages/workflow-list/executed-workflow-list/executed-workflow-list.tsx
@@ -1,28 +1,18 @@
 import PageContainer from '../../../common/PageContainer';
-import React, { FC, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Progress } from '@chakra-ui/react';
-import {
-  ExecutedWorkflowsFlat,
-  ExecutedWorkflowsHierarchical,
-  NestedExecutedWorkflow,
-} from '@frinx/workflow-ui/src/helpers/types';
+import { ExecutedWorkflows, NestedExecutedWorkflow } from '@frinx/workflow-ui/src/helpers/types';
 import ExecutedWorkflowSearchBox from './executed-workflow-searchbox/executed-workflow-searchbox';
 import { useNavigate } from 'react-router-dom';
-import {
-  fetchNewData,
-  fetchParentWorkflows,
-  getOrderValue,
-  getSortValue,
-  getValueOfProperty,
-  isValid,
-} from './search-execs';
-import ExecutedWorkflowHierarchicalTable from './executed-workflow-table/executed-workflow-hierarchical-table/executed-workflow-hierarchical-table';
+import { getSortOrder, getWorkflows } from './search-execs';
 import ExecutedWorkflowFlatTable from './executed-workflow-table/executed-workflow-flat-table/executed-workflow-flat-table';
-import { orderBy } from 'lodash';
 import ExecutedWorkflowBulkOperationsBlock from './executed-workflow-bulk-operations-block/executed-workflow-bulk-operations';
 import Paginator from '@frinx/workflow-ui/src/common/pagination';
-import { usePagination } from '@frinx/workflow-ui/src/common/pagination-hook';
 import useQueryParams from '@frinx/workflow-ui/src/hooks/use-query-params';
+import usePagination from '../../../hooks/use-pagination';
+
+type SortBy = 'workflowId' | 'startTime' | 'endTime';
+type SortOrder = 'ASC' | 'DESC';
 
 type StateProps = {
   selectedWorkflows: string[];
@@ -31,25 +21,27 @@ type StateProps = {
   openParentWorkflows: NestedExecutedWorkflow[];
   isFlat: boolean;
   showChildren: NestedExecutedWorkflow[];
-  sort: number[];
+  sortBy: SortBy;
+  sortOrder: SortOrder;
   labels: string[];
 };
-const ITEMS_PER_PAGE = 20;
 
-const initialState = {
+const initialState: StateProps = {
   selectedWorkflows: [],
   detailsModal: false,
   workflowId: '',
   openParentWorkflows: [],
   isFlat: false,
   showChildren: [],
-  sort: [/*workflowId*/ 2, /*startTime*/ 0, /*endTime*/ 2],
+  sortBy: 'startTime',
+  sortOrder: 'DESC',
   labels: [],
 };
 
 const ExecutedWorkflowList = () => {
   const navigate = useNavigate();
   const query = useQueryParams();
+  const [pagination, setPagination] = usePagination();
   const searchKeyword = query.get('search') || '';
 
   const [state, setState] = useState<StateProps>({
@@ -57,34 +49,32 @@ const ExecutedWorkflowList = () => {
     workflowId: searchKeyword,
   });
 
-  const [flatWorkflows, setFlatWorkflows] = useState<ExecutedWorkflowsFlat | null>(null);
-  const [hierarchicalWorkflows, setHierarchicalWorkflows] = useState<ExecutedWorkflowsHierarchical | null>(null);
-  const hierarchicalPagination = usePagination([], ITEMS_PER_PAGE);
-  const flatViewPagination = usePagination([], ITEMS_PER_PAGE);
+  const [workflows, setWorkflows] = useState<ExecutedWorkflows | null>(null);
 
   useEffect(() => {
-    fetchNewData(
-      state.workflowId,
-      flatViewPagination.currentPage,
-      flatViewPagination.maxItemsPerPage,
-      state.labels,
-    ).then((response) => {
-      setFlatWorkflows(response);
-    });
-    fetchParentWorkflows(
-      state.workflowId,
-      hierarchicalPagination.currentPage,
-      hierarchicalPagination.maxItemsPerPage,
-      state.labels,
-    ).then((response) => {
-      setHierarchicalWorkflows(response);
-    });
+    const fetchData = async () => {
+      const executedWorkflows = await getWorkflows(
+        state.workflowId,
+        state.labels,
+        (pagination.page - 1) * pagination.pageSize,
+        pagination.pageSize,
+        state.sortBy,
+        state.sortOrder,
+        state.isFlat,
+      );
+
+      setWorkflows(executedWorkflows);
+      setPagination((prev) => ({ ...prev, pageCount: Math.ceil(executedWorkflows.result.totalHits / prev.pageSize) }));
+    };
+    fetchData();
   }, [
-    state,
-    hierarchicalPagination.currentPage,
-    hierarchicalPagination.maxItemsPerPage,
-    flatViewPagination.currentPage,
-    flatViewPagination.maxItemsPerPage,
+    state.workflowId,
+    state.isFlat,
+    state.sortOrder,
+    state.sortBy,
+    pagination.page,
+    pagination.pageSize,
+    state.labels,
   ]);
 
   useEffect(() => {
@@ -95,77 +85,18 @@ const ExecutedWorkflowList = () => {
     setState((prev) => ({ ...prev, selectedWorkflows: [...new Set<string>()] }));
   }, [state.isFlat]);
 
-  if (hierarchicalWorkflows == null || flatWorkflows == null) {
+  if (workflows == null) {
     return <Progress isIndeterminate size="xs" marginTop={-10} />;
   }
 
-  const update = (openParents: NestedExecutedWorkflow[], showChildren: NestedExecutedWorkflow[]) => {
-    setState((prev) => {
-      return {
-        ...prev,
-        openParentWorkflows: openParents,
-        showChildren,
-      };
-    });
-  };
-
-  const showChildrenWorkflows = (
-    workflow: NestedExecutedWorkflow,
-    children: NestedExecutedWorkflow[],
-    closeParentWorkflows: NestedExecutedWorkflow[] | null,
-    closeChildWorkflows: NestedExecutedWorkflow[] | null,
-  ) => {
-    let showChildren = closeChildWorkflows ? closeChildWorkflows : state.showChildren;
-    let openParents = closeParentWorkflows ? closeParentWorkflows : state.openParentWorkflows;
-
-    if (openParents.filter((Workflows) => Workflows.startTime === workflow.startTime).length) {
-      const closeParents = openParents.filter((workflow) => workflow.parentWorkflowId === workflow.workflowId);
-      openParents = openParents.filter((Workflows) => Workflows.startTime !== workflow.startTime);
-      showChildren = showChildren.filter((workflow) => workflow.parentWorkflowId !== workflow.workflowId);
-      closeParents.length
-        ? closeParents.forEach((open) => showChildrenWorkflows(open, children, openParents, showChildren))
-        : update(openParents, showChildren);
-    } else {
-      openParents.push(workflow);
-      showChildren = showChildren.concat(
-        children.filter((workflow) => workflow.parentWorkflowId === workflow.workflowId),
-      );
-      update(openParents, showChildren);
-    }
-  };
-
   const changeLabels = (labels: string[]) => {
     setState((prev) => ({ ...prev, labels }));
-    hierarchicalPagination.setCurrentPage(1);
-    flatViewPagination.setCurrentPage(1);
+    setPagination((prev) => ({ ...prev, page: 1 }));
   };
 
   const changeQuery = (query: string) => {
     setState((prev) => ({ ...prev, workflowId: query }));
-    hierarchicalPagination.setCurrentPage(1);
-    flatViewPagination.setCurrentPage(1);
-  };
-
-  const indent = (workflows: NestedExecutedWorkflow[], i: number, size?: number) => {
-    const indentSize = size ? size : 6;
-    if ('parentWorkflowId' in workflows[i]) {
-      let layers = 0;
-      if (state.showChildren.some((child) => child.workflowId === workflows[i].parentWorkflowId)) {
-        let parent = workflows[i];
-        while (parent.parentWorkflowId) {
-          layers++;
-          parent = workflows[workflows.findIndex((id) => id.workflowId === parent.parentWorkflowId)];
-          if (layers > 10) break;
-        }
-        return layers * indentSize + 'px';
-      }
-      return indentSize + 'px';
-    }
-    return '0px';
-  };
-
-  const setView = () => {
-    setState((prev) => ({ ...prev, isFlat: !prev.isFlat }));
+    setPagination((prev) => ({ ...prev, page: 1 }));
   };
 
   const selectWorkflow = (workflowId: string, isChecked: boolean) => {
@@ -188,12 +119,7 @@ const ExecutedWorkflowList = () => {
   const selectAllWorkflows = (isChecked: boolean) => {
     if (isChecked) {
       setState((prev) => {
-        let selectedWorkflows: Set<string>;
-        if (state.isFlat) {
-          selectedWorkflows = new Set(flatWorkflows.result.hits.map((workflow) => workflow.workflowId));
-        } else {
-          selectedWorkflows = new Set(hierarchicalWorkflows.parents.map((workflow) => workflow.workflowId));
-        }
+        const selectedWorkflows = new Set(workflows.result.hits.map((workflow) => workflow.workflowId));
 
         return { ...prev, selectedWorkflows: [...selectedWorkflows] };
       });
@@ -202,74 +128,27 @@ const ExecutedWorkflowList = () => {
     }
   };
 
-  const selectChildrenworkflow = (parentId: string, workflowIds: string[]) => {
-    const { children } = hierarchicalWorkflows;
-    const newWorkflowIds = children
-      .filter((workflow: NestedExecutedWorkflow) => workflow.parentWorkflowId === parentId)
-      .map((workflow: NestedExecutedWorkflow) => workflow.workflowId);
-    for (let i = 0; i < newWorkflowIds.length; i++)
-      workflowIds = workflowIds.concat(selectChildrenworkflow(newWorkflowIds[i], newWorkflowIds));
-    return [...new Set(workflowIds)];
-  };
-
-  const sortWorkflow = (sortId: number) => {
-    const sort = state.sort.map((value, i) => (sortId === i ? getSortValue(value) : 2));
-    if (!state.isFlat) {
-      state.openParentWorkflows.forEach((parent) =>
-        showChildrenWorkflows(parent, hierarchicalWorkflows.children, null, null),
-      );
-      update([], []);
-    }
+  const sortWorkflow = (sortBy: SortBy) => {
     setState((prev) => ({
       ...prev,
-      sort,
+      sortBy,
+      sortOrder: getSortOrder(sortBy, prev.sortBy, prev.sortOrder),
     }));
   };
 
   const changeView = () => {
-    setView();
+    setState((prev) => ({ ...prev, isFlat: !prev.isFlat }));
+    setPagination((prev) => ({ ...prev, page: 1 }));
   };
 
   const handlePaginationClick = (pageNumber: number) => {
-    if (state.isFlat) {
-      flatViewPagination.setCurrentPage(pageNumber);
-    } else {
-      hierarchicalPagination.setCurrentPage(pageNumber);
-    }
+    setPagination((prev) => ({ ...prev, page: pageNumber }));
   };
-
-  const hierarchy: ExecutedWorkflowsHierarchical = {
-    parents: orderBy(
-      hierarchicalWorkflows.parents.filter((parent) => isValid(parent, state.workflowId, state.labels)) ?? [],
-      [getValueOfProperty(state.sort)],
-      [getOrderValue(state.sort)],
-    ),
-    children: orderBy(
-      hierarchicalWorkflows.children.filter((child) => isValid(child, state.workflowId, state.labels)) ?? [],
-      [getValueOfProperty(state.sort)],
-      [getOrderValue(state.sort)],
-    ),
-    count: hierarchicalWorkflows.count ?? 0,
-    hits: hierarchicalWorkflows.hits ?? 0,
-  };
-
-  const flat: ExecutedWorkflowsFlat = {
-    result: {
-      hits: orderBy(
-        flatWorkflows.result.hits.filter((workflow) => isValid(workflow, state.workflowId, state.labels)) ?? [],
-        [getValueOfProperty(state.sort)],
-        [getOrderValue(state.sort)],
-      ),
-      totalHits: flatWorkflows.result.totalHits ?? 0,
-    },
-  };
-
-  const workflowsAmount = state.isFlat ? flat.result.totalHits : hierarchy.parents.length;
 
   return (
     <PageContainer>
       <ExecutedWorkflowBulkOperationsBlock
-        workflowsAmount={workflowsAmount}
+        workflowsAmount={workflows.result.totalHits}
         selectedWorkflows={state.selectedWorkflows}
         selectAllWorkflows={selectAllWorkflows}
       />
@@ -280,51 +159,26 @@ const ExecutedWorkflowList = () => {
         changeQuery={changeQuery}
         changeView={changeView}
         labels={state.labels}
-        query={state.workflowId}
       />
 
-      {!state.isFlat && (
-        <>
-          <ExecutedWorkflowHierarchicalTable
-            selectAllWorkflows={selectAllWorkflows}
-            sortWf={sortWorkflow}
-            indent={indent}
-            hierarchicalWorkflows={hierarchy}
-            openParentWfs={state.openParentWorkflows}
-            selectWf={selectWorkflow}
-            selectedWfs={state.selectedWorkflows}
-            showChildrenWorkflows={showChildrenWorkflows}
-            sort={state.sort}
-          />
+      <>
+        <ExecutedWorkflowFlatTable
+          selectAllWorkflows={selectAllWorkflows}
+          sortWf={sortWorkflow}
+          selectWf={selectWorkflow}
+          selectedWfs={state.selectedWorkflows}
+          sortBy={state.sortBy}
+          sortOrder={state.sortOrder}
+          workflows={workflows}
+        />
 
-          <Paginator
-            currentPage={hierarchicalPagination.currentPage}
-            onPaginationClick={handlePaginationClick}
-            pagesCount={Math.ceil(hierarchy.hits / ITEMS_PER_PAGE)}
-            showPageNumbers={true}
-          />
-        </>
-      )}
-
-      {state.isFlat && (
-        <>
-          <ExecutedWorkflowFlatTable
-            selectAllWorkflows={selectAllWorkflows}
-            sortWf={sortWorkflow}
-            selectWf={selectWorkflow}
-            selectedWfs={state.selectedWorkflows}
-            sort={state.sort}
-            flatWorkflows={flat}
-          />
-
-          <Paginator
-            currentPage={flatViewPagination.currentPage}
-            onPaginationClick={handlePaginationClick}
-            pagesCount={Math.ceil(workflowsAmount / ITEMS_PER_PAGE)}
-            showPageNumbers={true}
-          />
-        </>
-      )}
+        <Paginator
+          currentPage={pagination.page}
+          onPaginationClick={handlePaginationClick}
+          pagesCount={pagination.pageCount}
+          showPageNumbers={true}
+        />
+      </>
     </PageContainer>
   );
 };
