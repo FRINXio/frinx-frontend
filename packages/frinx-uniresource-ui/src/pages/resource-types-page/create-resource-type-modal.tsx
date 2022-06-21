3cb924aed34dd949c9bb459adc9ab8fe97ccd9c4
Marco <marcomruz1@gmail.com>
2022-06-21 16:18:50 +0200

    add new fresh design for resource types page

---
+++ packages/frinx-uniresource-ui/src/pages/resource-types-page/create-resource-type-modal.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import {
+  Button,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Spacer,
+} from '@chakra-ui/react';
+import * as yup from 'yup';
+import { useFormik } from 'formik';
+
+type Props = {
+  isOpen: boolean;
+  onClose: () => void;
+  onCreate: (resourceTypeName: string) => void;
+};
+
+const validationSchema = yup.object({
+  resourceTypeName: yup.string().required('Resource type name is required'),
+});
+
+export const CreateResourceTypeModal = ({ isOpen, onClose, onCreate }: Props) => {
+  const { errors, values, handleChange, submitForm, isSubmitting, isValidating } = useFormik({
+    initialValues: {
+      resourceTypeName: '',
+    },
+    validationSchema,
+    onSubmit: (data) => {
+      onCreate(data.resourceTypeName);
+      onClose();
+    },
+  });
+
+  return (
+    <Modal onClose={onClose} isOpen={isOpen} isCentered>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalCloseButton />
+        <ModalHeader>Create Resource Type</ModalHeader>
+        <ModalBody>
+          <FormControl isRequired isInvalid={errors.resourceTypeName != null}>
+            <FormLabel>Resource Type name</FormLabel>
+            <Input name="resourceTypeName" onChange={handleChange} value={values.resourceTypeName} />
+            <FormErrorMessage>{errors.resourceTypeName}</FormErrorMessage>
+          </FormControl>
+        </ModalBody>
+        <ModalFooter>
+          <Spacer />
+          <Button isLoading={isSubmitting || isValidating} colorScheme="blue" onClick={submitForm} type="submit">
+            Create
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
---
+++ packages/frinx-uniresource-ui/src/pages/resource-types-page/resource-types-table.tsx
@@ -0,0 +1,44 @@
+import { DeleteIcon } from '@chakra-ui/icons';
+import { Table, Thead, Tr, Th, Tbody, Td, HStack, IconButton } from '@chakra-ui/react';
+import React from 'react';
+import { ResourceTypesQuery } from '../../__generated__/graphql';
+
+type Props = {
+  resourceTypes: ResourceTypesQuery['QueryResourceTypes'];
+  onDelete: (id: string) => void;
+};
+
+export const ResourceTypesTable = ({ resourceTypes, onDelete }: Props) => {
+  return (
+    <Table backgroundColor="white">
+      <Thead>
+        <Tr>
+          <Th>ID</Th>
+          <Th>Name</Th>
+          <Th>Actions</Th>
+        </Tr>
+      </Thead>
+      <Tbody>
+        {resourceTypes.map((resourceType) => (
+          <Tr key={resourceType.id}>
+            <Td>{resourceType.id}</Td>
+            <Td>{resourceType.Name}</Td>
+            <Td>
+              <HStack>
+                <IconButton
+                  variant="outline"
+                  colorScheme="red"
+                  aria-label="delete"
+                  icon={<DeleteIcon />}
+                  onClick={() => {
+                    onDelete(resourceType.id);
+                  }}
+                />
+              </HStack>
+            </Td>
+          </Tr>
+        ))}
+      </Tbody>
+    </Table>
+  );
+};
