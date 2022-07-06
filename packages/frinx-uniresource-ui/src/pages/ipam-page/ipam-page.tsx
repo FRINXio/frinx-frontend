a2a50967fbf4a953abe0600b5cf89fab691bb505
MarcoMruz <marcomruz1@gmail.com>
2022-07-06 17:10:26 +0200

    add ipam page to menu and create ipam initial page - empty

---
+++ packages/frinx-uniresource-ui/src/pages/ipam-page/ipam-page.tsx
@@ -0,0 +1,8 @@
+import React, { VoidFunctionComponent } from 'react';
+import { Heading } from '@chakra-ui/react';
+
+const IpamPage: VoidFunctionComponent = () => {
+  return <Heading>IPAM page</Heading>;
+};
+
+export default IpamPage;
