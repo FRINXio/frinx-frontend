08a95a21df80f5efcdac8ded57615aeb60ef127d
MarcoMruz <marcomruz1@gmail.com>
2022-07-12 14:30:16 +0200

    add aggregates page and separate component for aggregates table

---
+++ packages/frinx-uniresource-ui/src/pages/ipam-page/aggregates-table.tsx
@@ -0,0 +1,92 @@
+import {
+  TableContainer,
+  Table,
+  Thead,
+  Tr,
+  Th,
+  Tbody,
+  Td,
+  Tag,
+  HStack,
+  Progress,
+  Link as ChakraLink,
+  Text,
+} from '@chakra-ui/react';
+import React, { VoidFunctionComponent } from 'react';
+import { Link } from 'react-router-dom';
+
+type Aggregate = {
+  id: string;
+  aggregate: string;
+  prefixes: number;
+  freeCapacity: string | undefined;
+  utilizedCapacity: string | undefined;
+  tags: {
+    tag: string;
+    id: string;
+  }[];
+};
+
+type Props = {
+  aggregates: Aggregate[];
+  onTagClick: (tag: string) => void;
+};
+
+const AggregatesTable: VoidFunctionComponent<Props> = ({ aggregates, onTagClick }) => {
+  return (
+    <TableContainer background="white">
+      <Table size="sm">
+        <Thead>
+          <Tr bgColor="gray.200">
+            <Th>Aggregate</Th>
+            <Th>Prefixes</Th>
+            <Th>Tags</Th>
+            <Th>Capacity utilization</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {aggregates == null || aggregates.length === 0 ? (
+            <Tr>
+              <Td>Currently there are no aggregates created yet</Td>
+            </Tr>
+          ) : (
+            aggregates.map(({ id, aggregate, freeCapacity, prefixes, utilizedCapacity, tags }) => {
+              const capacityUtilization = Math.floor(
+                (Number(utilizedCapacity) / (Number(freeCapacity) + Number(utilizedCapacity))) * 100,
+              );
+              return (
+                <Tr key={id}>
+                  <Td>{aggregate}</Td>
+                  {prefixes > 0 ? (
+                    <Td>
+                      <ChakraLink as={Link} to={`/uniresource/pools/nested/${id}`} color="blue">
+                        {prefixes}
+                      </ChakraLink>
+                    </Td>
+                  ) : (
+                    <Td>{prefixes}</Td>
+                  )}
+                  <Td>
+                    {tags.map(({ tag, id: tagId }) => (
+                      <Tag key={tagId} ml={1} onClick={() => onTagClick(tag)} cursor="pointer">
+                        {tag}
+                      </Tag>
+                    ))}
+                  </Td>
+                  <Td>
+                    <HStack>
+                      <Text>{capacityUtilization}%</Text>
+                      <Progress width="90%" value={capacityUtilization} />
+                    </HStack>
+                  </Td>
+                </Tr>
+              );
+            })
+          )}
+        </Tbody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default AggregatesTable;
---
+++ packages/frinx-uniresource-ui/src/pages/ipam-page/ipam-aggregates-page.tsx
@@ -0,0 +1,116 @@
+import { Heading, Progress, Text } from '@chakra-ui/react';
+import React, { VoidFunctionComponent } from 'react';
+import { gql, useQuery } from 'urql';
+import ipaddr from 'ipaddr.js';
+import { GetIpPoolsQuery, GetIpPoolsQueryVariables } from '../../__generated__/graphql';
+import useTags from '../../hooks/use-tags';
+import useMinisearch from '../../hooks/use-minisearch';
+import SearchFilterPoolsBar from '../../components/search-filter-pools-bar';
+import AggregatesTable from './aggregates-table';
+
+const GET_IP_POOLS = gql`
+  query GetIpPools {
+    QueryRootResourcePools {
+      id
+      Name
+      Tags {
+        id
+        Tag
+      }
+      ResourceType {
+        id
+        Name
+      }
+      Resources {
+        id
+        NestedPool {
+          id
+          Name
+        }
+      }
+      Capacity {
+        freeCapacity
+        utilizedCapacity
+      }
+      PoolProperties
+    }
+  }
+`;
+
+const isIpv4 = (name: string) => name === 'ipv4_prefix';
+
+const IpamAggregatesPage: VoidFunctionComponent = () => {
+  const [{ data: aggregatesQuery, fetching, error }] = useQuery<GetIpPoolsQuery, GetIpPoolsQueryVariables>({
+    query: GET_IP_POOLS,
+  });
+
+  const [selectedTags, { clearAllTags, handleOnTagClick }] = useTags();
+  const { results, searchText, setSearchText } = useMinisearch({
+    items: aggregatesQuery?.QueryRootResourcePools,
+    searchFields: ['PoolProperties'],
+    extractField: (document, fieldName) => {
+      if (fieldName === 'PoolProperties') {
+        return JSON.stringify(document[fieldName]);
+      }
+
+      return document[fieldName];
+    },
+  });
+
+  const handleOnClearSearch = () => {
+    setSearchText('');
+    clearAllTags();
+  };
+
+  if (fetching) {
+    return <Progress isIndeterminate size="xs" mt={-20} />;
+  }
+
+  if (error != null) {
+    return <Text>No aggregates exists</Text>;
+  }
+
+  const aggregates = results
+    .filter(
+      (aggregate) => aggregate.ResourceType.Name === 'ipv4_prefix' || aggregate.ResourceType.Name === 'ipv6_prefix',
+    )
+    .map((aggregate) => {
+      const { address, prefix } = aggregate.PoolProperties;
+
+      const aggregateInfo = isIpv4(aggregate.ResourceType.Name)
+        ? `${ipaddr.IPv4.networkAddressFromCIDR(`${address}/${prefix}`)}/${prefix}`
+        : `${ipaddr.IPv6.networkAddressFromCIDR(`${address}/${prefix}`)}/${prefix}`;
+      return {
+        id: aggregate.id,
+        aggregate: aggregateInfo,
+        prefixes: aggregate.Resources.filter((resource) => resource.NestedPool != null).length,
+        freeCapacity: aggregate.Capacity?.freeCapacity,
+        utilizedCapacity: aggregate.Capacity?.utilizedCapacity,
+        tags: aggregate.Tags.map(({ id, Tag: tagName }) => ({ tag: tagName, id })),
+      };
+    });
+
+  const filteredAggregates =
+    selectedTags.length === 0
+      ? aggregates
+      : aggregates.filter(({ tags }) => tags.some(({ tag }) => selectedTags.includes(tag)));
+
+  return (
+    <>
+      <Heading as="h1" size="lg" mb={5}>
+        Aggregates
+      </Heading>
+      <SearchFilterPoolsBar
+        clearAllTags={clearAllTags}
+        onTagClick={handleOnTagClick}
+        searchText={searchText}
+        selectedTags={selectedTags}
+        setSearchText={setSearchText}
+        onClearSearch={handleOnClearSearch}
+      />
+      <AggregatesTable aggregates={filteredAggregates} onTagClick={handleOnTagClick} />
+    </>
+  );
+};
+
+export default IpamAggregatesPage;
