1922e9e05ed0b6d9093383df6b047e3a5e7dade4
Marco <51787428+MarcoMruz@users.noreply.github.com>
2022-07-18 12:33:12 +0200

    Create ip ranges page (#791)
    * create ip ranges page and add to menu
    
    * add table, filtering logic and create nested ip ranges detail page
    
    * update and fix filtering of ipam page also update UI of IPAM
    
    * rename ip ranges detail page
    
    * move resource pool actions and data loading to hook
    
    * update ui and handle disabled logic of buttons
    
    Co-authored-by: Pavol Porubský <paulooze@gmail.com>

---
+++ packages/frinx-uniresource-ui/src/pages/ipam-page/ip-ranges-table.tsx
@@ -0,0 +1,134 @@
+import {
+  TableContainer,
+  Table,
+  Thead,
+  Tr,
+  Th,
+  Tbody,
+  Td,
+  Tag,
+  IconButton,
+  Icon,
+  Button,
+  Link as ChakraLink,
+  ButtonGroup,
+} from '@chakra-ui/react';
+import React, { VoidFunctionComponent } from 'react';
+import { Link } from 'react-router-dom';
+import FeatherIcon from 'feather-icons-react';
+
+type IpRange = {
+  id: string;
+  name: string;
+  network: string;
+  broadcast: string;
+  tags: { Tag: string; id: string }[];
+  nestedRanges: {
+    id: string;
+    nestedPoolId: string | undefined;
+  }[];
+  size: bigint | number;
+  freeCapacity: bigint | number;
+};
+
+type Props = {
+  ipRanges: IpRange[];
+  onTagClick: (tagName: string) => void;
+  onGetAddressClick?: (poolId: string) => void;
+  onDeleteBtnClick?: (poolId: string) => void;
+};
+
+const IpRangesTable: VoidFunctionComponent<Props> = ({ ipRanges, onTagClick, onGetAddressClick, onDeleteBtnClick }) => {
+  return (
+    <TableContainer bgColor="white">
+      <Table size="sm">
+        <Thead bgColor="gray.200">
+          <Tr>
+            <Th>Network</Th>
+            <Th>Broadcast</Th>
+            <Th>Size</Th>
+            <Th>Name</Th>
+            <Th>Tags</Th>
+            <Th>Actions</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {ipRanges.length > 0 ? (
+            ipRanges.map(({ broadcast, network, size, tags, id, name, nestedRanges, freeCapacity }) => {
+              return (
+                <Tr key={id}>
+                  {nestedRanges.length > 0 ? (
+                    <Td>
+                      <ChakraLink as={Link} textColor="blue" to={`../ipam/ip-ranges/${id}/nested-ranges`}>
+                        {network}
+                      </ChakraLink>
+                    </Td>
+                  ) : (
+                    <Td>{network}</Td>
+                  )}
+                  <Td>{broadcast}</Td>
+                  <Td>{new Intl.NumberFormat('fr').format(size)}</Td>
+                  <Td>{name}</Td>
+                  <Td>
+                    {tags.map(({ Tag: tagName, id: tagId }) => (
+                      <Tag key={tagId} mr={1} onClick={() => onTagClick(tagName)} cursor="pointer">
+                        {tagName}
+                      </Tag>
+                    ))}
+                  </Td>
+                  <Td>
+                    <ButtonGroup spacing={1}>
+                      {onGetAddressClick && (
+                        <Button
+                          variant="solid"
+                          colorScheme="blue"
+                          size="xs"
+                          onClick={() => onGetAddressClick(id)}
+                          disabled={freeCapacity > 0}
+                        >
+                          Get address
+                        </Button>
+                      )}
+                      <IconButton
+                        aria-label="config"
+                        size="xs"
+                        variant="outline"
+                        icon={<Icon as={FeatherIcon} size={20} icon="settings" />}
+                        as={Link}
+                        to={`../pools/${id}`}
+                      />
+                      {onDeleteBtnClick && (
+                        <IconButton
+                          variant="outline"
+                          size="xs"
+                          colorScheme="red"
+                          aria-label="delete"
+                          icon={<Icon size={20} as={FeatherIcon} icon="trash-2" color="red" />}
+                          disabled={freeCapacity !== 0}
+                          onClick={() => {
+                            onDeleteBtnClick(id);
+                          }}
+                        />
+                      )}
+                    </ButtonGroup>
+                  </Td>
+                </Tr>
+              );
+            })
+          ) : (
+            <Tr>
+              <Td>
+                There are no ip ranges yet{' '}
+                <Button variant="solid" colorScheme="blue" as={Link} to="/uniresource/pools/new" size="xs" ml={5}>
+                  Create one
+                </Button>
+              </Td>
+            </Tr>
+          )}
+        </Tbody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default IpRangesTable;
---
+++ packages/frinx-uniresource-ui/src/pages/ipam-page/ipam-ip-ranges-detail-page/ipam-ip-ranges-detail-page.tsx
@@ -0,0 +1,123 @@
+import { Heading, Progress, useDisclosure } from '@chakra-ui/react';
+import { IPv4, IPv6 } from 'ipaddr.js';
+import { compact } from 'lodash';
+import React, { useState, VoidFunctionComponent } from 'react';
+import { useParams } from 'react-router-dom';
+import SearchFilterPoolsBar from '../../../components/search-filter-pools-bar';
+import useMinisearch from '../../../hooks/use-minisearch';
+import useTags from '../../../hooks/use-tags';
+import useResourcePoolActions from '../../../hooks/use-resource-pool-actions';
+import { ClaimAddressModal } from '../../pool-detail-page/claim-address-modal';
+import IpRangesTable from '../ip-ranges-table';
+
+const isIpv4 = (name: string) => name === 'ipv4_prefix';
+
+const getAddressesFromCIDR = (cidr: string, resourceTypeName: string) => ({
+  network: isIpv4(resourceTypeName)
+    ? IPv4.networkAddressFromCIDR(cidr).toString()
+    : IPv6.networkAddressFromCIDR(cidr).toString(),
+  broadcast: isIpv4(resourceTypeName)
+    ? IPv4.broadcastAddressFromCIDR(cidr).toString()
+    : IPv6.broadcastAddressFromCIDR(cidr).toString(),
+});
+
+const IpamNestedIpRangesDetailPage: VoidFunctionComponent = () => {
+  const { id } = useParams();
+  const { isOpen, onClose, onOpen } = useDisclosure();
+  const [poolId, setPoolId] = useState('');
+
+  const [{ poolDetail }, { handleOnClaimAddress, deleteResourcePool }] = useResourcePoolActions({ poolId: id });
+
+  const [selectedTags, { clearAllTags, handleOnTagClick }] = useTags();
+  const { results, searchText, setSearchText } = useMinisearch({
+    items: compact(
+      poolDetail.data?.QueryResourcePool.Resources.map((resource) => resource.NestedPool).filter(
+        (resource) => resource?.ResourceType.Name === 'ipv4_prefix' || resource?.ResourceType.Name === 'ipv6_prefix',
+      ),
+    ),
+    searchFields: ['Name', 'PoolProperties'],
+    extractField: (document, fieldName) => {
+      if (fieldName === 'PoolProperties') {
+        return JSON.stringify(document[fieldName]);
+      }
+
+      return document[fieldName];
+    },
+  });
+
+  const handleOnClearSearch = () => {
+    clearAllTags();
+    setSearchText('');
+  };
+
+  if (poolDetail.fetching) {
+    return <Progress isIndeterminate size="sm" mt={-10} />;
+  }
+
+  if (poolDetail.error != null) {
+    return <Heading>There was problem to load nested ip ranges</Heading>;
+  }
+
+  const nestedIpRanges = results
+    .filter(({ Tags }) => (selectedTags.length > 0 ? Tags.some(({ Tag: tag }) => selectedTags.includes(tag)) : true))
+    .map(({ Capacity, Name, PoolProperties, Tags, id: ipRangeId, ResourceType, Resources }) => {
+      const { network, broadcast } = getAddressesFromCIDR(
+        `${PoolProperties.address}/${PoolProperties.prefix}`,
+        ResourceType.Name,
+      );
+      return {
+        id: ipRangeId,
+        name: Name,
+        size: Capacity != null ? BigInt(Capacity.utilizedCapacity) + BigInt(Capacity.freeCapacity) : 0,
+        freeCapacity: Capacity != null ? BigInt(Capacity.freeCapacity) : 0,
+        tags: Tags,
+        network: `${network}/${PoolProperties.prefix}`,
+        broadcast: `${broadcast}/${PoolProperties.prefix}`,
+        nestedRanges: Resources.filter(({ NestedPool }) => NestedPool != null)
+          .filter(
+            (resource) =>
+              resource?.NestedPool?.ResourceType.Name === 'ipv4_prefix' ||
+              resource?.NestedPool?.ResourceType.Name === 'ipv6_prefix',
+          )
+          .map((resource) => ({
+            id: resource.id,
+            nestedPoolId: resource.NestedPool?.id,
+          })),
+      };
+    });
+
+  return (
+    <>
+      {poolId && (
+        <ClaimAddressModal
+          isOpen={isOpen}
+          onClose={onClose}
+          onClaimAddress={(formValues) => handleOnClaimAddress(poolId, formValues)}
+          resourceProperties={poolDetail.data?.QueryResourcePool.PoolProperties}
+        />
+      )}
+      <Heading as="h1" size="lg" mb={5}>
+        IP Ranges of {poolDetail.data?.QueryResourcePool.Name}
+      </Heading>
+      <SearchFilterPoolsBar
+        searchText={searchText}
+        setSearchText={setSearchText}
+        clearAllTags={clearAllTags}
+        selectedTags={selectedTags}
+        onTagClick={handleOnTagClick}
+        onClearSearch={handleOnClearSearch}
+      />
+      <IpRangesTable
+        ipRanges={nestedIpRanges}
+        onTagClick={handleOnTagClick}
+        onGetAddressClick={(selectedPoolId) => {
+          setPoolId(selectedPoolId);
+          onOpen();
+        }}
+        onDeleteBtnClick={deleteResourcePool}
+      />
+    </>
+  );
+};
+
+export default IpamNestedIpRangesDetailPage;
