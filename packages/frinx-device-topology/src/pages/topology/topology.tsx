9f78b3d7022c23636806a1849456d1c4f10faa03
Pavol Porubsky <paulooze@gmail.com>
2022-08-11 15:49:28 +0200

    add first version of topology graph

---
+++ packages/frinx-device-topology/src/pages/topology/topology.tsx
@@ -0,0 +1,51 @@
+import { Box, Container, Flex, Heading } from '@chakra-ui/react';
+import React, { VoidFunctionComponent } from 'react';
+import { gql, useQuery } from 'urql';
+import { TopologyQuery, TopologyQueryVariables } from '../../__generated__/graphql';
+import TopologyGraph from './topology-graph';
+
+const TOPOLOGY_QUERY = gql`
+  query Topology {
+    topology {
+      nodes {
+        id
+        device {
+          id
+          name
+        }
+      }
+      edges {
+        id
+        source
+        target
+      }
+    }
+  }
+`;
+
+const Topology: VoidFunctionComponent = () => {
+  const [{ data, fetching, error }] = useQuery<TopologyQuery, TopologyQueryVariables>({ query: TOPOLOGY_QUERY });
+
+  if (fetching || error) {
+    return null;
+  }
+
+  const nodes = data?.topology.nodes.map((n) => ({ id: n.id, name: n.device.name })) ?? [];
+
+  console.log({ nodes, edges: data?.topology.edges ?? [] });
+
+  return (
+    <Container maxWidth={1280}>
+      <Flex justify="space-between" align="center" marginBottom={6}>
+        <Heading as="h2" size="3xl">
+          Device topology
+        </Heading>
+      </Flex>
+      <Box>
+        <TopologyGraph data={{ nodes, edges: data?.topology.edges ?? [] }} />
+      </Box>
+    </Container>
+  );
+};
+
+export default Topology;
--- packages/frinx-device-topology/src/root.tsx
+++ packages/frinx-device-topology/src/root.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Topology from './pages/topology/topology';
 
 const Root = () => {
-  return <div>Root device topology</div>;
+  return (
+    <Routes>
+      <Route index element={<Topology />} />
+    </Routes>
+  );
 };
 
 export default Root;
