8664acf9ed191bbcf4e1eba93386761bc9f502d4
filipvnencak <vnencak.filip@gmail.com>
2022-08-12 12:12:09 +0200

    Search tags icon package replace

--- packages/frinx-uniresource-ui/src/components/search-tags.tsx
+++ packages/frinx-uniresource-ui/src/components/search-tags.tsx
@@ -1,6 +1,6 @@
-import React, { VoidFunctionComponent } from 'react';
-import { Icon, InfoIcon } from '@chakra-ui/icons';
-import { HStack, Button, Tooltip, Tag, Text, Box } from '@chakra-ui/react';
+import React, { VoidFunctionComponent, useState } from 'react';
+import { HStack, Button, Tooltip, Tag, Text, Box, Icon } from '@chakra-ui/react';
+import FeatherIcon from 'feather-icons-react';
 
 type Props = {
   selectedTags: string[];
@@ -9,18 +9,28 @@ type Props = {
 };
 
 const SearchTags: VoidFunctionComponent<Props> = ({ selectedTags, onTagClick, clearAllTags }) => {
+  const [isHovering, setIsHovering] = useState(false);
+  const handleMouseOver = () => {
+    setIsHovering(true);
+  };
+  const handleMouseOut = () => {
+    setIsHovering(false);
+  };
+
   return (
     <HStack>
       {selectedTags.length > 0 ? (
         <Button onClick={clearAllTags}>Clear tags</Button>
       ) : (
         <HStack>
-          <Text fontSize="sm">Currently you have not selected any tag</Text>
           <Tooltip
-            label="By clicking on tag of resource pool you can start filtering. By clicking on the same tag you will unselect
-    tag"
+            openDelay={10}
+            placement="right"
+            label="By clicking on tag of resource pool you can start filtering. By clicking on the same tag you will unselect tag"
           >
-            <Icon cursor="pointer" as={InfoIcon} />
+            <Text fontSize="sm">
+              Currently you have not selected any tag <Icon as={FeatherIcon} icon="info" />
+            </Text>
           </Tooltip>
         </HStack>
       )}
