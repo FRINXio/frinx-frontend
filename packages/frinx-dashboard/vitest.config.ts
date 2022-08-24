54c21f3d652594c028fa02242f03bdc3eade7aa4
Martin Sottnik <msottnik@gmail.com>
2022-08-24 11:41:10 +0200

    dashboard header test added

--- packages/frinx-dashboard/vitest.config.ts
+++ packages/frinx-dashboard/vitest.config.ts
@@ -3,13 +3,5 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     environment: 'jsdom',
-    alias: {
-      '@frinx/workflow-ui': '/src/index-test.tsx',
-      '@frinx/uniresource-ui': '/src/index-test.tsx',
-      '@frinx/inventory-client': '/src/index-test.tsx',
-      '@frinx/workflow-builder': '/src/index-test.tsx',
-      '@frinx/frinx-gamma': '/src/index-test.tsx',
-    },
   },
-  resolve: {},
 });
