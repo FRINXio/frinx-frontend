95f9845b69a5b4a590aae8e11e490478424d7ab1
Martin Sottnik <msottnik@gmail.com>
2022-08-25 12:16:06 +0200

    add component testing to uniresource

---
+++ packages/frinx-uniresource-ui/src/pages/pools-page/pools-table.test.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { describe, expect, test } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { faker } from '@faker-js/faker';
+import { BrowserRouter } from 'react-router-dom';
+import PoolsTable from './pools-table';
+import { ResourcePool } from '../../__generated__/graphql';
+
+faker.seed(123);
+
+const pools: ResourcePool[] = [
+  {
+    id: faker.datatype.uuid(),
+    allocatedResources: null,
+    AllocationStrategy: null,
+    Capacity: {
+      freeCapacity: '1000',
+      utilizedCapacity: '1000',
+    },
+    Name: faker.random.words(2),
+    ParentResource: null,
+    PoolProperties: null,
+    PoolType: 'allocating',
+    Resources: [],
+    ResourceType: {
+      id: faker.datatype.uuid(),
+      Name: faker.commerce.productName(),
+      Pools: [],
+      PropertyTypes: [],
+    },
+    Tags: [],
+  },
+];
+
+describe('Inventory app', () => {
+  test('should render device table', () => {
+    render(
+      <BrowserRouter>
+        <PoolsTable isLoading={false} onDeleteBtnClick={() => {}} pools={pools} />
+      </BrowserRouter>,
+    );
+    expect(screen.getByRole('table')).toBeDefined();
+  });
+});
