671ebffd9c20c34eef592a1dc05656b84f2e6774
Martin Sottnik <msottnik@gmail.com>
2022-09-26 19:02:02 +0200

    remove unused code

--- packages/frinx-device-topology/src/pages/topology/graph.helpers.ts
+++ packages/frinx-device-topology/src/pages/topology/graph.helpers.ts
@@ -145,13 +145,6 @@ export function getDefaultPositionsMap(nodes: GraphNode[], edges: GraphEdge[]): 
   };
 }
 
-export function getNormalizedVectorAngle(p1: Position, p2: Position): number {
-  const dx = p2.x - p1.x;
-  const dy = p2.y - p1.y;
-
-  return -(dx / dy);
-}
-
 export function getPointOnCircle(source: Position, target: Position, radius = 1): Position {
   const angle = getAngleBetweenPoints(source, target);
   const y = radius * Math.sin(angle);
