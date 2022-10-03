bb0e874425d8d3ab6e4a903f99599acf9971bc30
MarcoMruz <marcomruz1@gmail.com>
2022-10-03 10:27:03 +0200

    update URL after workflow execution after product names update

--- packages/frinx-workflow-ui/src/common/modals/execute-workflow-modal.tsx
+++ packages/frinx-workflow-ui/src/common/modals/execute-workflow-modal.tsx
@@ -102,7 +102,7 @@ const ExecuteWorkflowModal: FC<Props> = ({
         </ModalBody>
         <ModalFooter>
           {executedWorkflowId != null && (
-            <Button variant="link" colorScheme="blue" as={Link} to={`/uniflow/executed/${executedWorkflowId}`}>
+            <Button variant="link" colorScheme="blue" as={Link} to={`/workflow-manager/executed/${executedWorkflowId}`}>
               Executed workflow in detail
             </Button>
           )}
