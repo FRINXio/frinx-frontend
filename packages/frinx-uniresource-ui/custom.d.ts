c38145cf34db9a68a490edd41af6bf938ca07d90
Marco Mrúz <marcomruz1@gmail.com>
2022-10-27 16:56:14 +0200

    create separate .d.ts for yup

--- packages/frinx-uniresource-ui/custom.d.ts
+++ packages/frinx-uniresource-ui/custom.d.ts
@@ -1,17 +1,5 @@
-import { ArraySchema as YupArraySchema } from 'yup';
-
 declare module 'feather-icons-react' {
   const content: FC<{ icon: string; size: string }>;
 
   export default content;
-}
-
-// Not needed to explicitly define in specific files when TS can infer it
-declare module 'yup' {
-  // reasoning: https://typescript-eslint.io/rules/consistent-type-definitions/#when-not-to-use-it
-  // TS implementation of methods to yup -> https://github.com/jquense/yup/issues/345#issuecomment-537338283
-  // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
-  interface ArraySchema<T> extends YupArraySchema<T> {
-    unique(errMsg: string, mapper: (a: T) => T): ArraySchema<T>;
-  }
 }
