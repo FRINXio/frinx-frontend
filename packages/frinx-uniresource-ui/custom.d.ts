45c80118ab66df7284e525728ab4a87ccb644579
Marco Mrúz <marcomruz1@gmail.com>
2022-10-27 12:46:09 +0200

    move declaration of yup custom method to definition file

--- packages/frinx-uniresource-ui/custom.d.ts
+++ packages/frinx-uniresource-ui/custom.d.ts
@@ -3,3 +3,13 @@ declare module 'feather-icons-react' {
 
   export default content;
 }
+
+// Not needed to explicitly define in specific files when TS can infer it
+declare module 'yup' {
+  // reasoning: https://typescript-eslint.io/rules/consistent-type-definitions/#when-not-to-use-it
+  // TS implementation of methods to yup -> https://github.com/jquense/yup/issues/345#issuecomment-537338283
+  // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+  interface ArraySchema<T> {
+    unique(errMsg: string, mapper: (a: T) => T): ArraySchema<T>;
+  }
+}
