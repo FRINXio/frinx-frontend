c710e5b02b26472fedc9c8356389aa9348fe8928
Marco Mrúz <marcomruz1@gmail.com>
2022-10-27 17:22:56 +0200

    add definition file

---
+++ packages/frinx-uniresource-ui/yup.d.ts
@@ -0,0 +1,11 @@
+import 'yup';
+
+// Not needed to explicitly define in specific files when TS can infer it
+declare module 'yup' {
+  // reasoning: https://typescript-eslint.io/rules/consistent-type-definitions/#when-not-to-use-it
+  // TS implementation of methods to yup -> https://github.com/jquense/yup/issues/345#issuecomment-537338283
+  // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+  export interface ArraySchema<T> {
+    unique(errMsg: string, mapper: (a: T) => T): ArraySchema<T>;
+  }
+}
