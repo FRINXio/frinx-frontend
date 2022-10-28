82b89666a5e4d53c289ba9bc970ae509460a9fc8
Martin Sottnik <msottnik@gmail.com>
2022-10-28 13:17:14 +0200

    rename api uniresource -> resource-manager

--- packages/frinx-api/src/index.ts
+++ packages/frinx-api/src/index.ts
@@ -1,7 +1,7 @@
 export { default as UniconfigApi } from './uniconfig-api';
 export { default as UniflowApi } from './uniflow-api';
 export { default as InventoryApi } from './inventory-api';
-export { default as UniresourceApi } from './uniresource-api';
+export { default as ResourceManagerApi } from './resource-manager-api';
 export { default as UnistoreApi } from './unistore-api';
 
 export type { ApiConfig } from './types';
