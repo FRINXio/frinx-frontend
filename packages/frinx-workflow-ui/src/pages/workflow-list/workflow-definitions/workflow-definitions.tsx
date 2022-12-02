7e872dca77a0c4cf9cb225c90a642eb81933cf3d
Peto <peto@Dell.Peto>
2022-12-02 11:18:25 +0100

    removed unwanted change

--- packages/frinx-workflow-ui/src/pages/workflow-list/workflow-definitions/workflow-definitions.tsx
+++ packages/frinx-workflow-ui/src/pages/workflow-list/workflow-definitions/workflow-definitions.tsx
@@ -111,7 +111,7 @@ const WorkflowDefinitions = () => {
             return a.name.localeCompare(b.name);
           }) || [];
         setWorkflows(dataset);
-        setItemList(dataset, 1);
+        setItemList(dataset);
         setAllLabels(getLabels(dataset));
       });
     });
