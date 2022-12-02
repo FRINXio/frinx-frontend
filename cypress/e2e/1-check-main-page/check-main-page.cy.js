37cc56f9913d0090a4cbd986e5dac2bf950f4212
Simon Misencik <simon.misencik@gmail.com>
2022-12-02 07:40:58 +0100

    Lint code
    Signed-off-by: Simon Misencik <simon.misencik@gmail.com>

--- cypress/e2e/1-check-main-page/check-main-page.cy.js
+++ cypress/e2e/1-check-main-page/check-main-page.cy.js
@@ -3,15 +3,15 @@
 /// <reference types="cypress" />
 
 describe('check main page of Frinx-machine', () => {
-    beforeEach(() => {
-        cy.visit(Cypress.env('host'))
-    })
+  beforeEach(() => {
+    cy.visit(Cypress.env('host'));
+  });
 
-    it('Check title of the main page', () => {
-        cy.title().should('eq', 'FRINX Dashboard')
-    })
+  it('Check title of the main page', () => {
+    cy.title().should('eq', 'FRINX Dashboard');
+  });
 
-    it('Check the main page', () => {
-        cy.findAllByText('Workflow manager', { timeout: 10000 }).should('be.visible')
-    })
-})
+  it('Check the main page', () => {
+    cy.findAllByText('Workflow manager', { timeout: 10000 }).should('be.visible');
+  });
+});
