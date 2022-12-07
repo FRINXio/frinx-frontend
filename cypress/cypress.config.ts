00eed8a23fbc37494373c02a888526c54f574062
Paulooze <paulooze@gmail.com>
2022-12-07 10:48:41 +0100

    update GH action

--- cypress/cypress.config.ts
+++ cypress/cypress.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents() {
-      // implement node event listeners here
-    },
+    specPattern: 'e2e/**/*.cy.{js,jsx,ts,tsx}',
   },
 });
