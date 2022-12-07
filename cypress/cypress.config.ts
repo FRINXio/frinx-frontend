8dacd6e853b671da1507102202172dde3d28feed
Paulooze <paulooze@gmail.com>
2022-12-07 10:32:58 +0100

    fix folder structure in cypress test

--- cypress/cypress.config.ts
+++ cypress/cypress.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    supportFile: false,
     setupNodeEvents() {
       // implement node event listeners here
     },
