7562a1001ef0f8999d7ea7985077d67e5a612213
Peto <peto@Dell.Peto>
2023-01-10 11:58:51 +0100

    formatting

--- packages/frinx-resource-manager/src/pages/strategies-page/strategies-page.tsx
+++ packages/frinx-resource-manager/src/pages/strategies-page/strategies-page.tsx
@@ -175,7 +175,6 @@ const StrategiesPage: VoidFunctionComponent = () => {
         onScriptBtnClick={handleScriptBtnClick}
       />
       <StrategyScriptModal
-      
         isOpen={scriptState != null}
         onClose={() => {
           setScriptState(null);
