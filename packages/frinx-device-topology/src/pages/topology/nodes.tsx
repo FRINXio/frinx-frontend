29c88e81b7fd02f0250b645c5a1bea27ee0d5c11
Martin Sottnik <msottnik@gmail.com>
2023-01-18 17:24:07 +0100

    node click timer removed (use state to distinguish click/drag)

--- packages/frinx-device-topology/src/pages/topology/nodes.tsx
+++ packages/frinx-device-topology/src/pages/topology/nodes.tsx
@@ -1,5 +1,5 @@
 import { unwrap } from '@frinx/shared/src';
-import React, { useRef, useState, VoidFunctionComponent } from 'react';
+import React, { useState, VoidFunctionComponent } from 'react';
 import NodeIcon from '../../components/node-icon/node-icon';
 import { GraphNodeWithDiff } from '../../helpers/topology-helpers';
 import { setSelectedNode } from '../../state.actions';
@@ -18,6 +18,8 @@ type Props = {
 };
 
 const Nodes: VoidFunctionComponent<Props> = ({ nodesWithDiff, onNodePositionUpdate, onNodePositionUpdateFinish }) => {
+  const [isPointerDown, setIsPointerDown] = useState(false);
+  const [isMoved, setIsMoved] = useState(false);
   const { state, dispatch } = useStateContext();
   const { nodePositions, connectedNodeIds, selectedNode, interfaceGroupPositions } = state;
   const [position, setPosition] = useState<StatePosition>({
@@ -25,14 +27,10 @@ const Nodes: VoidFunctionComponent<Props> = ({ nodesWithDiff, onNodePositionUpda
     isActive: false,
     offset: { x: 0, y: 0 },
   });
-  const timeoutRef = useRef<number | null>(null);
 
   const handlePointerDown = (event: React.PointerEvent<SVGRectElement>, node: GraphNode) => {
-    timeoutRef.current = Number(
-      setTimeout(() => {
-        dispatch(setSelectedNode(node));
-      }, 250),
-    );
+    setIsPointerDown(true);
+    setIsMoved(false);
     const element = event.currentTarget;
     const bbox = element.getBoundingClientRect();
     const x = event.clientX - bbox.left;
@@ -47,11 +45,8 @@ const Nodes: VoidFunctionComponent<Props> = ({ nodesWithDiff, onNodePositionUpda
       },
     });
   };
-  const handlePointerMove = (event: React.PointerEvent<SVGRectElement>) => {
-    if (position.isActive) {
-      if (timeoutRef.current != null) {
-        clearTimeout(timeoutRef.current);
-      }
+  const handlePointerMove = (event: React.PointerEvent<SVGRectElement>, node: GraphNode) => {
+    if (position.isActive && isPointerDown) {
       const bbox = event.currentTarget.getBoundingClientRect();
       const x = event.clientX - bbox.left;
       const y = event.clientY - bbox.top;
@@ -61,13 +56,19 @@ const Nodes: VoidFunctionComponent<Props> = ({ nodesWithDiff, onNodePositionUpda
       onNodePositionUpdate(nodeId, { x: newX, y: newY });
     }
   };
-  const handlePointerUp = () => {
-    setPosition({
-      offset: { x: 0, y: 0 },
-      nodeId: null,
-      isActive: false,
-    });
-    onNodePositionUpdateFinish();
+  const handlePointerUp = (event: React.PointerEvent<SVGRectElement>, node: GraphNode) => {
+    setIsPointerDown(false);
+
+    if (isMoved) {
+      setPosition({
+        offset: { x: 0, y: 0 },
+        nodeId: null,
+        isActive: false,
+      });
+      onNodePositionUpdateFinish();
+    } else {
+      dispatch(setSelectedNode(node));
+    }
   };
 
   return (
@@ -78,8 +79,12 @@ const Nodes: VoidFunctionComponent<Props> = ({ nodesWithDiff, onNodePositionUpda
           onPointerDown={(event) => {
             handlePointerDown(event, node);
           }}
-          onPointerMove={handlePointerMove}
-          onPointerUp={handlePointerUp}
+          onPointerMove={(event) => {
+            handlePointerMove(event, node);
+          }}
+          onPointerUp={(event) => {
+            handlePointerUp(event, node);
+          }}
           positions={{ nodes: nodePositions, interfaceGroups: interfaceGroupPositions }}
           isFocused={connectedNodeIds.includes(node.device.name)}
           isSelected={selectedNode?.device.id === node.device.id}
