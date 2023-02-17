08bab8dc239871e5184f79854e5d9497faa5a089
Stanislav Chlebec <schlebec@frinx.io>
2023-02-17 16:57:22 +0100

    fix test

--- cypress/cypress/e2e/4-workflow-builder/workflow-builder.cy.js
+++ cypress/cypress/e2e/4-workflow-builder/workflow-builder.cy.js
@@ -126,7 +126,7 @@ describe('Create workflow, test and delete it', () => {
 
     // it('workflow editor', () => {
     cy.log('-- 05. Actions/Workflow editor --');
-    cy.contains('button', 'Actions').should('be.visible');
+    cy.contains('button', 'Actions', { timeout: 30000 }).should('be.visible');
     cy.contains('button', 'Actions').click();
     cy.contains('button', 'Workflow editor').click();
     cy.get('.ace_content').type('{backspace}{backspace}{backspace}{backspace}{{}1}{enter}}');
