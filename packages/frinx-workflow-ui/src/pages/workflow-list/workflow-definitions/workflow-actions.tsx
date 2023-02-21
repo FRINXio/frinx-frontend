a9d2e2fe617f17fe732af536f0c8946f29fb174b
Stanislav Chlebec <schlebec@frinx.io>
2023-02-21 06:57:21 +0100

    satisfy linter

--- packages/frinx-workflow-ui/src/pages/workflow-list/workflow-definitions/workflow-actions.tsx
+++ packages/frinx-workflow-ui/src/pages/workflow-list/workflow-definitions/workflow-actions.tsx
@@ -28,7 +28,13 @@ const WorkflowActions: FC<Props> = ({
   return (
     <Stack direction="row" spacing={4}>
       <ButtonGroup>
-        <Button colorScheme="red" size="sm" variant="outline" onClick={onDeleteBtnClick} data-cy={`del_${workflow.name}_${workflow.version}`} >
+        <Button
+          colorScheme="red"
+          size="sm"
+          variant="outline"
+          onClick={onDeleteBtnClick}
+          data-cy={`del_${workflow.name}_${workflow.version}`}
+        >
           <Box as="span" flexShrink={0} alignSelf="center">
             <Box as={FeatherIcon} size="1em" icon="trash-2" flexShrink={0} lineHeight={4} verticalAlign="middle" />
           </Box>
@@ -45,25 +51,37 @@ const WorkflowActions: FC<Props> = ({
             <Box as={FeatherIcon} size="1em" icon="edit" flexShrink={0} lineHeight={4} verticalAlign="middle" />
           </Box>
         </Button>
-        <Button colorScheme="blue" size="sm" variant="outline" onClick={onExecuteBtnClick} data-cy={`exec_${workflow.name}_${workflow.version}`} >
+        <Button
+          colorScheme="blue"
+          size="sm"
+          variant="outline"
+          onClick={onExecuteBtnClick}
+          data-cy={`exec_${workflow.name}_${workflow.version}`}
+        >
           <Box as="span" flexShrink={0} alignSelf="center">
             <Box as={FeatherIcon} size="1em" icon="play" flexShrink={0} lineHeight={4} verticalAlign="middle" />
           </Box>
         </Button>
         <Menu>
-          <MenuButton as={Button} variant="outline" size="sm" colorScheme="black" data-cy={`menu_${workflow.name}_${workflow.version}`} >
+          <MenuButton
+            as={Button}
+            variant="outline"
+            size="sm"
+            colorScheme="black"
+            data-cy={`menu_${workflow.name}_${workflow.version}`}
+          >
             <Box as="span" flexShrink={0} alignSelf="center">
               <Box as={FeatherIcon} size="1em" icon="menu" flexShrink={0} lineHeight={4} verticalAlign="middle" />
             </Box>
           </MenuButton>
           <MenuList>
-            <MenuItem onClick={onFavouriteBtnClick} data-cy={`favor_${workflow.name}_${workflow.version}`} >
+            <MenuItem onClick={onFavouriteBtnClick} data-cy={`favor_${workflow.name}_${workflow.version}`}>
               <Box as="span" fontSize="0.8em" marginRight={3} flexShrink={0} alignSelf="center">
                 <Box as={FeatherIcon} size="1em" icon="star" flexShrink={0} lineHeight={4} verticalAlign="middle" />
               </Box>
               {isFavourite ? 'Remove from favourites' : 'Add to favourites'}
             </MenuItem>
-            <MenuItem onClick={onDiagramBtnClick} data-cy={`diag_${workflow.name}_${workflow.version}`} >
+            <MenuItem onClick={onDiagramBtnClick} data-cy={`diag_${workflow.name}_${workflow.version}`}>
               <Box as="span" fontSize="0.8em" marginRight={3} flexShrink={0} alignSelf="center">
                 <Box
                   as={FeatherIcon}
@@ -76,13 +94,13 @@ const WorkflowActions: FC<Props> = ({
               </Box>
               Show diagram
             </MenuItem>
-            <MenuItem onClick={onDefinitionBtnClick} data-cy={`showdef_${workflow.name}_${workflow.version}`} >
+            <MenuItem onClick={onDefinitionBtnClick} data-cy={`showdef_${workflow.name}_${workflow.version}`}>
               <Box as="span" fontSize="0.8em" marginRight={3} flexShrink={0} alignSelf="center">
                 <Box as={FeatherIcon} size="1em" icon="code" flexShrink={0} lineHeight={4} verticalAlign="middle" />
               </Box>
               Show definition
             </MenuItem>
-            <MenuItem onClick={onScheduleBtnClick} data-cy={`sched_${workflow.name}_${workflow.version}`} >
+            <MenuItem onClick={onScheduleBtnClick} data-cy={`sched_${workflow.name}_${workflow.version}`}>
               <Box as="span" fontSize="0.8em" marginRight={3} flexShrink={0} alignSelf="center">
                 <Box as={FeatherIcon} size="1em" icon="clock" flexShrink={0} lineHeight={4} verticalAlign="middle" />
               </Box>
