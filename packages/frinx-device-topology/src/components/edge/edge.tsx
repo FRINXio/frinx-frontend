03b4a56fc074762aa674ceaf46f927ab848a9594
Pavol Porubský <paulooze@gmail.com>
2023-02-21 10:07:06 +0100

    Update interface behaviour (#1095)
    * fix unknown edge design; fix diff label text
    
    * fix eslint error
    
    * fix interface label position; use name instead id for interface label
    
    * update color for unknown interface label
    
    * remove forgotten comment

--- packages/frinx-device-topology/src/components/edge/edge.tsx
+++ packages/frinx-device-topology/src/components/edge/edge.tsx
@@ -8,84 +8,52 @@ import { getEdgeColor } from './edge.helpers';
 type Props = {
   edge: GraphEdgeWithDiff;
   isActive: boolean;
-  isSelected: boolean;
   controlPoints: Position[];
   linePoints: Line;
   onClick: (edge: GraphEdgeWithDiff | null) => void;
   isUnknown: boolean;
 };
 
-const Edge: VoidFunctionComponent<Props> = ({
-  edge,
-  isActive,
-  isSelected,
-  controlPoints,
-  linePoints,
-  onClick,
-  isUnknown,
-}) => {
+const Edge: VoidFunctionComponent<Props> = ({ edge, isActive, controlPoints, linePoints, onClick, isUnknown }) => {
   const { start, end } = linePoints;
   const { colors } = useTheme<Theme>();
 
-  return (
-    <>
-      {isActive ? (
-        <g>
-          <path
-            strokeWidth={1}
-            stroke={get(colors, getEdgeColor(edge.change))}
-            strokeLinejoin="round"
-            fill="none"
-            d={getCurvePath(start, end, controlPoints)}
-            cursor="pointer"
-            opacity={isUnknown ? 0.5 : 1}
-          />
-          <path
-            strokeWidth={5}
-            stroke="transparent"
-            strokeLinejoin="round"
-            fill="none"
-            d={getCurvePath(start, end, controlPoints)}
-            cursor="pointer"
-            pointerEvents={edge.change === 'DELETED' ? 'none' : 'all'}
-            onClick={() => {
-              onClick(edge);
-            }}
-            opacity={isUnknown ? 0.5 : 1}
-          />
-        </g>
-      ) : (
-        <Box
-          as="line"
-          x1={start.x}
-          y1={start.y}
-          x2={end.x}
-          y2={end.y}
-          stroke={getEdgeColor(edge.change)}
-          strokeWidth={isActive ? 3 : 1}
-          strokeLinecap="round"
-          borderWidth={3}
-          transition="all .2s ease-in-out"
-          strokeDasharray={isUnknown ? '15, 15' : undefined}
-        />
-      )}
-
-      {isSelected && (
-        <>
-          <defs>
-            <path id="sourcePath" d={getCurvePath(start, end, controlPoints)} />
-          </defs>
-          <text dx={30} dy={20} fontSize={14}>
-            <textPath href="#sourcePath">{edge.source.interface}</textPath>
-          </text>
-          <text dx={-30} dy={-10} fontSize={14}>
-            <textPath href="#sourcePath" startOffset="100%" textAnchor="end">
-              {edge.target.interface}
-            </textPath>
-          </text>
-        </>
-      )}
-    </>
+  return isActive ? (
+    <g>
+      <path
+        strokeWidth={1}
+        stroke={get(colors, getEdgeColor(edge.change, isUnknown))}
+        strokeLinejoin="round"
+        fill="none"
+        d={getCurvePath(start, end, controlPoints)}
+        cursor="pointer"
+      />
+      <path
+        strokeWidth={5}
+        stroke="transparent"
+        strokeLinejoin="round"
+        fill="none"
+        d={getCurvePath(start, end, controlPoints)}
+        cursor="pointer"
+        pointerEvents={edge.change === 'DELETED' ? 'none' : 'all'}
+        onClick={() => {
+          onClick(edge);
+        }}
+      />
+    </g>
+  ) : (
+    <Box
+      as="line"
+      x1={start.x}
+      y1={start.y}
+      x2={end.x}
+      y2={end.y}
+      stroke={getEdgeColor(edge.change, isUnknown)}
+      strokeWidth={isActive ? 3 : 1}
+      strokeLinecap="round"
+      borderWidth={3}
+      transition="all .2s ease-in-out"
+    />
   );
 };
 
