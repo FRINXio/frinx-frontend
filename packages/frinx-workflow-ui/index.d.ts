9911fb5562b40a021fa40f6ad661fbb7211f5bfa
Pavol Porubský <paulooze@gmail.com>
2023-03-15 11:23:35 +0100

    Cleanup workflow UI (#1166)
    * fix(deps): update dependency react-redux to v8
    
    * remove unused deps; fix workflow ui lint/tygen
    
    * fix device-topology package name (frinxio => frinx)
    
    * fix type checks
    
    * fix missing dependencies (sass and file-loader)
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: Martin Sottnik <msottnik@gmail.com>

--- packages/frinx-workflow-ui/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { FC } from 'react';
-import { UniflowApiCallbacks } from './src/uniflow-api-provider';
-
-declare module '@frinx/workflow-ui' {
-  export const ReduxProvider: FC;
-  export const getUniflowApiProvider: (callbacks: UniflowApiCallbacks) => FC;
-  export const UniflowApp: VoidFunctionComponent;
-}
