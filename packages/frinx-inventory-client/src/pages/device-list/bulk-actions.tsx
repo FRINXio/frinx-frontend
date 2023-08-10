1b4793056e2e8c1de1f7816e854e25913d36330d
PeterL <lehocky.peter@yahoo.com>
2023-08-10 09:51:11 +0200

    removed unneccesary css

--- packages/frinx-inventory-client/src/pages/device-list/bulk-actions.tsx
+++ packages/frinx-inventory-client/src/pages/device-list/bulk-actions.tsx
@@ -15,7 +15,7 @@ const BulkActions: VoidFunctionComponent<Props> = ({
   areButtonsDisabled,
 }) => {
   return (
-    <HStack mt={0}>
+    <HStack>
       <Button
         data-cy="send-to-workflows"
         isDisabled={areButtonsDisabled}
