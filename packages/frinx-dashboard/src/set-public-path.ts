09d83d9766f846697627a992b584679f9096095d
Paulooze <paulooze@gmail.com>
2023-08-16 11:33:39 +0200

    remove traces of webpack

--- packages/frinx-dashboard/src/set-public-path.ts
+++
@@ -1,8 +0,0 @@
-// its necessary in order to change path to the static files on the fly (e.g. {URLBasename}/static/[id].bundle.js ...)
-// https://webpack.js.org/guides/public-path/
-
-export function setWebpackPublicPath(): void {
-  if (process.env.NODE_ENV === 'production') {
-    __webpack_public_path__ = `${window.__CONFIG__.URLBasename}/`.replace(/(\/)\/+/g, '$1');
-  }
-}
