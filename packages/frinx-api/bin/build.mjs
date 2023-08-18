f3ee51b71c822f2dd4b294dcbe8e219d8b9163d0
Paulooze <paulooze@gmail.com>
2023-08-18 11:36:24 +0200

    use ts api to generate d.ts files

--- packages/frinx-api/bin/build.mjs
+++ packages/frinx-api/bin/build.mjs
@@ -1,5 +1,6 @@
 import { Builder } from '@frinx/build-scripts';
 import pkg from '../package.json' assert { type: 'json' };
+import tsConfig from '../tsconfig.typegen.json' assert { type: 'json' };
 
 const builder = new Builder({ packageName: pkg.name });
 
@@ -8,4 +9,4 @@ await builder.buildPackage({
   mainFileName: pkg.main,
   moduleFileName: pkg.module,
 });
-await builder.buildTypes(pkg.scripts.typegen);
+await builder.buildTypes(['src/index.ts'], tsConfig.compilerOptions);
