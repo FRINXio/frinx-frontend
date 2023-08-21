38db2e01d724919ee7b73c0b26fce1abd22e50b9
marco.mruz <marcomruz1@gmail.com>
2023-08-21 09:14:18 +0200

    refactor code to make it simpler in subworkflow task form

--- packages/frinx-workflow-builder/src/components/task-form/subworkflow-task-form.tsx
+++ packages/frinx-workflow-builder/src/components/task-form/subworkflow-task-form.tsx
@@ -13,36 +13,6 @@ type Props = {
   onChange: (inputParameters: Record<string, string>) => void;
 };
 
-function isWorkflowInputParamKeyAlreadyInTaskInputParams(
-  taskInputParams: Record<string, string>,
-  workflowAvailableInputParamKey: string,
-): boolean {
-  const taskInputParamsEntries = Object.entries(taskInputParams);
-
-  return taskInputParamsEntries.some(([taskInputParamKey]) => taskInputParamKey === workflowAvailableInputParamKey);
-}
-
-function removeAlreadyExistingPropertiesFromWorkflowAvailableInputParams(
-  taskInputParams: Record<string, string>,
-  workflowAvailableInputParams: Record<string, string>,
-): Record<string, string> {
-  const workflowAvailableInputParamsEntries = Object.entries(workflowAvailableInputParams);
-
-  return workflowAvailableInputParamsEntries.reduce(
-    (acc, [workflowAvailableInputParamKey, workflowAvailableInputParamValue]) => {
-      if (isWorkflowInputParamKeyAlreadyInTaskInputParams(taskInputParams, workflowAvailableInputParamKey)) {
-        return acc;
-      }
-
-      return {
-        ...acc,
-        [workflowAvailableInputParamKey]: workflowAvailableInputParamValue,
-      };
-    },
-    {},
-  );
-}
-
 const SubworkflowTaskForm: VoidFunctionComponent<Props> = ({ taskInputParameters, workflow, onChange }) => {
   const parsedWorkflowInputParams = parseInputParameters(workflow.inputParameters);
   const dynamicWorkflowInputParams = getDynamicInputParametersFromWorkflow(workflow);
@@ -59,10 +29,7 @@ const SubworkflowTaskForm: VoidFunctionComponent<Props> = ({ taskInputParameters
   };
 
   const parsedInputParameters = Object.keys({
-    ...removeAlreadyExistingPropertiesFromWorkflowAvailableInputParams(
-      taskInputParameters,
-      availableInputParamsOfWorkflowEntries,
-    ),
+    ...availableInputParamsOfWorkflowEntries,
     ...taskInputParameters,
   }).sort();
 
