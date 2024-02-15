c99a5d26e0018e6a588013bce0b9cf74a8abf96b
marco.mruz <marcomruz1@gmail.com>
2024-02-15 13:25:50 +0100

    remove typenames and ids from workflow definition modal

--- packages/frinx-workflow-ui/src/components/modals/definition-modal.tsx
+++ packages/frinx-workflow-ui/src/components/modals/definition-modal.tsx
@@ -9,30 +9,40 @@ import {
   ModalHeader,
   ModalOverlay,
 } from '@chakra-ui/react';
-import { ClientWorkflow, Editor } from '@frinx/shared';
+import { ClientWorkflowWithTasks, Editor } from '@frinx/shared';
+import { omit } from 'lodash';
 
 type ModalProps = {
   isOpen: boolean;
   onClose: () => void;
-  workflow?: ClientWorkflow;
+  workflow?: ClientWorkflowWithTasks;
 };
 
-const DefinitionModal: VoidFunctionComponent<ModalProps> = ({ isOpen, onClose, workflow }) => (
-  <Modal size="3xl" scrollBehavior="inside" isOpen={isOpen} onClose={onClose} isCentered>
-    <ModalOverlay />
-    <ModalCloseButton />
-    <ModalContent>
-      <ModalHeader>{workflow?.name}</ModalHeader>
-      <ModalBody>
-        <Editor defaultValue={JSON.stringify(workflow, null, 2)} width="100%" />
-      </ModalBody>
-      <ModalFooter>
-        <Button colorScheme="gray" onClick={onClose}>
-          Close
-        </Button>
-      </ModalFooter>
-    </ModalContent>
-  </Modal>
-);
+const DefinitionModal: VoidFunctionComponent<ModalProps> = ({ isOpen, onClose, workflow }) => {
+  const workflowTasksWithoutGraphqlSpecs = workflow?.tasks.map((task) => omit(task, ['id', '__typename']));
+  const workflowWithoutGraphqlSpecs = {
+    ...omit(workflow, ['id', '__typename']),
+    tasks: workflowTasksWithoutGraphqlSpecs,
+    tasksJson: JSON.stringify(workflowTasksWithoutGraphqlSpecs),
+  };
+
+  return (
+    <Modal size="3xl" scrollBehavior="inside" isOpen={isOpen} onClose={onClose} isCentered>
+      <ModalOverlay />
+      <ModalCloseButton />
+      <ModalContent>
+        <ModalHeader>{workflow?.name}</ModalHeader>
+        <ModalBody>
+          <Editor defaultValue={JSON.stringify(workflowWithoutGraphqlSpecs, null, 2)} width="100%" />
+        </ModalBody>
+        <ModalFooter>
+          <Button colorScheme="gray" onClick={onClose}>
+            Close
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
 
 export default DefinitionModal;
