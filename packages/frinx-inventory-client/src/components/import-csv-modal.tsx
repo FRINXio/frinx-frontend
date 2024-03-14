dfc997170b64cc7ef48da9dd143e344ff7463a05
Martin Sottnik <msottnik@gmail.com>
2024-03-14 18:57:58 +0100

    form validation added (#1504)

--- packages/frinx-inventory-client/src/components/import-csv-modal.tsx
+++ packages/frinx-inventory-client/src/components/import-csv-modal.tsx
@@ -18,8 +18,10 @@ import {
 } from '@chakra-ui/react';
 import { useNotifications } from '@frinx/shared';
 import FeatherIcon from 'feather-icons-react';
-import React, { ChangeEvent, FormEvent, useMemo, useRef, useState, VoidFunctionComponent } from 'react';
+import React, { ChangeEvent, useMemo, useRef, VoidFunctionComponent } from 'react';
 import { gql, useMutation, useQuery } from 'urql';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 import {
   UploadFileMutation,
   UploadFileMutationVariables,
@@ -51,6 +53,11 @@ const ZONES_QUERY = gql`
   }
 `;
 
+const uploadSchema = yup.object({
+  file: yup.mixed().required(),
+  zoneId: yup.string().required(),
+});
+
 type FormValues = {
   file: File | null;
   zoneId: string;
@@ -70,43 +77,43 @@ const ImportCSVModal: VoidFunctionComponent<Props> = ({ onClose }) => {
   const inputRef = useRef<HTMLInputElement | null>(null);
   const [, uploadFile] = useMutation<UploadFileMutation, UploadFileMutationVariables>(UPLOAD_FILE_MUTATION);
   const [{ data, fetching }] = useQuery<ZonesImportQuery, ZonesImportQueryVariables>({ query: ZONES_QUERY });
-  const [values, setValues] = useState<FormValues>(INITIAL_VALUES);
-
   const { addToastNotification } = useNotifications();
+  const { values, handleSubmit, setFieldValue, isValid } = useFormik<FormValues>({
+    initialValues: INITIAL_VALUES,
+    validationSchema: uploadSchema,
+    validateOnMount: true,
+    onSubmit: async (formData: FormValues) => {
+      await uploadFile(
+        {
+          input: formData,
+        },
+        context,
+      ).then((res) => {
+        if (res.error?.name === 'CombinedError') {
+          addToastNotification({
+            content: 'We could not import all devices because some of the imported devices already exsits',
+            type: 'error',
+          });
+        } else {
+          addToastNotification({
+            content: 'CSV imported successfully',
+            type: 'success',
+          });
+        }
+      });
+      onClose();
+    },
+  });
 
   const handleOnFileInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { files } = event.target;
     if (files == null) {
       return;
     }
-    setValues((vals) => ({
-      ...vals,
-      file: files[0],
-    }));
+    setFieldValue('file', files[0]);
   };
   const handleOnZoneIdInputChange = (event: ChangeEvent<HTMLSelectElement>) => {
-    setValues((vals) => ({
-      ...vals,
-      zoneId: event.target.value,
-    }));
-  };
-
-  const handleFormSubmit = async (event: FormEvent) => {
-    event.preventDefault();
-    await uploadFile(
-      {
-        input: values,
-      },
-      context,
-    ).then((res) => {
-      if (res.error?.name === 'CombinedError') {
-        addToastNotification({
-          content: 'We could not import all devices because some of the imported devices already exsits',
-          type: 'error',
-        });
-      }
-    });
-    onClose();
+    setFieldValue('zoneId', event.target.value);
   };
 
   if (data == null) {
@@ -122,60 +129,62 @@ const ImportCSVModal: VoidFunctionComponent<Props> = ({ onClose }) => {
   return (
     <Modal isOpen onClose={onClose} size="5xl" closeOnOverlayClick={false}>
       <ModalOverlay />
-      <ModalContent as="form" onSubmit={handleFormSubmit}>
-        <ModalHeader>Import devices from CSV</ModalHeader>
-        <ModalCloseButton />
-        <ModalBody>
-          <FormControl isRequired id="file" my={6}>
-            <FormLabel htmlFor="writeUpFile">CSV File</FormLabel>
-            <InputGroup>
-              <InputLeftElement pointerEvents="none">
-                <Icon size={20} as={FeatherIcon} icon="file" />
-              </InputLeftElement>
-              <input
-                type="file"
-                accept=".csv"
-                name="file"
-                ref={inputRef}
-                style={{ display: 'none' }}
-                onChange={handleOnFileInputChange}
-              />
-              <Input
-                data-cy="import-csv-modal-file"
-                placeholder="Your file ..."
-                onClick={() => {
-                  inputRef.current?.click();
-                }}
-                value={values.file?.name || ''}
-                readOnly
-              />
-            </InputGroup>
-          </FormControl>
-          <FormControl id="zoneId" my={6}>
-            <FormLabel>UniConfig zone</FormLabel>
-            <Select
-              data-cy="import-csv-modal-zone"
-              placeholder="Select zone for devices"
-              onChange={handleOnZoneIdInputChange}
-            >
-              {zones.map((zone) => (
-                <option key={zone.id} value={zone.id}>
-                  {zone.name}
-                </option>
-              ))}
-            </Select>
-          </FormControl>
-        </ModalBody>
-        <ModalFooter>
-          <HStack>
-            <Button data-cy="import-csv-modal-close" onClick={onClose}>
-              Close
-            </Button>
-            <Button data-cy="import-csv-modal-import" type="submit" colorScheme="blue">
-              Import
-            </Button>
-          </HStack>
-        </ModalFooter>
+      <ModalContent>
+        <form onSubmit={handleSubmit}>
+          <ModalHeader>Import devices from CSV</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <FormControl isRequired id="file" my={6}>
+              <FormLabel htmlFor="writeUpFile">CSV File</FormLabel>
+              <InputGroup>
+                <InputLeftElement pointerEvents="none">
+                  <Icon size={20} as={FeatherIcon} icon="file" />
+                </InputLeftElement>
+                <input
+                  type="file"
+                  accept=".csv"
+                  name="file"
+                  ref={inputRef}
+                  style={{ display: 'none' }}
+                  onChange={handleOnFileInputChange}
+                />
+                <Input
+                  data-cy="import-csv-modal-file"
+                  placeholder="Your file ..."
+                  onClick={() => {
+                    inputRef.current?.click();
+                  }}
+                  value={values.file?.name || ''}
+                  readOnly
+                />
+              </InputGroup>
+            </FormControl>
+            <FormControl id="zoneId" my={6}>
+              <FormLabel>UniConfig zone</FormLabel>
+              <Select
+                data-cy="import-csv-modal-zone"
+                placeholder="Select zone for devices"
+                onChange={handleOnZoneIdInputChange}
+              >
+                {zones.map((zone) => (
+                  <option key={zone.id} value={zone.id}>
+                    {zone.name}
+                  </option>
+                ))}
+              </Select>
+            </FormControl>
+          </ModalBody>
+          <ModalFooter>
+            <HStack>
+              <Button data-cy="import-csv-modal-close" onClick={onClose}>
+                Close
+              </Button>
+              <Button data-cy="import-csv-modal-import" type="submit" colorScheme="blue" isDisabled={!isValid}>
+                Import
+              </Button>
+            </HStack>
+          </ModalFooter>
+        </form>
       </ModalContent>
     </Modal>
   );
