1c90cfb47a9f2b5a4434a6bf4db2b04e9fa272c8
Martin Sottnik <msottnik@gmail.com>
2024-05-16 14:10:11 +0200

    handle expired unistore transaction (#1552)

--- packages/frinx-dashboard/src/auth-helpers.ts
+++ packages/frinx-dashboard/src/auth-helpers.ts
@@ -1,4 +1,36 @@
+import EventEmitter from 'eventemitter3';
 import { useEffect, useState } from 'react';
+
+const LS_TOKEN_KEY = 'id_token';
+
+export type UnauthorizedEventKey = 'UNAUTHORIZED';
+export type EventKeys = 'UNAUTHORIZED' | 'FORBIDDEN' | 'ACCESS_REJECTED';
+
+export class AuthContext {
+  public eventEmitter: EventEmitter<EventKeys> = new EventEmitter();
+
+  private authToken: string | null = localStorage.getItem(LS_TOKEN_KEY);
+
+  public setAuthToken(authToken: string): void {
+    localStorage.setItem(LS_TOKEN_KEY, authToken);
+    this.authToken = authToken;
+  }
+
+  public deleteAuthToken(): void {
+    localStorage.removeItem(LS_TOKEN_KEY);
+    this.authToken = null;
+  }
+
+  public getAuthToken(): string | null {
+    return this.authToken;
+  }
+
+  public emit(event: EventKeys): void {
+    this.eventEmitter.emit(event);
+  }
+}
+
+export const authContext = new AuthContext();
 
 export type UserInfo = {
   user: string;
