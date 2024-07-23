f25564c182d3a3a8c04e78b17c81aec28c420356
oskrovan <ondrej.skrovan@elisapolystar.com>
2024-07-23 15:15:39 +0200

    added to topology - geomap data query, showing on a map

--- packages/frinx-device-topology/src/pages/topology/map/map-topology.container.tsx
+++ packages/frinx-device-topology/src/pages/topology/map/map-topology.container.tsx
@@ -1,19 +1,51 @@
+import * as L from 'leaflet';
 import React, { VoidFunctionComponent } from 'react';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import "leaflet/dist/leaflet.css";
+import { gql, useQuery } from 'urql';
+import { GeoMapDataQueryQuery, GeoMapDataQueryQueryVariables } from '../../../__generated__/graphql';
+
+const GEOMAP_DATA_QUERY = gql`
+query GeoMapDataQuery {
+  deviceInventory {
+    deviceMetadata {
+      nodes {
+        id
+        deviceName
+        geolocation {
+          latitude
+          longitude
+        }
+      }
+    }
+  }
+}
+`;
 
 const MapTopologyContainer: VoidFunctionComponent = () => {
 
+  const [{ data: deviceData, error }] = useQuery<GeoMapDataQueryQuery, GeoMapDataQueryQueryVariables>({
+    query: GEOMAP_DATA_QUERY,
+  });
+
+  console.log(deviceData);
+
   return (
-      <MapContainer style={{height:400}} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <MapContainer style={{height:600}} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+          {deviceData && deviceData?.deviceInventory.deviceMetadata?.nodes?.filter((node) => node?.geolocation?.latitude && node.geolocation.longitude).map((node) => {
+            return (
+              <Marker position={[node?.geolocation?.longitude!, node?.geolocation?.latitude!]} key={node?.id}>
+                <Popup>
+                  {node?.deviceName}<br /> Easily customizable.
+                </Popup>
+              </Marker>
+            )
+          })}
+        
       </MapContainer>
   );
 };
