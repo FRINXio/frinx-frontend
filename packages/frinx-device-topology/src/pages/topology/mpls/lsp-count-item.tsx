67e34b31b5c9c4d3ad4d98f3f3d22a4691bb501f
Martin Sottnik <msottnik@gmail.com>
2024-08-14 15:13:10 +0200

    add title to lsp count

--- packages/frinx-device-topology/src/pages/topology/mpls/lsp-count-item.tsx
+++ packages/frinx-device-topology/src/pages/topology/mpls/lsp-count-item.tsx
@@ -1,6 +1,5 @@
 import { chakra } from '@chakra-ui/react';
 import React, { useRef, VoidFunctionComponent } from 'react';
-import { getEdgeColor } from '../../../components/edge/edge.helpers';
 import { getCurvePath, getPointAtLength, Line, LspCount } from '../graph.helpers';
 
 const G = chakra('g');
@@ -24,7 +23,7 @@ const LspCountItem: VoidFunctionComponent<Props> = ({ linePoints, lspCount }) =>
     <g>
       <path
         ref={edgeRef}
-        strokeWidth={1}
+        strokeWidth={3}
         stroke="red"
         strokeLinejoin="round"
         fill="red"
@@ -32,8 +31,29 @@ const LspCountItem: VoidFunctionComponent<Props> = ({ linePoints, lspCount }) =>
         cursor="pointer"
       />
       <G transform={`translate3d(${incomingPosition.x}px, ${incomingPosition.y}px, 0)`} transformOrigin="center center">
-        <Circle r="15" fill="white" strokeWidth={2} stroke={getEdgeColor('NONE', false, false, false)} />
-        <Text height="sm" textAnchor="middle" y="-20">
+        <G transform="translate3d(-18px, -18px, 0) scale(1.5)">
+          <path
+            d="M11 9L8 12M8 12L11 15M8 12H16M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
+            stroke="#000000"
+            strokeWidth="1"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            fill="#fff"
+          />
+        </G>
+        <Circle r="12" fillOpacity={0}>
+          <title>incoming Laps: {lspCount.incomingLsps}</title>
+        </Circle>
+        <Text
+          height="sm"
+          textAnchor="middle"
+          y="-20"
+          paintOrder="stroke"
+          strokeWidth={3}
+          stroke="white"
+          strokeLinecap="butt"
+          strokeLinejoin="round"
+        >
           {lspCount.incomingLsps}
         </Text>
       </G>
@@ -41,8 +61,29 @@ const LspCountItem: VoidFunctionComponent<Props> = ({ linePoints, lspCount }) =>
         transform={`translate3d(${outcomingPosition.x}px, ${outcomingPosition.y}px, 0)`}
         transformOrigin="center center"
       >
-        <Circle r="15" fill="white" strokeWidth={2} stroke={getEdgeColor('NONE', false, false, false)} />
-        <Text height="sm" textAnchor="middle" y="-20">
+        <G transform="translate3d(-18px, -18px, 0) scale(1.5)">
+          <path
+            d="M17 12H7M17 12L13 16M17 12L13 8M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
+            stroke="#000000"
+            strokeWidth="1"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            fill="#fff"
+          />
+        </G>
+        <Circle r="12" fillOpacity={0}>
+          <title>outcoming Laps: {lspCount.outcomingLsps}</title>
+        </Circle>
+        <Text
+          height="sm"
+          textAnchor="middle"
+          y="-20"
+          paintOrder="stroke"
+          strokeWidth={2}
+          stroke="white"
+          strokeLinecap="butt"
+          strokeLinejoin="round"
+        >
           {lspCount.outcomingLsps}
         </Text>
       </G>
