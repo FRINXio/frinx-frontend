f2601ffe5d191bb22d90b9d9080c7f949439892e
oskovran <ondrej.skrovan@elisapolystar.com>
2024-09-18 09:48:22 +0200

    Minor lable text mistake fix (#1628)

--- packages/frinx-device-topology/src/pages/topology/mpls/mpls-table.tsx
+++ packages/frinx-device-topology/src/pages/topology/mpls/mpls-table.tsx
@@ -12,7 +12,7 @@ const MplsTable: VoidFunctionComponent<Props> = ({ data }) => {
     ['inputLabel', 'Input Label'],
     ['inputInterface', 'Input Interface'],
     ['outputLabel', 'Output Label'],
-    ['outputInterface', 'Output Ingerface'],
+    ['outputInterface', 'Output Interface'],
     ['operState', 'Oper State'],
     ['mplsOperation', 'MPLS Operation'],
     ['ldpPrefix', 'Ldp Prefix'],
